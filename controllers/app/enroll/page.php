b1de6ba6b59c5c846072adc5586ca409264c7ba9
jasony62 <jasony62@qq.com>
2015-07-17 18:22:35 +0800

    支持按页或者按活动获取登记活动的登记项

--- controllers/app/enroll/page.php
+++ controllers/app/enroll/page.php
@@ -22,15 +22,45 @@ class page extends \member_base {
      * $pageid
      * $size
      */
-    public function schemaGet_action($mpid, $id, $pageid=null, $size=null)
+    public function schemaGet_action($mpid, $id, $pageid=null, $size=null, $byPage='Y')
     {
         $modelPage = $this->model('app\enroll\page');
         
         $pages = $modelPage->byEnrollId($id);
         
-        $defs = array();
-        foreach ($pages as $page) {
-            $page->type === 'I' && $defs[$page->name] = $modelPage->schemaByHtml($page->html, $size);
+        if ($byPage === 'Y') {
+            /**
+             * 按页返回
+             */
+            $defs = array();
+            foreach ($pages as $page) {
+                $page->type === 'I' && $defs[$page->name] = $modelPage->schemaByHtml($page->html, $size);
+            }
+        } else {
+            /**
+             * 按活动返回
+             */
+            $defs = array();
+            foreach ($pages as $page) {
+                if ($page->type === 'I') {
+                    $pageDefs = $modelPage->schemaByHtml($page->html);
+                    $defs = array_merge($defs, $pageDefs);  
+                } 
+            }
+            if ($size !== null && $size > 0 && $size < count($defs)) {
+                /**
+                 * 随机获得指定数量的登记项
+                 */
+                $randomDefs = array();
+                $upper = count($defs) - 1;
+                for ($i = 0; $i < $size; $i++) {
+                    $random = mt_rand(0, $upper);
+                    $randomDefs[] = $defs[$random];
+                    array_splice($defs, $random, 1);
+                    $upper--;
+                }
+                $defs = $randomDefs;
+            }
         }
         
         return new \ResponseData($defs);

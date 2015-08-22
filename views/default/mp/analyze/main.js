01b1b079bde6326842f9b1e0804a595098775815
jasony62 <jasony62@qq.com>
2015-08-22 09:15:14 +0800

    优化访问统计代码

--- views/default/mp/analyze/main.js
+++ views/default/mp/analyze/main.js
@@ -1,11 +1,26 @@
-xxtApp.controller('analyzeCtrl', ['$scope', '$http', function ($scope, $http) {
+xxtApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/page/mp/analyze/mp', {
+        templateUrl: '/views/default/mp/analyze/mp.html',
+        controller: 'mpCtrl',
+    }).when('/page/mp/analyze/user', {
+        templateUrl: '/views/default/mp/analyze/user.html',
+        controller: 'userCtrl',
+    }).when('/page/mp/analyze/matter', {
+        templateUrl: '/views/default/mp/analyze/matter.html',
+        controller: 'matterCtrl'
+    }).otherwise({
+        templateUrl: '/views/default/mp/analyze/mp.html',
+        controller: 'mpCtrl'
+    });
+}]);
+xxtApp.controller('analyzeCtrl', ['$scope', function($scope) {
     var current, startAt, endAt;
     current = new Date();
     startAt = {
         year: current.getFullYear(),
         month: current.getMonth() + 1,
         mday: current.getDate(),
-        getTime: function () {
+        getTime: function() {
             var d = new Date(this.year, this.month - 1, this.mday, 0, 0, 0, 0);
             return d.getTime();
         }
@@ -14,43 +29,50 @@ xxtApp.controller('analyzeCtrl', ['$scope', '$http', function ($scope, $http) {
         year: current.getFullYear(),
         month: current.getMonth() + 1,
         mday: current.getDate(),
-        getTime: function () {
+        getTime: function() {
             var d = new Date(this.year, this.month - 1, this.mday, 23, 59, 59, 0);
             return d.getTime();
         }
     };
     $scope.startAt = startAt.getTime() / 1000;
     $scope.endAt = endAt.getTime() / 1000;
-    $scope.open = function ($event) {
+    $scope.open = function($event) {
         $event.preventDefault();
         $event.stopPropagation();
         $scope.opened = true;
     };
+    $scope.subView = '';
 }]);
-xxtApp.controller('mpactionCtrl', ['$scope', 'http2', function ($scope, http2) {
-    $scope.page = { size: 30 };
-    $scope.getData = function (page) {
+xxtApp.controller('mpCtrl', ['$scope', 'http2', function($scope, http2) {
+    $scope.$parent.subView = 'mp';
+    $scope.page = {
+        size: 30
+    };
+    $scope.getData = function(page) {
         if (page) $scope.page.current = page;
         var url = '/rest/mp/analyze/mpActions';
         url += '?startAt=' + $scope.startAt;
         url += '&endAt=' + $scope.endAt;
         url += '&page=' + $scope.page.current;
         url += '&size=' + $scope.page.size;
-        http2.get(url, function (rsp) {
+        http2.get(url, function(rsp) {
             $scope.logs = rsp.data[0];
             $scope.page.total = rsp.data[1];
         });
     };
-    $scope.$on('xxt.tms-datepicker.change', function (event, data) {
+    $scope.$on('xxt.tms-datepicker.change', function(event, data) {
         $scope[data.state] = data.value;
         $scope.getData(1);
     });
     $scope.getData(1);
 }]);
-xxtApp.controller('useractionCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.page = { size: 30 };
+xxtApp.controller('userCtrl', ['$scope', 'http2', function($scope, http2) {
+    $scope.$parent.subView = 'user';
+    $scope.page = {
+        size: 30
+    };
     $scope.orderby = 'read';
-    $scope.getData = function (page) {
+    $scope.getData = function(page) {
         if (page) $scope.page.current = page;
         var url = '/rest/mp/analyze/userActions';
         url += '?orderby=' + $scope.orderby;
@@ -58,25 +80,27 @@ xxtApp.controller('useractionCtrl', ['$scope', '$http', function ($scope, $http)
         url += '&endAt=' + $scope.endAt;
         url += '&page=' + $scope.page.current;
         url += '&size=' + $scope.page.size;
-        $http.get(url).
-            success(function (rsp) {
-                $scope.users = rsp.data[0];
-                $scope.page.total = rsp.data[1];
-            });
+        http2.get(url, function(rsp) {
+            $scope.users = rsp.data[0];
+            $scope.page.total = rsp.data[1];
+        });
     };
-    $scope.viewUser = function (openid) {
+    $scope.viewUser = function(openid) {
         location.href = '/rest/mp/user?openid=' + openid;
     };
-    $scope.$on('xxt.tms-datepicker.change', function (event, data) {
+    $scope.$on('xxt.tms-datepicker.change', function(event, data) {
         $scope[data.state] = data.value;
         $scope.getData(1);
     });
     $scope.getData(1);
 }]);
-xxtApp.controller('matteractionCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.page = { size: 30 };
+xxtApp.controller('matterCtrl', ['$scope', 'http2', function($scope, http2) {
+    $scope.$parent.subView = 'matter';
+    $scope.page = {
+        size: 30
+    };
     $scope.orderby = 'read';
-    $scope.getData = function (page) {
+    $scope.getData = function(page) {
         if (page) $scope.page.current = page;
         var url = '/rest/mp/analyze/matterActions';
         url += '?orderby=' + $scope.orderby;
@@ -84,13 +108,12 @@ xxtApp.controller('matteractionCtrl', ['$scope', '$http', function ($scope, $htt
         url += '&endAt=' + $scope.endAt;
         url += '&page=' + $scope.page.current;
         url += '&size=' + $scope.page.size;
-        $http.get(url).
-            success(function (rsp) {
-                $scope.matters = rsp.data[0];
-                $scope.page.total = rsp.data[1];
-            });
+        http2.get(url, function(rsp) {
+            $scope.matters = rsp.data[0];
+            $scope.page.total = rsp.data[1];
+        });
     };
-    $scope.$on('xxt.tms-datepicker.change', function (event, data) {
+    $scope.$on('xxt.tms-datepicker.change', function(event, data) {
         $scope[data.state] = data.value;
         $scope.getData(1);
     });
--- views/default/mp/matter/article/main.js
+++ views/default/mp/matter/article/main.js
@@ -1,4 +1,4 @@
-xxtApp.config(['$routeProvider', function ($routeProvider) {
+xxtApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/rest/mp/matter/article/edit', {
         templateUrl: '/views/default/mp/matter/article/edit.html',
         controller: 'editCtrl',
@@ -16,17 +16,17 @@ xxtApp.config(['$routeProvider', function ($routeProvider) {
         controller: 'editCtrl'
     });
 }]);
-xxtApp.controller('articleCtrl', ['$scope', '$location', 'http2', function ($scope, $location, http2) {
+xxtApp.controller('articleCtrl', ['$scope', '$location', 'http2', function($scope, $location, http2) {
     $scope.id = $location.search().id;
     $scope.subView = '';
-    $scope.back = function () {
+    $scope.back = function() {
         location.href = '/page/mp/matter/articles';
     };
     $scope.entryUrl = '';
-    http2.get('/rest/mp/mpaccount/get', function (rsp) {
+    http2.get('/rest/mp/mpaccount/get', function(rsp) {
         $scope.mpaccount = rsp.data;
         $scope.hasParent = rsp.data.parent_mpid && rsp.data.parent_mpid.length;
-        http2.get('/rest/mp/matter/article/get?id=' + $scope.id, function (rsp) {
+        http2.get('/rest/mp/matter/article/get?id=' + $scope.id, function(rsp) {
             $scope.editing = rsp.data;
             $scope.editing.attachments === undefined && ($scope.editing.attachments = []);
             $scope.entryUrl = 'http://' + location.host + '/rest/mi/matter?mpid=' + $scope.mpaccount.mpid + '&id=' + $scope.id + '&type=article';
@@ -38,56 +38,64 @@ xxtApp.controller('articleCtrl', ['$scope', '$location', 'http2', function ($sco
         });
     });
 }]);
-xxtApp.controller('editCtrl', ['$scope', '$modal', 'http2', function ($scope, $modal, http2) {
+xxtApp.controller('editCtrl', ['$scope', '$modal', 'http2', function($scope, $modal, http2) {
     $scope.$parent.subView = 'edit';
-    $scope.innerlinkTypes = [
-        { value: 'article', title: '单图文', url: '/rest/mp/matter' },
-        { value: 'news', title: '多图文', url: '/rest/mp/matter' },
-        { value: 'channel', title: '频道', url: '/rest/mp/matter' }
-    ];
-    var getInitData = function () {
-        http2.get('/rest/mp/matter/tag?resType=article', function (rsp) {
+    $scope.innerlinkTypes = [{
+        value: 'article',
+        title: '单图文',
+        url: '/rest/mp/matter'
+    }, {
+        value: 'news',
+        title: '多图文',
+        url: '/rest/mp/matter'
+    }, {
+        value: 'channel',
+        title: '频道',
+        url: '/rest/mp/matter'
+    }];
+    var getInitData = function() {
+        http2.get('/rest/mp/matter/tag?resType=article', function(rsp) {
             $scope.tags = rsp.data;
         });
-        http2.get('/rest/mp/mpaccount/feature?fields=matter_visible_to_creater', function (rsp) {
+        http2.get('/rest/mp/mpaccount/feature?fields=matter_visible_to_creater', function(rsp) {
             $scope.features = rsp.data;
         });
     };
-    window.onbeforeunload = function (e) {
+    window.onbeforeunload = function(e) {
         var message;
         if ($scope.bodyModified) {
             message = '已经修改的正文还没有保存',
-            e = e || window.event;
+                e = e || window.event;
             if (e) {
                 e.returnValue = message;
             }
             return message;
         }
     };
-    $scope.onBodyChange = function () {
+    $scope.onBodyChange = function() {
         $scope.bodyModified = true;
     };
-    $scope.update = function (name) {
+    $scope.update = function(name) {
         var nv = {};
         nv[name] = name === 'body' ? encodeURIComponent($scope.editing[name]) : $scope.editing[name];
-        http2.post('/rest/mp/matter/article/update?id=' + $scope.editing.id, nv, function () {
+        http2.post('/rest/mp/matter/article/update?id=' + $scope.editing.id, nv, function() {
             name === 'body' && ($scope.bodyModified = false);
         });
     };
-    $scope.setPic = function () {
+    $scope.setPic = function() {
         var options = {
-            callback: function (url) {
+            callback: function(url) {
                 $scope.editing.pic = url + '?_=' + (new Date()) * 1;
                 $scope.update('pic');
             }
         };
         $scope.$broadcast('mediagallery.open', options);
     };
-    $scope.removePic = function () {
+    $scope.removePic = function() {
         $scope.editing.pic = '';
         $scope.update('pic');
     };
-    $scope.$on('tinymce.multipleimage.open', function (event, callback) {
+    $scope.$on('tinymce.multipleimage.open', function(event, callback) {
         var options = {
             callback: callback,
             multiple: true,
@@ -95,31 +103,33 @@ xxtApp.controller('editCtrl', ['$scope', '$modal', 'http2', function ($scope, $m
         };
         $scope.$broadcast('mediagallery.open', options);
     });
-    $scope.delAttachment = function (index, att) {
+    $scope.delAttachment = function(index, att) {
         $scope.$root.progmsg = '删除文件';
         http2.get('/rest/mp/matter/article/attachmentDel?id=' + att.id, function success(rsp) {
             $scope.editing.attachments.splice(index, 1);
             $scope.$root.progmsg = null;
         });
     };
-    $scope.downloadUrl = function (att) {
+    $scope.downloadUrl = function(att) {
         return '/rest/mi/article/attachmentGet?mpid=' + $scope.editing.mpid + '&articleid=' + $scope.editing.id + '&attachmentid=' + att.id;
     };
-    $scope.gotoCode = function () {
+    $scope.gotoCode = function() {
         if ($scope.editing.page_id != 0)
             location.href = '/rest/code?pid=' + $scope.editing.page_id;
         else {
-            http2.get('/rest/code/create', function (rsp) {
-                var nv = { 'page_id': rsp.data.id };
-                http2.post('/rest/mp/matter/article/update?id=' + $scope.editing.id, nv, function () {
+            http2.get('/rest/code/create', function(rsp) {
+                var nv = {
+                    'page_id': rsp.data.id
+                };
+                http2.post('/rest/mp/matter/article/update?id=' + $scope.editing.id, nv, function() {
                     $scope.editing.page_id = rsp.data.id;
                     location.href = '/rest/code?pid=' + rsp.data.id;
                 });
             });
         }
     };
-    $scope.embedMatter = function () {
-        $scope.$broadcast('mattersgallery.open', function (matters, type) {
+    $scope.embedMatter = function() {
+        $scope.$broadcast('mattersgallery.open', function(matters, type) {
             var editor, dom, i, matter, mtype, fn;
             editor = tinymce.get('body1');
             dom = editor.dom;
@@ -128,87 +138,104 @@ xxtApp.controller('editCtrl', ['$scope', '$modal', 'http2', function ($scope, $m
                 mtype = matter.type ? matter.type : type;
                 fn = "openMatter(" + matter.id + ",'" + mtype + "')";
                 console.log('fn', fn);
-                editor.insertContent(dom.createHTML('p', { 'class': 'matter-link' }, dom.createHTML('a', {
+                editor.insertContent(dom.createHTML('p', {
+                    'class': 'matter-link'
+                }, dom.createHTML('a', {
                     href: '#',
                     "onclick": fn,
                 }, dom.encode(matter.title))));
             }
         });
     };
-    var insertVideo = function (url) {
+    var insertVideo = function(url) {
         var editor, dom, html;
         if (url.length > 0) {
             editor = tinymce.get('body1');
             dom = editor.dom;
             html = dom.createHTML('p', {},
                 dom.createHTML(
-                    'video',
-                    {
+                    'video', {
                         style: 'width:100%',
                         controls: "controls",
                     },
                     dom.createHTML(
-                        'source',
-                        {
+                        'source', {
                             src: url,
                             type: "video/mp4",
                         })
-                    )
-                );
+                )
+            );
             editor.insertContent('<p>&nbsp;</p>' + html + '<p>&nbsp;</p>');
         }
     };
-    $scope.embedVideo = function () {
+    $scope.embedVideo = function() {
         $modal.open({
             templateUrl: 'insertMedia.html',
-            controller: ['$modalInstance', '$scope', function ($mi, $scope) {
-                $scope.data = { url: '' };
-                $scope.cancel = function () { $mi.dismiss() };
-                $scope.ok = function () { $mi.close($scope.data) };
+            controller: ['$modalInstance', '$scope', function($mi, $scope) {
+                $scope.data = {
+                    url: ''
+                };
+                $scope.cancel = function() {
+                    $mi.dismiss()
+                };
+                $scope.ok = function() {
+                    $mi.close($scope.data)
+                };
             }],
             backdrop: 'static',
-        }).result.then(function (data) {
+        }).result.then(function(data) {
             insertVideo(data.url);
         });
     };
-    var insertAudio = function (url) {
+    var insertAudio = function(url) {
         var editor, dom, html;
         if (url.length > 0) {
             editor = tinymce.get('body1');
             dom = editor.dom;
-            html = dom.createHTML('p', {}, dom.createHTML('audio', { src: url, controls: "controls", }));
+            html = dom.createHTML('p', {}, dom.createHTML('audio', {
+                src: url,
+                controls: "controls",
+            }));
             editor.insertContent('<p>&nbsp;</p>' + html + '<p>&nbsp;</p>');
         }
     };
-    $scope.embedAudio = function () {
+    $scope.embedAudio = function() {
         if ($scope.mpaccount._env.SAE) {
             $modal.open({
                 templateUrl: 'insertMedia.html',
-                controller: ['$modalInstance', '$scope', function ($mi, $scope) {
-                    $scope.data = { url: '' };
-                    $scope.cancel = function () { $mi.dismiss() };
-                    $scope.ok = function () { $mi.close($scope.data) };
+                controller: ['$modalInstance', '$scope', function($mi, $scope) {
+                    $scope.data = {
+                        url: ''
+                    };
+                    $scope.cancel = function() {
+                        $mi.dismiss()
+                    };
+                    $scope.ok = function() {
+                        $mi.close($scope.data)
+                    };
                 }],
                 backdrop: 'static',
-            }).result.then(function (data) {
+            }).result.then(function(data) {
                 insertAudio(data.url);
             });
         } else {
-            $scope.$broadcast('mediagallery.open', { mediaType: '音频', callback: insertAudio });
+            $scope.$broadcast('mediagallery.open', {
+                mediaType: '音频',
+                callback: insertAudio
+            });
         }
     };
-    $scope.upload2Mp = function () {
+    $scope.upload2Mp = function() {
         var url;
         url = '/rest/mp/matter/article/upload2Mp?id=' + $scope.id;
         $scope.editing.media_id && $scope.editing.media_id.length && (url += '&mediaId=' + $scope.editing.media_id);
-        http2.get(url, function (rsp) {
+        http2.get(url, function(rsp) {
             $scope.editing.media_id = rsp.data.media_id;
             $scope.editing.upload_at = rsp.data.upload_at;
             $scope.$root.infomsg = '上传成功';
         });
     };
-
-    $scope.$on('tag.xxt.combox.done', function (event, aSelected) {
+    $scope.$on('tag.xxt.combox.done', function(event, aSelected) {
         var aNewTags = [];
         for (var i in aSelected) {
             var existing = false;
@@ -217,21 +244,22 @@ xxtApp.controller('editCtrl', ['$scope', '$modal', 'http2', function ($scope, $m
                     existing = true;
                     break;
                 }
-            }
-            !existing && aNewTags.push(aSelected[i]);
+            }!existing && aNewTags.push(aSelected[i]);
         }
-        http2.post('/rest/mp/matter/article/addTag?id=' + $scope.id, aNewTags, function (rsp) {
+        http2.post('/rest/mp/matter/article/addTag?id=' + $scope.id, aNewTags, function(rsp) {
             $scope.editing.tags = $scope.editing.tags.concat(aNewTags);
         });
     });
-    $scope.$on('tag.xxt.combox.add', function (event, newTag) {
-        var oNewTag = { title: newTag };
-        http2.post('/rest/mp/matter/article/addTag?id=' + $scope.id, [oNewTag], function (rsp) {
+    $scope.$on('tag.xxt.combox.add', function(event, newTag) {
+        var oNewTag = {
+            title: newTag
+        };
+        http2.post('/rest/mp/matter/article/addTag?id=' + $scope.id, [oNewTag], function(rsp) {
             $scope.editing.tags.push(oNewTag);
         });
     });
-    $scope.$on('tag.xxt.combox.del', function (event, removed) {
-        http2.post('/rest/mp/matter/article/removeTag?id=' + $scope.editing.id, [removed], function (rsp) {
+    $scope.$on('tag.xxt.combox.del', function(event, removed) {
+        http2.post('/rest/mp/matter/article/removeTag?id=' + $scope.editing.id, [removed], function(rsp) {
             $scope.editing.tags.splice($scope.editing.tags.indexOf(removed), 1);
         });
     });
@@ -241,67 +269,75 @@ xxtApp.controller('editCtrl', ['$scope', '$modal', 'http2', function ($scope, $m
         testChunks: false,
     });
     r.assignBrowse(document.getElementById('addAttachment'));
-    r.on('fileAdded', function (file, event) {
+    r.on('fileAdded', function(file, event) {
         console.log('fileAdded.');
         $scope.$root.progmsg = '开始上传文件';
         $scope.$root.$apply('progmsg');
         r.upload();
     });
-    r.on('progress', function (file, event) {
+    r.on('progress', function(file, event) {
         console.log('progress.');
         $scope.$root.progmsg = '正在上传文件：' + Math.floor(r.progress() * 100) + '%';
         $scope.$root.$apply('progmsg');
     });
-    r.on('complete', function () {
+    r.on('complete', function() {
         console.log('complete.');
         var f, lastModified, posted;
         f = r.files.pop().file;
         lastModified = f.lastModified ? f.lastModified : (f.lastModifiedDate ? f.lastModifiedDate.getTime() : 0);
-        posted = { name: f.name, size: f.size, type: f.type, lastModified: lastModified };
+        posted = {
+            name: f.name,
+            size: f.size,
+            type: f.type,
+            lastModified: lastModified
+        };
         http2.post('/rest/mp/matter/article/attachmentAdd?id=' + $scope.id, posted, function success(rsp) {
             $scope.editing.attachments.push(rsp.data);
             $scope.$root.progmsg = null;
         });
     });
-    $scope.$watch('editing.custom_body', function (nv) {
+    $scope.$watch('editing.custom_body', function(nv) {
         if (!nv) return;
         $scope.entryUrl = $scope.entryUrl.replace(/tpl=[^&]*/, nv === 'Y' ? 'tpl=cus' : 'tpl=std');
     });
 }]);
-xxtApp.controller('remarkCtrl', ['$scope', 'http2', function ($scope, http2) {
+xxtApp.controller('remarkCtrl', ['$scope', 'http2', function($scope, http2) {
     $scope.$parent.subView = 'remark';
-    $scope.page = { current: 1, size: 30 };
-    $scope.delRemark = function (remark, index) {
+    $scope.page = {
+        current: 1,
+        size: 30
+    };
+    $scope.delRemark = function(remark, index) {
         var ret = window.prompt('删除当前评论吗？请输入文章的标题');
         if (ret === $scope.editing.title) {
-            http2.get('/rest/mp/matter/article/remarkDel?id=' + remark.id, function (rsp) {
+            http2.get('/rest/mp/matter/article/remarkDel?id=' + remark.id, function(rsp) {
                 $scope.remarks.splice(index, 1);
             });
         }
     };
-    $scope.cleanRemark = function () {
+    $scope.cleanRemark = function() {
         var ret = window.prompt('删除当前评论吗？请输入文章的标题');
         if (ret === $scope.editing.title) {
-            http2.get('/rest/mp/matter/article/remarkClean?articleid=' + $scope.id, function (rsp) {
+            http2.get('/rest/mp/matter/article/remarkClean?articleid=' + $scope.id, function(rsp) {
                 $scope.remarks = [];
             });
         }
     };
-    $scope.doSearch = function () {
+    $scope.doSearch = function() {
         var page = 'page=' + $scope.page.current + '&size=' + $scope.page.size;
-        http2.get('/rest/mp/matter/article/remarkGet?id=' + $scope.id + '&' + page, function (rsp) {
+        http2.get('/rest/mp/matter/article/remarkGet?id=' + $scope.id + '&' + page, function(rsp) {
             $scope.remarks = rsp.data[0];
             $scope.page.total = rsp.data[1];
         });
     };
     $scope.doSearch();
 }]);
-xxtApp.controller('statCtrl', ['$scope', 'http2', function ($scope, http2) {
+xxtApp.controller('statCtrl', ['$scope', 'http2', function($scope, http2) {
     $scope.$parent.subView = 'stat';
 }])
-xxtApp.controller('readCtrl', ['$scope', 'http2', function ($scope, http2) {
+xxtApp.controller('readCtrl', ['$scope', 'http2', function($scope, http2) {
     $scope.$parent.subView = 'read';
-    http2.get('/rest/mp/matter/article/readGet?id=' + $scope.id, function (rsp) {
+    http2.get('/rest/mp/matter/article/readGet?id=' + $scope.id, function(rsp) {
         $scope.reads = rsp.data;
     });
 }]);

bdea8f06f9f1045bbcfd9399fca60c50d6c6f263
jasony62 <jasony62@qq.com>
2015-11-02 11:27:44 +0800

    update code

--- controllers/mp/app/merchant/template/product/basic.js
+++ controllers/mp/app/merchant/template/product/basic.js
@@ -1,18 +1,18 @@
 app.register.controller('productCtrl', ['$scope', '$http', 'Product', 'Sku', function($scope, $http, Product, Sku) {
-	var facProduct, facSku, today;
-	today = new Date();
-	today.setHours(0, 0, 0, 0);
-	today = today.getTime() / 1000;
+	var facProduct, facSku;
 	facProduct = new Product($scope.$parent.mpid, $scope.$parent.shopId);
 	var productGet = function(id) {
 		facProduct.get(id).then(function(product) {
-			var propValue, today, options;
+			var propValue, options;
 			$scope.product = product;
 			$scope.catelog = product.catelog;
 			$scope.propValues = product.propValue;
 			facSku = new Sku($scope.$parent.mpid, $scope.$parent.shopId, id);
 			options = {};
 			facSku.get(options).then(function(skus) {
+				if (skus && skus.length === 1) {
+					$scope.chooseSku(skus[0]);
+				}
 				$scope.skus = skus;
 			})
 		});

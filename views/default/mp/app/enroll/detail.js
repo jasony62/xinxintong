ad02dc678b1ca4d4d82b90a66be4e1f3b101f366
jasony62 <jasony62@qq.com>
2015-11-24 18:02:54 +0800

    update code

--- views/default/mp/app/enroll/detail.js
+++ views/default/mp/app/enroll/detail.js
@@ -33,7 +33,18 @@ xxtApp.config(['$routeProvider', function($routeProvider) {
         }
     }).when('/rest/mp/app/enroll/round', {
         templateUrl: '/views/default/mp/app/enroll/round.html',
-        controller: 'roundCtrl'
+        controller: 'roundCtrl',
+        resolve: {
+            load: function($q) {
+                var defer = $q.defer();
+                (function() {
+                    $.getScript('/views/default/mp/app/enroll/round.js', function() {
+                        defer.resolve();
+                    });
+                })();
+                return defer.promise;
+            }
+        }
     }).when('/rest/mp/app/enroll/page', {
         templateUrl: '/views/default/mp/app/enroll/page.html?_=1',
         controller: 'pageCtrl',
@@ -134,99 +145,6 @@ xxtApp.controller('enrollCtrl', ['$scope', '$location', 'http2', function($scope
     http2.get('/rest/mp/mpaccount/feature?fields=matter_visible_to_creater', function(rsp) {
         $scope.features = rsp.data;
     });
-}]);
-xxtApp.controller('roundCtrl', ['$scope', '$modal', 'http2', function($scope, $modal, http2) {
-    $scope.$parent.subView = 'round';
-    $scope.roundState = ['新建', '启用', '停止'];
-    $scope.add = function() {
-        $modal.open({
-            templateUrl: 'roundEditor.html',
-            backdrop: 'static',
-            resolve: {
-                roundState: function() {
-                    return $scope.roundState;
-                }
-            },
-            controller: ['$scope', '$modalInstance', 'roundState', function($scope, $modalInstance, roundState) {
-                $scope.round = {
-                    state: 0
-                };
-                $scope.roundState = roundState;
-                $scope.close = function() {
-                    $modalInstance.dismiss();
-                };
-                $scope.ok = function() {
-                    $modalInstance.close($scope.round);
-                };
-                $scope.start = function() {
-                    $scope.round.state = 1;
-                    $modalInstance.close($scope.round);
-                };
-            }]
-        }).result.then(function(newRound) {
-            http2.post('/rest/mp/app/enroll/round/add?aid=' + $scope.aid, newRound, function(rsp) {
-                if ($scope.editing.rounds.length > 0 && rsp.data.state == 1)
-                    $scope.editing.rounds[1].state = 2;
-                $scope.editing.rounds.splice(0, 0, rsp.data);
-            });
-        });
-    };
-    $scope.open = function(round) {
-        $modal.open({
-            templateUrl: 'roundEditor.html',
-            backdrop: 'static',
-            resolve: {
-                roundState: function() {
-                    return $scope.roundState;
-                }
-            },
-            controller: ['$scope', '$modalInstance', 'roundState', function($scope, $modalInstance, roundState) {
-                $scope.round = angular.copy(round);
-                $scope.roundState = roundState;
-                $scope.close = function() {
-                    $modalInstance.dismiss();
-                };
-                $scope.ok = function() {
-                    $modalInstance.close({
-                        action: 'update',
-                        data: $scope.round
-                    });
-                };
-                $scope.remove = function() {
-                    $modalInstance.close({
-                        action: 'remove'
-                    });
-                };
-                $scope.start = function() {
-                    $scope.round.state = 1;
-                    $modalInstance.close({
-                        action: 'update',
-                        data: $scope.round
-                    });
-                };
-            }]
-        }).result.then(function(rst) {
-            var url;
-            if (rst.action === 'update') {
-                url = '/rest/mp/app/enroll/round/update';
-                url += '?aid=' + $scope.aid;
-                url += '&rid=' + round.rid;
-                http2.post(url, rst.data, function(rsp) {
-                    if ($scope.editing.rounds.length > 1 && rst.data.state == 1)
-                        $scope.editing.rounds[1].state = 2;
-                    angular.extend(round, rst.data);
-                });
-            } else if (rst.action === 'remove') {
-                url = '/rest/mp/app/enroll/round/remove';
-                url += '?aid=' + $scope.aid;
-                url += '&rid=' + round.rid;
-                http2.get(url, function(rsp) {
-                    var i = $scope.editing.rounds.indexOf(round);
-                    $scope.editing.rounds.splice(i, 1);
-                });
-            }
-        });
-    };
 }]);
 xxtApp.controller('statCtrl', ['$scope', 'http2', function($scope, http2) {
     $scope.$parent.subView = 'stat';

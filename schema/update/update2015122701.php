0584dbdd05a165ca14c033bf0ca6d702812552ec
jasony62 <jasony62@qq.com>
2015-12-27 11:24:39 +0800

    update code

---
+++ schema/update/update2015122701.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../../db.php';
+
+$sqls = array();
+$sqls[] = "alter table xxt_log change method method varchar(255) not null";
+
+foreach ($sqls as $sql) {
+	if (!$mysqli->query($sql)) {
+		header('HTTP/1.0 500 Internal Server Error');
+		echo 'database error: ' . $mysqli->error;
+	}
+}
+echo "end update " . __FILE__ . PHP_EOL;

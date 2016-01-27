ba468ab644492375a48cc14843713d3b4cfe55a6
Jason Young <jasony62@qq.com>
2016-01-28 07:11:53 +0800

    update code

---
+++ views/default/custom-loading.js
@@ -0,0 +1,57 @@
+window.loading = {
+	finish: function() {
+		eleLoading = document.querySelector('.loading');
+		eleLoading.parentNode.removeChild(eleLoading);
+	},
+	load: function() {
+		require.config({
+			paths: {
+				"domReady": '/static/js/domReady',
+				"angular": "/static/js/angular.min",
+				"angular-sanitize": "/static/js/angular-sanitize.min",
+				"xxt-share": "/static/js/xxt.share",
+			},
+			shim: {
+				"angular": {
+					exports: "angular"
+				},
+				"angular-sanitize": {
+					deps: ['angular'],
+					exports: "angular-sanitize"
+				},
+				"xxt-share": {
+					exports: "xxt-share"
+				},
+			},
+			deps: ['/views/default/custom.js?_=1']
+		});
+	}
+};
+if (/MicroMessenger/i.test(navigator.userAgent)) {
+	var mpid = location.search.match(/[\?&]mpid=([^&]*)/)[1];
+	requirejs(["http://res.wx.qq.com/open/js/jweixin-1.0.0.js"], function(wx) {
+		window.wx = wx;
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', "/rest/mi/matter/wxjssdksignpackage?mpid=" + mpid + "&url=" + encodeURIComponent(location.href.split('#')[0]), true);
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				if (xhr.status >= 200 && xhr.status < 400) {
+					try {
+						eval("(" + xhr.responseText + ')');
+						signPackage.debug = false;
+						signPackage.jsApiList = ['hideOptionMenu', 'onMenuShareTimeline', 'onMenuShareAppMessage'];
+						wx.config(signPackage);
+						window.loading.load();
+					} catch (e) {
+						alert('local error:' + e.toString());
+					}
+				} else {
+					alert('http error:' + xhr.statusText);
+				}
+			};
+		}
+		xhr.send();
+	});
+} else {
+	window.loading.load();
+}

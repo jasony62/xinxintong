b10320519c7bdc1bab1afe4cd0e59c248fc58b1a
jasony62 <jasony62@qq.com>
2016-02-01 15:01:52 +0800

    update code

---
+++ views/default/mp/mission/detail.js
@@ -0,0 +1,38 @@
+xxtApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/rest/mp/mission/setting', {
+        templateUrl: '/views/default/mp/mission/setting.html?_=1',
+        controller: 'settingCtrl',
+        resolve: {
+            load: function($q) {
+                var defer = $q.defer();
+                (function() {
+                    $.getScript('/views/default/mp/mission/setting.js', function() {
+                        defer.resolve();
+                    });
+                })();
+                return defer.promise;
+            }
+        }
+    }).otherwise({
+        templateUrl: '/views/default/mp/mission/setting.html?_=1',
+        controller: 'settingCtrl'
+    });
+}]);
+xxtApp.controller('ctrlMission', ['$scope', '$location', 'http2', function($scope, $location, http2) {
+    $scope.id = $location.search().id;
+    $scope.back = function() {
+        history.back();
+    };
+    $scope.$on('$routeChangeSuccess', function(evt, nextRoute, lastRoute) {
+        if (nextRoute.loadedTemplateUrl.indexOf('/setting') !== -1) {
+            $scope.subView = 'setting';
+        }
+    });
+    http2.get('/rest/mp/mpaccount/get', function(rsp) {
+        $scope.mpaccount = rsp.data;
+    });
+    http2.get('/rest/mp/mission/get?id=' + $scope.id, function(rsp) {
+        $scope.editing = rsp.data;
+        $scope.editing.type = 'mission';
+    });
+}]);

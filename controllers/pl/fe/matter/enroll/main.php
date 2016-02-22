877d599871d356eebd2d69466274781c6bd267ee
jasony62 <jasony62@qq.com>
2016-02-22 16:18:39 +0800

    update code

---
+++ controllers/pl/fe/matter/enroll/main.php
@@ -0,0 +1,36 @@
+<?php
+namespace pl\fe\matter\enroll;
+
+require_once dirname(dirname(__FILE__)) . '/base.php';
+/*
+ * 登记活动主控制器
+ */
+class main extends \pl\fe\matter\base {
+	/**
+	 *
+	 */
+	protected function getMatterType() {
+		return 'enroll';
+	}
+	/**
+	 * 返回视图
+	 */
+	public function index_action() {
+		\TPL::output('/pl/fe/matter/enroll/frame');
+		exit;
+	}
+	/**
+	 * 返回视图
+	 */
+	public function setting_action() {
+		\TPL::output('/pl/fe/matter/enroll/frame');
+		exit;
+	}
+	/**
+	 * 返回视图
+	 */
+	public function page_action() {
+		\TPL::output('/pl/fe/matter/enroll/frame');
+		exit;
+	}
+}

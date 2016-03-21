0f8e26c3944877071339226045c5f3edab892bbb
jasony62 <jasony62@qq.com>
2016-03-21 16:44:03 +0800

    update code

--- models/site/fe/way.php
+++ models/site/fe/way.php
@@ -19,7 +19,7 @@ class way_model extends \TMS_MODEL {
 				$modelSns = \TMS_App::M('site\sns\\' . $snsName);
 				$siteSns = $modelSns->bySite($siteId);
 				if (isset($siteSns->by_platform) && $siteSns->by_platform === 'Y') {
-					$cookieUser = $this->_bindPlatformSnsUser($snsName, $snsUser, $cookieUser);
+					$cookieUser = $this->_bindPlatformSnsUser($siteId, $snsName, $snsUser, $cookieUser);
 				} else {
 					$cookieUser = $this->_bindSiteSnsUser($siteId, $snsName, $snsUser, $cookieUser);
 				}
@@ -37,90 +37,6 @@ class way_model extends \TMS_MODEL {
 		$this->setCookieUser($siteId, $cookieUser);
 
 		return $cookieUser;
-	}
-	/**
-	 * 检查指定的用户是否已经登录
-	 */
-	public function isLogined($siteId, $who) {
-		/*如果已经超过有效期，认证不通过*/
-		if (empty($who->loginExpire) || $who->loginExpire < time()) {
-			$this->mySetcookie("_site_{$siteId}_fe_login", '', time() - 3600);
-			return false;
-		}
-		/*通过cookie返回登录状态*/
-		$this->setCookieLogin($siteId, $who);
-
-		return true;
-	}
-	/**
-	 * 设置 COOKIE
-	 * @param string $name
-	 * @param string $value
-	 * @param int $expire
-	 * @param string $path
-	 * @param string $domain
-	 * @param string $secure
-	 */
-	protected function mySetCookie($name, $value = '', $expire = null, $path = '/', $domain = null, $secure = false) {
-		if (!$domain and G_COOKIE_DOMAIN) {
-			$domain = G_COOKIE_DOMAIN;
-		}
-		return setcookie(G_COOKIE_PREFIX . $name, $value, $expire, $path, $domain, $secure);
-	}
-	/**
-	 * 获取cookie的值
-	 */
-	protected function myGetCookie($name) {
-		$cookiename = G_COOKIE_PREFIX . $name;
-		if (isset($_COOKIE[$cookiename])) {
-			return $_COOKIE[$cookiename];
-		}
-		return false;
-	}
-	/**
-	 *
-	 */
-	protected function getCookieKey($seed) {
-		return md5($seed);
-	}
-	/**
-	 * 将当前用户的身份保留的在cookie中
-	 */
-	public function setCookieUser($siteId, $user) {
-		$cookiekey = $this->getCookieKey($siteId);
-		$cookieUser = $user;
-		$cookieUser = json_encode($cookieUser);
-		$encoded = $this->encrypt($cookieUser, 'ENCODE', $cookiekey);
-		$expireAt = time() + (86400 * TMS_COOKIE_SITE_USER_EXPIRE);
-		$this->mySetCookie("_site_{$siteId}_fe_user", $encoded, $expireAt);
-
-		return true;
-	}
-	/**
-	 * 从cookie中获取当前用户信息
-	 */
-	public function getCookieUser($siteId) {
-		$cookiekey = $this->getCookieKey($siteId);
-		$encoded = $this->myGetCookie("_site_{$siteId}_fe_user");
-		if (empty($encoded)) {
-			return false;
-		}
-		$cookieUser = $this->encrypt($encoded, 'DECODE', $cookiekey);
-		$cookieUser = json_decode($cookieUser);
-
-		return $cookieUser;
-	}
-	/**
-	 * 设置用户登录信息
-	 */
-	public function setCookieLogin($siteId, $who) {
-		/*通过cookie返回登录状态*/
-		$login = new \stdClass;
-		$login->nickname = empty($who->nickname) ? '匿名' : $who->nickname;
-		$json = $this->toJson($login);
-		$this->mySetcookie("_site_{$siteId}_fe_login", $json);
-
-		return true;
 	}
 	/**
 	 * 绑定站点第三方认证用户
@@ -214,8 +130,175 @@ class way_model extends \TMS_MODEL {
 	/**
 	 * 绑定平台第三方认证用户
 	 */
-	private function _bindPlatformSnsUser($snsName, $snsUser, $cookieUser) {
+	private function _bindPlatformSnsUser($siteId, $snsName, $snsUser, $cookieUser) {
+		$modelSiteUser = \TMS_App::M('site\user\account');
+		$modelSnsUser = \TMS_App::M('site\sns\pl\\' . $snsName . 'fan');
+		// 已经存在的第三方认证用户
+		$authedUser = $modelSnsUser->byOpenid($snsUser->openid, 'userid,nickname,headimgurl,sex,country,province,city');
+
+		if ($authedUser) {
+			if ($cookieUser === false) {
+				if (!empty($authedUser->userid)) {
+					/* 已经绑定过站点用户 */
+					$siteUser = $modelSiteUser->byId($authedUser->userid);
+				} else {
+					/* 创建新的站点用户 */
+					$siteUser = $modelSiteUser->blank($siteId, true, array('ufrom' => $snsName));
+				}
+				/* 新的cookie用户 */
+				$cookieUser = new \stdClass;
+			} else {
+				$siteUser = $modelSiteUser->byId($cookieUser->uid);
+				if ($siteUser === false) {
+					if (!empty($authedUser->userid)) {
+						/* 已经绑定过站点用户 */
+						$siteUser = $modelSiteUser->byId($authedUser->userid);
+					} else {
+						/* 创建新的站点用户 */
+						$siteUser = $modelSiteUser->blank($siteId, true, array('ufrom' => $snsName));
+					}
+				} else if ($authedUser->userid !== $siteUser->uid) {
+					/* 更新认证用户关联的站点用户信息 */
+					$modelSiteUser->update(
+						'xxt_site_account',
+						array('assoc_uid' => $siteUser->uid),
+						"uid='{$authedUser->userid}'"
+					);
+					$modelSnsUser->update(
+						$siteId,
+						$snsUser->openid,
+						array('userid' => $siteUser->uid)
+					);
+					$siteUser = $modelSiteUser->byId($authedUser->userid);
+				}
+			}
+			/* 清空不必要的数据，减小cookie尺寸 */
+			unset($authedUser->userid);
+		} else {
+			/* 不是关注用户，建一个空的关注用户 */
+			$options = array();
+			if ($snsName === 'wx') {
+				isset($snsUser->nickname) && $options['nickname'] = $snsUser->nickname;
+				isset($snsUser->sex) && $options['sex'] = $snsUser->sex;
+				isset($snsUser->headimgurl) && $options['headimgurl'] = $snsUser->headimgurl;
+				isset($snsUser->country) && $options['country'] = $snsUser->country;
+				isset($snsUser->province) && $options['province'] = $snsUser->province;
+				isset($snsUser->city) && $options['city'] = $snsUser->city;
+			}
+			if ($cookieUser === false) {
+				$siteUser = $modelSiteUser->blank($siteId, true, array('ufrom' => $snsName));
+				$options['userid'] = $siteUser->uid;
+				$authedUser = $modelSnsUser->blank($snsUser->openid, true, $options);
+				/* 新的cookie用户 */
+				$cookieUser = new \stdClass;
+			} else {
+				$siteUser = $modelSiteUser->byId($cookieUser->uid);
+				if ($siteUser === false) {
+					/* 没有站点用户创建个新的 */
+					$siteUser = $modelSiteUser->blank($siteId, true, array('ufrom' => $snsName));
+				}
+				/* 创建新的第三方认证用户 */
+				$options['userid'] = $siteUser->uid;
+				$authedUser = $modelSnsUser->blank($snsUser->openid, true, $options);
+				/* 清空不必要的数据，减小cookie尺寸 */
+				unset($authedUser->userid);
+				unset($authedUser->siteid);
+				unset($authedUser->subscribe_at);
+				unset($authedUser->sync_at);
+			}
+		}
+		/* 更新cookie信息 */
+		$cookieUser->uid = $siteUser->uid;
+		$cookieUser->nickname = $authedUser->nickname;
+		$cookieUser->expire = time() + (86400 * TMS_COOKIE_SITE_USER_EXPIRE);
+		!isset($cookieUser->sns) && $cookieUser->sns = new \stdClass;
+		$cookieUser->sns->{$snsName} = $authedUser;
+
 		return $cookieUser;
+	}
+	/**
+	 * 检查指定的用户是否已经登录
+	 */
+	public function isLogined($siteId, $who) {
+		/*如果已经超过有效期，认证不通过*/
+		if (empty($who->loginExpire) || $who->loginExpire < time()) {
+			$this->mySetcookie("_site_{$siteId}_fe_login", '', time() - 3600);
+			return false;
+		}
+		/*通过cookie返回登录状态*/
+		$this->setCookieLogin($siteId, $who);
+
+		return true;
+	}
+	/**
+	 * 设置 COOKIE
+	 * @param string $name
+	 * @param string $value
+	 * @param int $expire
+	 * @param string $path
+	 * @param string $domain
+	 * @param string $secure
+	 */
+	protected function mySetCookie($name, $value = '', $expire = null, $path = '/', $domain = null, $secure = false) {
+		if (!$domain and G_COOKIE_DOMAIN) {
+			$domain = G_COOKIE_DOMAIN;
+		}
+		return setcookie(G_COOKIE_PREFIX . $name, $value, $expire, $path, $domain, $secure);
+	}
+	/**
+	 * 获取cookie的值
+	 */
+	protected function myGetCookie($name) {
+		$cookiename = G_COOKIE_PREFIX . $name;
+		if (isset($_COOKIE[$cookiename])) {
+			return $_COOKIE[$cookiename];
+		}
+		return false;
+	}
+	/**
+	 *
+	 */
+	protected function getCookieKey($seed) {
+		return md5($seed);
+	}
+	/**
+	 * 将当前用户的身份保留的在cookie中
+	 */
+	public function setCookieUser($siteId, $user) {
+		$cookiekey = $this->getCookieKey($siteId);
+		$cookieUser = $user;
+		$cookieUser = json_encode($cookieUser);
+		$encoded = $this->encrypt($cookieUser, 'ENCODE', $cookiekey);
+		$expireAt = time() + (86400 * TMS_COOKIE_SITE_USER_EXPIRE);
+		$this->mySetCookie("_site_{$siteId}_fe_user", $encoded, $expireAt);
+
+		return true;
+	}
+	/**
+	 * 从cookie中获取当前用户信息
+	 */
+	public function getCookieUser($siteId) {
+		$cookiekey = $this->getCookieKey($siteId);
+		$encoded = $this->myGetCookie("_site_{$siteId}_fe_user");
+		if (empty($encoded)) {
+			return false;
+		}
+		$cookieUser = $this->encrypt($encoded, 'DECODE', $cookiekey);
+		$cookieUser = json_decode($cookieUser);
+
+		return $cookieUser;
+	}
+	/**
+	 * 设置用户登录信息
+	 */
+	public function setCookieLogin($siteId, $who) {
+		/*通过cookie返回登录状态*/
+		$login = new \stdClass;
+		$login->nickname = empty($who->nickname) ? '匿名' : $who->nickname;
+		$json = $this->toJson($login);
+		$this->mySetcookie("_site_{$siteId}_fe_login", $json);
+
+		return true;
 	}
 	/**
 	 * 清除用户登录信息

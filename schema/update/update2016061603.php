23ebe1e4993a0ce55ee6795b1c43143b4eb87b58
jasony62 <jasony62@qq.com>
2016-06-16 14:50:32 +0800

    update code

--- schema/update/update2016061603.php
+++ schema/update/update2016061603.php
@@ -3,9 +3,12 @@ require_once '../../db.php';
 $sqls = array();
 //
 $sqls[] = "alter table xxt_article change summary summary varchar(240) not null default ''";
-$sqls[] = "alter table xxt_channel change summary summary varchar(240) not null default ''";
 $sqls[] = "alter table xxt_link change summary summary varchar(240) not null default ''";
+$sqls[] = "alter table xxt_channel change summary summary varchar(240) not null default ''";
+$sqls[] = "alter table xxt_channel change fixed_title fixed_title varchar(70) not null default ''";
 $sqls[] = "alter table xxt_news change summary summary varchar(240) not null default ''";
+$sqls[] = "alter table xxt_news change empty_reply_type empty_reply_type varchar(20) not null default ''";
+$sqls[] = "alter table xxt_news change empty_reply_id empty_reply_id varchar(40) not null default ''";
 
 foreach ($sqls as $sql) {
 	if (!$mysqli->query($sql)) {

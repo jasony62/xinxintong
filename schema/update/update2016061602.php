1e89b427b06d95ac89ad93574f43ea4b5505d090
jasony62 <jasony62@qq.com>
2016-06-16 11:14:43 +0800

    update code

---
+++ schema/update/update2016061602.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../../db.php';
+$sqls = array();
+//
+$sqls[] = "alter table xxt_log_matter_read change mpid mpid varchar(32) not null default ''";
+$sqls[] = "alter table xxt_log_matter_share change mpid mpid varchar(32) not null default ''";
+$sqls[] = "alter table xxt_log_tmplmsg change mpid mpid varchar(32) not null default ''";
+$sqls[] = "alter table xxt_log_user_action change mpid mpid varchar(32) not null default ''";
+$sqls[] = "alter table xxt_log_user_matter change mpid mpid varchar(32) not null default ''";
+$sqls[] = "alter table xxt_log_matter_action change mpid mpid varchar(32) not null default ''";
+//
+$sqls[] = "alter table xxt_log_matter_op change matter_title matter_title varchar(70) not null default ''";
+$sqls[] = "alter table xxt_log_matter_op change matter_summary matter_summary varchar(240) not null default ''";
+
+foreach ($sqls as $sql) {
+	if (!$mysqli->query($sql)) {
+		header('HTTP/1.0 500 Internal Server Error');
+		echo 'database error: ' . $mysqli->error;
+	}
+}
+
+echo "end update " . __FILE__ . PHP_EOL;

e019a0023be9956945fbdce409e6c7bb1166e71e
lt <lqqllt@163.com>
2016-07-04 11:13:12 +0800

    添加配置信息 Signed-off-by: lt <lqqllt@163.com>

---
+++ cus/db.php
@@ -0,0 +1,8 @@
+<?php 
+define('TMS_MYSQL_HOST',"localhost");
+define('TMS_MYSQL_PORT',3306);
+define('TMS_MYSQL_USER',"root");
+define('TMS_MYSQL_PASS',"123456");
+define('TMS_MYSQL_DB', "ctsi");
+
+ ?>

201b827540a419cf9e823a64fbbbf84f3179fbcf
lt <lqqllt@163.com>
2016-07-05 09:29:15 +0800

    忽略 Signed-off-by: lt <lqqllt@163.com>

--- cus/db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('TMS_MYSQL_HOST',"localhost");
-define('TMS_MYSQL_PORT',3306);
-define('TMS_MYSQL_USER',"root");
-define('TMS_MYSQL_PASS',"123456");
-define('TMS_MYSQL_DB', "ctsi");
-
- ?>

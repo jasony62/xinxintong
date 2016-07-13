d9838253bb03f5349fc611db9572df5837114c56
aYellowApple <2764411477@qq.com>
2016-07-13 22:43:57 +0800

    recover master

--- views/default/pl/fe/matter/addressbook/main.vw.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once dirname(dirname(__FILE__)).'/inapp.vw.php';
-
-$view['params']['app-view'] = '/mp/app/addressbook';
-$view['params']['angular-modules'] = "'ui.bootstrap'";
-$view['params']['css'] = array(array('/mp/app/addressbook','main'));
-$view['params']['js'] = array(array('/mp/app/addressbook','main'));

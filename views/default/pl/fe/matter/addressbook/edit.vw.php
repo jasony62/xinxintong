c0cd5e6b7c15d897eae7aaf83a3e92f083c36a18
lt <lqqllt@163.com>
2016-07-13 15:37:40 +0800

    删除视图回复无关工作的代码 Signed-off-by: lt <lqqllt@163.com>

--- views/default/pl/fe/matter/addressbook/edit.vw.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once  'inmp.vw.php';
-
-$view['params']['menu'] = '/rest/mp/app';
-$view['params']['layout-body'] = '/pl/fe/matter/addressbook/frame';
-$view['params']['global_js'] = array('xxt.ui', 'jquery.form.min');
-$view['params']['angular-modules'] = "'ui.xxt','ui.bootstrap'";
-$view['params']['css'] = array(array('/mp/app/addressbook', 'edit'));
-$view['params']['js'] = array(array('/mp/app/addressbook', 'edit', 'deptSelector'));
--- views/default/pl/fe/matter/addressbook/main.vw.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once dirname(__FILE__).'/inapp.vw.php';
-
-$view['params']['app-view'] = '/pl/fe/matter/addressbook';
-$view['params']['angular-modules'] = "'ui.bootstrap'";
-$view['params']['css'] = array(array('/pl/fe/matter/addressbook','main'));
-$view['params']['js'] = array(array('/pl/fe/matter/addressbook','main'));

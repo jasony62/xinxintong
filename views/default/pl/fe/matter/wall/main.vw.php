85d8539b71a08840438850536904f28183f98999
liao1a <153887111@qq.com>
2016-07-14 19:42:48 +0800

    删除xxt中的前台页面

--- views/default/pl/fe/matter/wall/main.vw.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once dirname(__FILE__).'/inapp.vw.php';
-
-TPL::assign('entryURL', 'http://'.$_SERVER['HTTP_HOST'].'/rest/app/wall?mpid='.$_SESSION['mpid']);
-
-$view['params']['app-view'] = '/pl/fe/matter/wall';
-$view['params']['css'] = array(array('/pl/fe/matter/wall', 'main'));
-$view['params']['js'] = array(array('/pl/fe/matter/wall', 'main'));

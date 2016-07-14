9758beb64671870477c0f53c6cce8a4852a92fb3
aYellowApple <2764411477@qq.com>
2016-07-14 12:43:08 +0800

    add edit.css and edit.js html

--- views/default/pl/fe/matter/addressbook/edit.js
+++ views/default/pl/fe/matter/addressbook/edit.js
@@ -18,14 +18,14 @@ xxtApp.controller('ImportAddressbookModalInstCtrl', ['$scope', '$uibModalInstanc
         $mi.dismiss();
     };
 }]);
-xxtApp.controller('abCtrl', ['$scope', 'http2', function($scope, http2) {
+xxtApp.controller('ctrlAddressbook', ['$scope', 'http2', function($scope, http2) {
     $scope.back = function() {
-        location.href = '/rest/mp/app/addressbook';
+        location.href = '/rest/pl/fe/matter/addressbook';
     };
     $scope.update = function(name) {
         var nv = {};
         nv[name] = $scope.editing[name];
-        http2.post('/rest/mp/app/addressbook/update?abid=' + $scope.editing.id, nv);
+        http2.post('/rest/pl/fe/matter/addressbook/update?abid=' + $scope.editing.id, nv);
     };
     $scope.setPic = function() {
         var options = {

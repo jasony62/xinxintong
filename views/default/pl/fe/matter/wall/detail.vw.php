363864b6c99f5ab28843486e89b74578ef654987
liao1a <153887111@qq.com>
2016-07-15 11:12:55 +0800

    Xxt (#10)
    清除迁移的view

--- views/default/pl/fe/matter/wall/detail.vw.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once dirname(__FILE__).'/inmp.vw.php';
-
-$view['params']['menu'] = '/rest/mp/app';
-$view['params']['layout-body'] = '/pl/fe/matter/wall/detail';
-$view['params']['angular-modules'] = "'ngRoute','ui.xxt','ui.bootstrap'";
-$view['params']['global_js'] = array('xxt.ui', true);
-$view['params']['css'] = array(array('/pl/fe/matter/wall', 'detail', true));
-$view['params']['js'] = array(array('/pl/fe/matter/wall', 'detail', true));

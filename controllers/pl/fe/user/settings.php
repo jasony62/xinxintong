4bc7cc8a93b581d79f1086b82639ae28f21f6800
jasony62 <jasony62@qq.com>
2016-07-18 09:08:07 +0800

    update code

---
+++ controllers/pl/fe/user/settings.php
@@ -0,0 +1,21 @@
+<?php
+namespace pl\fe\user;
+/**
+ * 平台管理端用户登录
+ */
+class settings extends \TMS_CONTROLLER {
+
+	public function get_access_rule() {
+		$rule_action['rule_type'] = 'white';
+		$rule_action['actions'] = array();
+		$rule_action['actions'][] = 'index';
+
+		return $rule_action;
+	}
+	/**
+	 * 进入平台管理页面用户身份验证页面
+	 */
+	public function index_action() {
+		$this->view_action('/pl/fe/user/settings');
+	}
+}

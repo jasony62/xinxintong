3191e9fd605f73f3279b10ca217d2581e503ac83
aYellowApple <2764411477@qq.com>
2016-07-20 12:50:54 +0800

    modified setting

--- views/default/pl/fe/matter/addressbook/frame.js
+++ views/default/pl/fe/matter/addressbook/frame.js
@@ -1,43 +1,45 @@
-define(['require'], function() {
+define(['require'], function(require) {
+    'use strict';
     var ngApp = angular.module('app', ['ngRoute', 'ui.tms', 'tinymce.ui.xxt', 'ui.xxt', 'channel.fe.pl']);
-    ngApp.config(['$controllerProvider', '$routeProvider', '$locationProvider', '$compileProvider', '$uibTooltipProvider', function($controllerProvider, $routeProvider, $locationProvider, $compileProvider, $uibTooltipProvider) {
-        var RouteParam = function(name) {
-            var baseURL = '/views/default/pl/fe/matter/addressbook/';
-            this.templateUrl = baseURL + name + '.html?_=' + (new Date() * 1);
-            this.controller = 'ctrl' + name[0].toUpperCase() + name.substr(1);
-            this.resolve = {
-                load: function($q) {
-                    var defer = $q.defer();
-                    require([baseURL + name + '.js'], function() {
-                        defer.resolve();
-                    });
-                    return defer.promise;
-                }
+    ngApp.config(['$controllerProvider', '$routeProvider', '$locationProvider', '$compileProvider', '$uibTooltipProvider',
+        function($controllerProvider, $routeProvider, $locationProvider, $compileProvider, $uibTooltipProvider) {
+            var RouteParam = function(name) {
+                var baseURL = '/views/default/pl/fe/matter/addressbook/';
+                this.templateUrl = baseURL + name + '.html?_=' + (new Date() * 1);
+                this.controller = 'ctrl' + name[0].toUpperCase() + name.substr(1);
+                this.resolve = {
+                    load: function($q) {
+                        var defer = $q.defer();
+                        require([baseURL + name + '.js'], function() {
+                            defer.resolve();
+                        });
+                        return defer.promise;
+                    }
+                };
             };
-        };
-        ngApp.provider = {
-            controller: $controllerProvider.register,
-            directive: $compileProvider.directive
-        };
-        $routeProvider
-            .when('/rest/pl/fe/matter/addressbook/setting', new RouteParam('setting'))
-            .when('/rest/pl/fe/matter/addressbook/dept', new RouteParam('dept'))
-            .when('/rest/pl/fe/matter/addressbook/roll', new RouteParam('roll'))
-            .otherwise(new RouteParam('setting'));
+            ngApp.provider = {
+                controller: $controllerProvider.register,
+                directive: $compileProvider.directive
+            };
+            $routeProvider
+                .when('/rest/pl/fe/matter/addressbook/dept', new RouteParam('dept'))
+                .when('/rest/pl/fe/matter/addressbook/roll', new RouteParam('roll'))
+                .otherwise(new RouteParam('setting'));
 
-        $locationProvider.html5Mode(true);
-        $uibTooltipProvider.setTriggers({
-          'show': 'hide'
-        });
-    }]);
-    ngApp.controller('ctrlFrame', ['$scope', '$location', '$uibModal', '$q', 'http2', 'noticebox', function($scope, $location, $uibModal, $q, http2, noticebox) {
+            $locationProvider.html5Mode(true);
+
+            $uibTooltipProvider.setTriggers({
+                'show': 'hide'
+            });
+        }]);
+    ngApp.controller('ctrlFrame', ['$scope', '$location', '$uibModal', '$q', 'http2', 'noticebox', function($scope,
+                                                                                                            $location, $uibModal, $q, http2, noticebox) {
         var ls = $location.search(),
             modifiedData = {};
 
         $scope.id = ls.id;
         $scope.siteId = ls.site;
         $scope.modified = false;
-
         $scope.getApp = function() {
             http2.get('/rest/pl/fe/site/snsList?site=' + $scope.siteId, function(rsp) {
                 $scope.sns = rsp.data;
--- views/default/pl/fe/matter/addressbook/setting.js
+++ views/default/pl/fe/matter/addressbook/setting.js
@@ -1,31 +1,34 @@
 define(['frame'], function(ngApp) {
-ngApp.provider.controller('ctrlSetting', ['$scope', '$q', 'http2', 'mattersgallery', 'noticebox',function($scope, $q, http2, mattersgallery, noticebox) {
-    /*$scope.update = function(name) {
-        var nv = {};
-        nv[name] = $scope.editing[name];
-        http2.post('/rest/pl/fe/matter/addressbook/update?abid=' + $scope.editing.id, nv);
-    };
-    $scope.setPic = function() {
-        var options = {
-            callback: function(url) {
-                $scope.editing.pic = url + '?_=' + (new Date()) * 1
-                $scope.update('pic');
-            }
+    ngApp.provider.controller('ctrlSetting', ['$scope', '$q', 'http2', 'mattersgallery', 'noticebox', function ($scope, $q, http2, mattersgallery, noticebox) {
+        $scope.back = function() {
+            location.href = '/rest/mp/app/addressbook';
         };
-        $scope.$broadcast('mediagallery.open', options);
-    };
-    $scope.removePic = function() {
-        $scope.editing.pic = '';
-        $scope.update('pic');
-    };
-    $scope.$watch('abid', function(nv) {
-        http2.get('/rest/pl/fe/matter/addressbook/get?abid=' + nv, function(rsp) {
-            $scope.editing = rsp.data;
-            $scope.entryUrl = "http://" + location.host + "/rest/app/addressbook?mpid=" + $scope.editing.mpid + "&id=" + $scope.editing.id;
+        $scope.update = function(name) {
+            var nv = {};
+            nv[name] = $scope.editing[name];
+            http2.post('/rest/mp/app/addressbook/update?abid=' + $scope.editing.id, nv);
+        };
+        $scope.setPic = function() {
+            var options = {
+                callback: function(url) {
+                    $scope.editing.pic = url + '?_=' + (new Date()) * 1
+                    $scope.update('pic');
+                }
+            };
+            $scope.$broadcast('mediagallery.open', options);
+        };
+        $scope.removePic = function() {
+            $scope.editing.pic = '';
+            $scope.update('pic');
+        };
+        $scope.$watch('abid', function(nv) {
+            http2.get('/rest/mp/app/addressbook/get?abid=' + nv, function(rsp) {
+                $scope.editing = rsp.data;
+                $scope.entryUrl = "http://" + location.host + "/rest/app/addressbook?mpid=" + $scope.editing.mpid + "&id=" + $scope.editing.id;
+            });
         });
-    });
-    http2.get('/rest/mp/mpaccount/get', function(rsp) {
-        $scope.mpaccount = rsp.data;
-    });*/
-}]);
-};
+        http2.get('/rest/mp/mpaccount/get', function(rsp) {
+            $scope.mpaccount = rsp.data;
+        });
+    }]);
+});

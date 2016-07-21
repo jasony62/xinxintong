471ddbb5c9df8661a2901a59527335689cec7265
lishuai <lszgf_li@163.com>
2016-07-21 11:07:59 +0800

    'message-page-users'

--- views/default/pl/fe/matter/wall/page.js
+++ views/default/pl/fe/matter/wall/page.js
@@ -1,3 +1,46 @@
+
+define(['frame'], function(ngApp) {
+	/**
+	 * app setting controller
+	 */
+	ngApp.provider.controller('ctrlPage', ['$scope', '$q', 'http2',function($scope, $q, http2) {
+		//nv是什么 frame.js 可能是传过来的 暂时自定义为true
+		//var nv = true;
+		$scope.$watch('wall', function(nv) {
+			if (nv) {
+				//$scope.url = 'http://' + location.host + '/rest/op/wall?mpid=' + $scope.mpaccount.mpid + '&wall=' + nv.id;
+				$scope.url = 'http://'+ location.host + '/rest/pl/fe/matter/wall?id=' + $scope.id + '&site=' + $scope.siteId;
+			}
+		});
+		//显示信息
+		$scope.pageTypes = [{
+			type: 'op',
+			name: '信息墙.大屏幕'
+		}];
+		http2.get('/rest/pl/fe/matter/wall/page/list?id=' + $scope.id + 'site=' + $scope.siteId, function(rsp) {
+			$scope.pages = {};
+			angular.forEach(rsp.data, function(page) {
+				$scope.pages[page.type] = page;
+			});
+		});
+		//去代码页面
+		$scope.gotoCode = function(page) {
+			window.open('/rest/code?pid=' + page.code_id, '_self');
+		};
+		//重置页面
+		$scope.resetCode = function(page) {
+			//console.log(page);
+			if (window.confirm('重置后将丢失已经做过的修改，确定操作？')) {
+				http2.get('/rest/pl/fe/matter/wall/page/reset?page=' + page.id, function(rsp) {
+					$scope.gotoCode(page);
+				});
+			}
+		}
+	}]);
+});
+
+
+
 //(function() {
 //	xxtApp.register.controller('pageCtrl', ['$scope', 'http2', function($scope, http2) {
 //		$scope.$parent.subView = 'page';
@@ -13,12 +56,15 @@
 //		});
 //		$scope.$watch('wall', function(nv) {
 //			if (nv) {
-//				$scope.url = 'http://' + location.host + '/rest/op/wall?mpid=' + $scope.mpaccount.mpid + '&wall=' + nv.id;
+//				//$scope.url = 'http://' + location.host + '/rest/op/wall?mpid=' + $scope.mpaccount.mpid + '&wall=' + nv.id;
+//				$scope.url = 'http://' + location.host + '/rest/pl/fe/matter/wall?id=' + $scope.id + '&site=' + $scope.site;
 //			}
 //		});
+//		//去代码页面
 //		$scope.gotoCode = function(page) {
 //			window.open('/rest/code?pid=' + page.code_id, '_self');
 //		};
+//		//重置页面
 //		$scope.resetCode = function(page) {
 //			if (window.confirm('重置后将丢失已经做过的修改，确定操作？')) {
 //				http2.get('/rest/mp/app/wall/page/reset?page=' + page.id, function(rsp) {
@@ -28,10 +74,3 @@
 //		}
 //	}]);
 //})();
-define(['frame'], function(ngApp) {
-	/**
-	 * app setting controller
-	 */
-	ngApp.provider.controller('ctrlDetail', ['$scope', '$q', 'http2',function($scope, $q, http2) {
-	}]);
-});
--- views/default/pl/fe/matter/wall/users.js
+++ views/default/pl/fe/matter/wall/users.js
@@ -1,11 +1,117 @@
+
+define(['frame'], function(ngApp) {
+    /**
+     * app setting controller
+     */
+    ngApp.provider.controller('ctrlUsers', ['$scope', '$q', 'http2','$uibModal',function($scope, $q, http2, $uibModal) {
+        //退出信息墙功能
+        $scope.quit = function() {
+            var vcode;
+            vcode = prompt('是否要退出所有在线用户？，若是，请输入讨论组名称。');
+            //下面需修改，与杨总讨论
+            if (vcode === $scope.wall.title) {
+                http2.get('/rest/mp/app/wall/users/quit?wall=' + $scope.wid, function(rsp) {
+                    $scope.users = null;
+                    $scope.$root.infomsg = '操作完成';
+                });
+            }
+        };
+        //导入用户
+        $scope.import = function() {
+            $uibModal.open({
+                templateUrl: 'importUser.html',
+                windowClass: 'auto-height',
+                controller: ['$scope', '$uibModalInstance', function($scope2, $mi) {
+                    http2.get('/rest/mp/app/enroll/list?page=1&size=999', function(rsp) {
+                        $scope2.apps = rsp.data[0];
+                    });
+                    $scope2.chooseApp = function(app) {
+                        $scope2.selectedApp = app;
+                    };
+                    $scope2.close = function() {
+                        $mi.dismiss();
+                    };
+                    $scope2.ok = function() {
+                        if ($scope2.selectedApp) {
+                            $mi.close($scope2.selectedApp);
+                        } else {
+                            $mi.dismiss();
+                        }
+                    };
+                }]
+            }).result.then(function(app) {
+                    http2.get('/rest/mp/app/wall/users/import?wall=' + $scope.wid + '&app=' + app.id, function(rsp) {
+                        $scope.$root.infomsg = '导入用户数：' + rsp.data;
+                        $scope.doSearch();
+                    });
+                });
+        };
+        //导出用户
+        $scope.export = function() {
+            $uibModal.open({
+                templateUrl: 'exportUser.html',
+                windowClass: 'auto-height',
+                controller: ['$scope', '$uibModalInstance', function($scope2, $mi) {
+                    $scope2.options = {
+                        onlySpeaker: 'N'
+                    };
+                    $scope2.chooseApp = function(app) {
+                        $scope2.selectedApp = app;
+                    };
+                    $scope2.close = function() {
+                        $mi.dismiss();
+                    };
+                    $scope2.ok = function() {
+                        if ($scope2.selectedApp) {
+                            $mi.close({
+                                app: $scope2.selectedApp,
+                                options: $scope2.options
+                            });
+                        } else {
+                            $mi.dismiss();
+                        }
+                    };
+                    http2.get('/rest/mp/app/enroll/list?page=1&size=999', function(rsp) {
+                        $scope2.apps = rsp.data[0];
+                    });
+                }]
+            }).result.then(function(params) {
+                    var url;
+                    url = '/rest/mp/app/wall/users/export?wall=' + $scope.wid;
+                    url += '&app=' + params.app.id;
+                    url += '&onlySpeaker=' + params.options.onlySpeaker;
+                    http2.get(url, function(rsp) {
+                        $scope.$root.infomsg = '导出用户数：' + rsp.data;
+                    });
+                });
+        };
+        //刷新
+        $scope.doSearch = function() {
+            http2.get('/rest/mp/app/wall/users/list?wall=' + $scope.wid, function(rsp) {
+                $scope.users = rsp.data;
+            });
+        };
+        $scope.doSearch();
+    }]);
+});
+
+//
+//
 //(function() {
 //    xxtApp.register.controller('usersCtrl', ['$scope', '$uibModal', 'http2', function($scope, $uibModal, http2) {
 //        $scope.$parent.subView = 'users';
-//        $scope.doSearch = function() {
-//            http2.get('/rest/mp/app/wall/users/list?wall=' + $scope.wid, function(rsp) {
-//                $scope.users = rsp.data;
-//            });
+//        //退出信息墙功能
+//        $scope.quit = function() {
+//            var vcode;
+//            vcode = prompt('是否要退出所有在线用户？，若是，请输入讨论组名称。');
+//            if (vcode === $scope.wall.title) {
+//                http2.get('/rest/mp/app/wall/users/quit?wall=' + $scope.wid, function(rsp) {
+//                    $scope.users = null;
+//                    $scope.$root.infomsg = '操作完成';
+//                });
+//            }
 //        };
+//        //导入用户
 //        $scope.import = function() {
 //            $uibModal.open({
 //                templateUrl: 'importUser.html',
@@ -29,12 +135,13 @@
 //                    };
 //                }]
 //            }).result.then(function(app) {
-//                http2.get('/rest/mp/app/wall/users/import?wall=' + $scope.wid + '&app=' + app.id, function(rsp) {
-//                    $scope.$root.infomsg = '导入用户数：' + rsp.data;
-//                    $scope.doSearch();
+//                    http2.get('/rest/mp/app/wall/users/import?wall=' + $scope.wid + '&app=' + app.id, function(rsp) {
+//                        $scope.$root.infomsg = '导入用户数：' + rsp.data;
+//                        $scope.doSearch();
+//                    });
 //                });
-//            });
 //        };
+//        //导出用户
 //        $scope.export = function() {
 //            $uibModal.open({
 //                templateUrl: 'exportUser.html',
@@ -64,32 +171,21 @@
 //                    });
 //                }]
 //            }).result.then(function(params) {
-//                var url;
-//                url = '/rest/mp/app/wall/users/export?wall=' + $scope.wid;
-//                url += '&app=' + params.app.id;
-//                url += '&onlySpeaker=' + params.options.onlySpeaker;
-//                http2.get(url, function(rsp) {
-//                    $scope.$root.infomsg = '导出用户数：' + rsp.data;
+//                    var url;
+//                    url = '/rest/mp/app/wall/users/export?wall=' + $scope.wid;
+//                    url += '&app=' + params.app.id;
+//                    url += '&onlySpeaker=' + params.options.onlySpeaker;
+//                    http2.get(url, function(rsp) {
+//                        $scope.$root.infomsg = '导出用户数：' + rsp.data;
+//                    });
 //                });
-//            });
 //        };
-//        $scope.quit = function() {
-//            var vcode;
-//            vcode = prompt('是否要退出所有在线用户？，若是，请输入讨论组名称。');
-//            if (vcode === $scope.wall.title) {
-//                http2.get('/rest/mp/app/wall/users/quit?wall=' + $scope.wid, function(rsp) {
-//                    $scope.users = null;
-//                    $scope.$root.infomsg = '操作完成';
-//                });
-//            }
+//        //刷新
+//        $scope.doSearch = function() {
+//            http2.get('/rest/mp/app/wall/users/list?wall=' + $scope.wid, function(rsp) {
+//                $scope.users = rsp.data;
+//            });
 //        };
 //        $scope.doSearch();
 //    }]);
 //})();
-define(['frame'], function(ngApp) {
-    /**
-     * app setting controller
-     */
-    ngApp.provider.controller('ctrlUsers', ['$scope', '$q', 'http2',function($scope, $q, http2) {
-    }]);
-});

0d5c58d1b7a8775536b49ef2370706dc4888a1ff
lishuai <lszgf_li@163.com>
2016-07-28 14:24:53 +0800

    'page'

--- views/default/pl/fe/matter/wall/page.js
+++ views/default/pl/fe/matter/wall/page.js
@@ -7,11 +7,11 @@ define(['frame'], function(ngApp) {
 		//nv是什么 frame.js 可能是传过来的 暂时自定义为true
 		//var nv = true;
 		$scope.$parent.subView = 'page';
-		//$scope.$watch('wall', function(nv) {
-		//	if (nv) {
-		//		//$scope.url = 'http://' + location.host + '/rest/op/wall?mpid=' + $scope.mpaccount.mpid + '&wall=' + nv.id;
-		//	}
-		//});
+		$scope.$watch('wall', function(nv) {
+			if (nv) {
+				$scope.url = 'http://' + location.host + '/rest/op/wall?mpid=' + $scope.mpaccount.mpid + '&wall=' + nv.id;
+			}
+		});
 		//显示信息
 		$scope.pageTypes = [{
 			type: 'op',

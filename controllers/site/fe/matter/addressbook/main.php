64450768181a1efe0638cc8d45ed972ebdee5678
lt <lqqllt@163.com>
2016-08-03 09:41:00 +0800

    app\matter\addressbook.php迁移到 site\fe\matter\addressbook目录下

---
+++ controllers/site/fe/matter/addressbook/main.php
@@ -0,0 +1,58 @@
+<?php
+namespace site\fe\matter\addressbook;
+
+include_once dirname(dirname(__FILE__)) . '/base.php';
+/**
+ * 讨论组 
+ */
+class main extends \member_base {
+    /**
+     *
+     */
+    public function get_access_rule() 
+    {
+        $rule_action['rule_type'] = 'black';
+        $rule_action['actions'] = array();
+
+        return $rule_action;
+    }
+    /**
+     *
+     */
+    public function index_action($siteid, $id)
+    {
+        $this->view_action('/site/fe/matter/addressbook/index');
+    }
+    /**
+     * 通讯录查询
+     *
+     * $siteid
+     * $abid
+     * $abbr
+     * $page
+     * $size
+     */
+    public function get_action($siteid, $abid, $abbr='', $deptid=null, $page=1, $size=20)
+    {
+        $model = $this->model('matter\addressbook');
+        
+        $rst = $model->searchPersons($siteid, $abid, $abbr, $deptid, $page, $size);
+
+        return new \ResponseData($rst);
+    }
+    /**
+     *
+     */
+    public function deptGet_action($siteid, $id) 
+    {
+        $q = array(
+            'id,name,pid',
+            'xxt_ab_dept',
+            "ab_id='$id'"
+        );
+        
+        $depts = $this->model()->query_objs_ss($q);
+
+        return new \ResponseData($depts);
+    }
+}

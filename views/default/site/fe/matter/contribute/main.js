8a22c4bf807f0bb98122ca013205d1f415484ce5
lishuai <lszgf_li@163.com>
2016-08-23 09:01:07 +0800

    'contribute'

--- views/default/site/fe/matter/contribute/main.js
+++ views/default/site/fe/matter/contribute/main.js
@@ -7,6 +7,32 @@ ngApp.controller('ctrlContribute', ['$scope', '$location', 'http2', function($sc
     $scope.appId = $location.search().app;
     http2.get('/rest/site/fe/matter/contribute/entry/list?site=' + $scope.siteId + '&app=' + $scope.appId, function(rsp) {
         $scope.entries = rsp.data.entries;
+        //mysef
+        var setMpShare = function(xxtShare) {
+            var shareid, sharelink;
+            //shareid = $scope.user.uid + (new Date()).getTime();
+            xxtShare.options.logger = function(shareto) {
+                /*var url = "/rest/mi/matter/logShare";
+                 url += "?shareid=" + shareid;
+                 url += "&site=" + siteId;
+                 url += "&id=" + id;
+                 url += "&type=article";
+                 url += "&title=" + $scope.article.title;
+                 url += "&shareto=" + shareto;
+                 //url += "&shareby=" + shareby;
+                 $http.get(url);*/
+            };
+            //∑÷œÌ¡¥Ω”
+            //sharelink = 'http://' + location.hostname + '/rest/site/fe/matter';
+            //sharelink += '?site=' + siteId;
+            //sharelink += '&type=article';
+            //sharelink += '&id=' + id;
+            //sharelink += "&shareby=" + shareid;
+            //???
+            xxtShare.set($scope.entries[0].title, sharelink,$scope.entries[0].summary,$scope.entries[0].pic);
+
+        };
+
     })
     $scope.initiate = function(entry) {
         var url = '/rest/site/fe/matter/contribute/initiate';

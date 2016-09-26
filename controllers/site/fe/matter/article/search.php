48873c7116bf0457ebbb1b6791b46969469cb2bb
lt <lqqllt@163.com>
2016-09-26 09:45:36 +0800

    格式对齐 Signed-off-by: lt <lqqllt@163.com>

--- controllers/site/fe/matter/article/search.php
+++ controllers/site/fe/matter/article/search.php
@@ -8,7 +8,7 @@ include_once dirname(dirname(__FILE__)) . '/base.php';
 */
 class search extends \site\fe\matter\base{
 	/**
-	 *
+	 * 搜索页面首页
 	 */
 	public function index_action() {
 		\TPL::output('/matter/article-list');
@@ -17,7 +17,7 @@ class search extends \site\fe\matter\base{
 	/*
 	 * 返回所有的搜索结果
 	 */
-	 public function list_action($site,$keyword='') {
+	public function list_action($site,$keyword='') {
 		$matters = \TMS_APP::model('matter\article')->search_all($site, $keyword);	
 		return new \ResponseData($matters);
 	}

70ace077783065f2dd617e2d6b7df88bad521fde
aYellowApple <2764411477@qq.com>
2016-09-27 10:16:41 +0800

    解决地址栏参数更换问题

--- views/default/site/fe/matter/article/list.js
+++ views/default/site/fe/matter/article/list.js
@@ -6,9 +6,9 @@ if (/MicroMessenger/.test(navigator.userAgent)) {
 angular.module('xxt', ['infinite-scroll']).config(['$locationProvider', function($locationProvider) {
     $locationProvider.html5Mode(true);
 }]).controller('ctrl', ['$scope', '$location', '$http', '$q', function($scope, $location, $http, $q) {
-    var siteId, channelId, shareby;
+    var siteId, channelId, shareby,keyWord;
     /*关键词*/
-    keyword = $location.search().keyword;
+    keyWord = $location.search().keyword;
     siteId = $location.search().site;
     channelId = $location.search().id;
     shareby = $location.search().shareby ? $location.search().shareby : '';
@@ -34,7 +34,7 @@ angular.module('xxt', ['infinite-scroll']).config(['$locationProvider', function
         window.xxt.share.set($scope.channel.title, sharelink, $scope.channel.title, '');
     };
     /*输入框需要绑定的内容*/
-    $scope.searchKeyword = keyword;
+    $scope.searchKeyword = keyWord;
     /*重置输入框*/
     $scope.reset = function() {
         $scope.searchKeyword = '';
@@ -47,62 +47,20 @@ angular.module('xxt', ['infinite-scroll']).config(['$locationProvider', function
         }
         return arr.join('/');
     }
-    $http.get('').success(rsp){
+    $http.get('/rest/site/fe/matter/article/search/list?site=' + siteId + '&keyword=' + keyWord).success(function(rsp){
         $scope.matters = rsp.data;
-    }
+    });
     $scope.search = function () {
-        $http.post('').success(rsp){
-            $scope.matters = rsp.data;
-        }
-    }
-    $scope.open = function(opened) {
-        location.href = opened.url;
-    };
-    var getChannel = function() {
-        var deferred = $q.defer();
-        $http.get('/rest/site/fe/matter/channel/get?site=' + siteId + '&id=' + channelId).success(function(rsp) {
-            $scope.user = rsp.data.user;
-            $scope.channel = rsp.data.channel;
-            if (/MicroMessenge|Yixin/i.test(navigator.userAgent)) {
-                setShare();
-            }
-            deferred.resolve();
-            if ($scope.Matter.matters) {
-                // 任意类型素材情况下不支持分页
-                if ($scope.channel.matter_type === '') {
-                    $scope.Matter.matters.end = true;
-                }
-            }
-            $http.post('/rest/site/fe/matter/logAccess?site=' + siteId + '&id=' + channelId + '&type=channel&title=' + $scope.channel.title + '&shareby=' + shareby, {
-                search: location.search.replace('?', ''),
-                referer: document.referrer
-            });
-        }).error(function(content, httpCode) {
-            if (httpCode === 401) {
-                var el = document.createElement('iframe');
-                el.setAttribute('id', 'frmAuth');
-                el.onload = function() {
-                    this.height = document.documentElement.clientHeight;
-                };
-                document.body.appendChild(el);
-                if (content.indexOf('http') === 0) {
-                    window.onAuthSuccess = function() {
-                        el.style.display = 'none';
-                        getChannel();
-                    };
-                    el.setAttribute('src', content);
-                    el.style.display = 'block';
-                } else {
-                    if (el.contentDocument && el.contentDocument.body) {
-                        el.contentDocument.body.innerHTML = content;
-                        el.style.display = 'block';
-                    }
-                }
-            } else {
-                alert(content);
+        $location.search('keyword',$scope.searchKeyword);
+        $http.post('/rest/site/fe/matter/article/search/list?site=' + siteId + '&keyword=' + $scope.searchKeyword).success(function(rsp){
+            if(rsp.data.length){
+                $scope.matters = rsp.data;
+            }else {
+                angular.element('#content').html('<p>没有值</p>');
             }
         });
-        return deferred.promise;
+    }
+    $scope.open = function(opened) {
+        location.href = '/rest/site/fe/matter?site='+ siteId + '&id=' + opened.id +'&type=article';
     };
-    getChannel();
 }]);

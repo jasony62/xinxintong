33192e002e6fbe3d31b3b2ca4960e2e5de2f455b
lt <lqqllt@163.com>
2016-10-10 16:44:58 +0800

    替换搜索头图 修改一些bug Signed-off-by: lt <lqqllt@163.com>

--- models/matter/article.php
+++ models/matter/article.php
@@ -302,7 +302,7 @@ class article_model extends article_base {
 		$articles=json_encode($articles);
 		$articles=json_decode($articles,1);
 		//内容标签
-		$q3="select * from xxt_article_tag t left join xxt_tag g on t.tag_id=g.id where t.mpid='$site' ";	
+		$q3="select * from xxt_article_tag t left join xxt_tag g on t.tag_id=g.id where t.mpid='$site' and t.sub_type=0";	
 		$tag_content=parent::query_objs($q3);
 
 		//频道标签

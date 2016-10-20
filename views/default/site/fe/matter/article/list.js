6640786bd53f5fd5e443beb6d7ebeea96f26f831
aYellowApple <2764411477@qq.com>
2016-10-20 09:21:36 +0800

    解决闪烁的问题

--- views/default/site/fe/matter/article/list.js
+++ views/default/site/fe/matter/article/list.js
@@ -3,9 +3,10 @@ if (/MicroMessenger/.test(navigator.userAgent)) {
     signPackage.jsApiList = ['hideOptionMenu', 'onMenuShareTimeline', 'onMenuShareAppMessage'];
     wx.config(signPackage);
 }
-angular.module('xxt', ['infinite-scroll']).config(['$locationProvider', function($locationProvider) {
+var app = angular.module('xxt', ['infinite-scroll']).config(['$locationProvider', function($locationProvider) {
     $locationProvider.html5Mode(true);
-}]).controller('ctrl', ['$scope', '$location', '$http', '$q', function($scope, $location, $http, $q) {
+}]);
+app.controller('ctrl', ['$scope', '$location', '$http', '$q', function($scope, $location, $http, $q) {
     var siteId, channelId, shareby, keyWord;
     /*关键词*/
     keyWord = $location.search().keyword;
@@ -35,16 +36,22 @@ angular.module('xxt', ['infinite-scroll']).config(['$locationProvider', function
     };
     /*输入框需要绑定的内容*/
     $scope.searchKeyword = keyWord;
-    /*标签展示*/
-    /*$scope.transform = function(obj) {
-        var arr = [];
-        for (var item in obj) {
-            arr.push(obj[item].title);
-        }
-        return arr.join('/');
-    }*/
+
+    var deferred = $q.defer();
+    var promise = deferred.promise;
     $http.get('/rest/site/fe/matter/article/search/list?site=' + siteId + '&keyword=' + keyWord).success(function(rsp) {
-        $scope.matters = rsp.data;
+        if (rsp.data.length) {
+            deferred.resolve(rsp.data);
+        } else {
+            deferred.reject("很抱歉，未找到与此相关的文章。");
+        }
+    });
+    promise.then(function(result) {
+        $scope.matters = result;
+    }, function(error) {
+        $scope.matters = [];
+        $scope.message = error;
+        return $scope.message;
     });
     $scope.keypress = function(event) {
         if (event.keyCode == 13) {
@@ -52,13 +59,21 @@ angular.module('xxt', ['infinite-scroll']).config(['$locationProvider', function
         }
     }
     $scope.search = function() {
+        var deferred = $q.defer();
+        var promise = deferred.promise;
         $http.post('/rest/site/fe/matter/article/search/list?site=' + siteId + '&keyword=' + $scope.searchKeyword).success(function(rsp) {
             if (rsp.data.length) {
-                $scope.matters = rsp.data;
+                deferred.resolve(rsp.data);
             } else {
-                $scope.matters = [];
-                $scope.word = $scope.searchKeyword;
+                deferred.reject("很抱歉，未找到与此相关的文章。");
             }
+            promise.then(function(result) {
+                $scope.matters = result;
+            }, function(error) {
+                $scope.matters = [];
+                $scope.message = error;
+                return $scope.message;
+            });
         });
     }
     $scope.open = function(opened) {

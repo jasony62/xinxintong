455914686f7c9589766ca6e81c98404ab2a17042
liao1a <153887111@qq.com>
2016-11-11 16:05:28 +0800

    修改表数据

--- schema/update/update2016110412.php
+++ schema/update/update2016110412.php
@@ -7,13 +7,13 @@ require_once '../../db.php';
 $sql = "create table if not exists xxt_log_sync(";//企业号同步表
 $sql .= "id int not null auto_increment";
 $sql .= ",siteid varchar(32) not null";
-$sql .= ",sync_at int not null"; // 与公众号最后一次同步时间
+$sql .= ",sync_at int not null default 0 "; // 与公众号最后一次同步时间
 $sql .= ",type varchar(20) not null default ''";//同步类型
 $sql .= ",userid varchar(40) not null default ''";
-$sql .= ",creater varchar(255) not null default ''";//同步者
-$sql .= ",sync_type varchar(20) not null default ''";//同步属性
-$sql .= ",sync_table varchar(20) not null default ''";//同步对应的表
-$sql .= ",sync_id varchar(20) not null default ''";//同步对应表的id
+$sql .= ",creater varchar(50) not null default ''";//同步者
+$sql .= ",sync_type varchar(50) not null default ''";//同步属性
+$sql .= ",sync_table varchar(50) not null default ''";//同步对应的表
+$sql .= ",sync_id int not null default 0 ";//插入同步表的id
 $sql .= ",sync_data text ";//同步数据
 $sql .= ",primary key(id)";
 $sql .= ")ENGINE=MyISAM DEFAULT CHARSET=utf8";

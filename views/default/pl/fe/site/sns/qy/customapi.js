f5555ab387251be09a81291326da543f05a6ea02
lishuai <1076283698@qq.com>
2016-11-17 09:47:40 +0800

    'clearNote'

--- views/default/pl/fe/site/sns/qy/customapi.js
+++ views/default/pl/fe/site/sns/qy/customapi.js
@@ -1,21 +1,17 @@
 define(['main'], function(ngApp) {
     'use strict';
     ngApp.provider.controller('ctrlCustomapi', ['$scope', 'http2', function ($scope, http2) {
-        //初始值
-        $scope.page = {//分页
+        $scope.page = {
             at: 1,
             size: 5
         };
         //同步属性
-        $scope.type = 'syncFromQy';//企业号日志查询
-        $scope.syncType = 'department';//部门状态
-        //切换日志方法
+        $scope.type = 'syncFromQy';
+        $scope.syncType = 'department';
         $scope.changeNote = function(type){
             $scope.type = type;
             $scope.doSearch(1);
         };
-        //获取日志
-        //接口 /rest/site/fe/user/member/syncLog' + ?site=' + $scope.siteId + '&type=' + $scope.type  + '&page=' + $scope.page.at +'&size=' + $scope.page.size
         $scope.doSearch = function (page, syncType) {
             var url = '/rest/site/fe/user/member/syncLog';
             page && ($scope.page.at = page );
@@ -31,8 +27,7 @@ define(['main'], function(ngApp) {
                 $scope.page.total = rsp.data.total;
             });
         };
-        //同步方法
-        //同步接口 /rest/site/fe/user/member/syncFromQy + '?site=' + $scope.siteId +&authid=' + 0
+
         $scope.syn = function () {
             var url = '/rest/site/fe/user/member/';
             url += $scope.type;
@@ -42,7 +37,6 @@ define(['main'], function(ngApp) {
                 if (rsp.err_code == 0) {
                     alert("同步" + rsp.data[0] + "个部门，" + rsp.data[1] + "个用户，" + rsp.data[2] + "个标签");
                     //$scope.$root.progmsg = "同步" + rsp.data[0] + "个部门，" + rsp.data[1] + "个用户，" + rsp.data[2] + "个标签";
-
                 }
                 $scope.doSearch(1);
             });

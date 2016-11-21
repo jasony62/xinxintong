d5f215e11036182530e49b59e5d9d693f55dd3c2
lishuai <1076283698@qq.com>
2016-11-21 10:19:10 +0800

    'entry_rule'

--- views/default/pl/fe/matter/enroll/publish.js
+++ views/default/pl/fe/matter/enroll/publish.js
@@ -127,9 +127,8 @@ define(['frame'], function(ngApp) {
 	 */
 	ngApp.provider.controller('ctrlAccessRule', ['$scope', 'http2', 'srvApp', function($scope, http2, srvApp) {
 		$scope.rule = {};
-		$scope.update = function(entry_rule_id) {
-			//srvApp.update('entry_rule');
-			srvApp.update(entry_rule_id);
+		$scope.update = function(entry_rule) {
+			srvApp.update(entry_rule);
 		};
 		$scope.reset = function() {
 			srvApp.resetEntryRule();

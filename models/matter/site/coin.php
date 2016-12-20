aa60b4bf369d812bab749550896523c8de064f7f
liao1a <153887111@qq.com>
2016-12-20 14:56:21 +0800

    修改站点注册用户的积分逻辑

--- models/matter/site/coin.php
+++ models/matter/site/coin.php
@@ -10,33 +10,9 @@ class coin_model extends \TMS_MODEL {
 	 * @param object $siteConfig
 	 */
 	public function &rulesByMatter($act, $siteConfig) {
-		$q = ['*', 'xxt_coin_rule', "matter_type='site' and act='$act' and "];
-
-		$w = "(";
-		$w .= "matter_filter='*'";
-		$w .= "or matter_filter='ID:{$siteConfig->id}'";
-		isset($siteConfig->entry) && $w .= "or matter_filter='ENTRY:{$siteConfig->entry}'";
-		$w .= ")";
-
-		$q[2] .= $w;
-
+		$q = ['*', 'xxt_coin_rule', "matter_type='site' and act='$act' and matter_filter='ID:{$siteConfig->id}'"];
 		$rules = $this->query_objs_ss($q);
 
 		return $rules;
-	}
-	/**
-	 * 返回用于记录积分的文章创建人
-	 *
-	 * @param object $siteConfig
-	 */
-	public function getCreator($siteConfig) {
-		$creator = false;
-		if (isset($siteConfig->creater_src) && $siteConfig->creater_src === 'M') {
-			if ($member = $this->model('site\user\member')->byId($siteConfig->creater)) {
-				$creator = $this->model('site\user\account')->byId($member->userid);
-			}
-		}
-
-		return $creator;
 	}
 }

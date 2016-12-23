279e0fc841bda67e5a32105d4d2805c71d16fc18
liao1a <153887111@qq.com>
2016-12-23 13:44:23 +0800

    修改在登记数据中手工添加获取openid的情况

--- models/matter/enroll/record.php
+++ models/matter/enroll/record.php
@@ -65,11 +65,13 @@ class record_model extends \TMS_MODEL {
 			}
 		}
 
-		$userOpenids = $this->model('site\user\account')->byId($user->uid,array('fields'=>'wx_openid,yx_openid,qy_openid'));
-		if($userOpenids){
-			$record['wx_openid'] = $userOpenids->wx_openid;
-			$record['yx_openid'] = $userOpenids->yx_openid;
-			$record['qy_openid'] = $userOpenids->qy_openid;
+		if(!empty($user)){
+			$userOpenids = $this->model('site\user\account')->byId($user->uid,array('fields'=>'wx_openid,yx_openid,qy_openid'));
+			if($userOpenids){
+				$record['wx_openid'] = $userOpenids->wx_openid;
+				$record['yx_openid'] = $userOpenids->yx_openid;
+				$record['qy_openid'] = $userOpenids->qy_openid;
+			}
 		}
 
 		$this->insert('xxt_enroll_record', $record, false);

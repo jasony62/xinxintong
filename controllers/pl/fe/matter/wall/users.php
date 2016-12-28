fed0d893204fdf8c1f5f55743d071e58e2617c71
liao1a <153887111@qq.com>
2016-12-28 09:54:59 +0800

    1

--- controllers/pl/fe/matter/wall/users.php
+++ controllers/pl/fe/matter/wall/users.php
@@ -211,6 +211,15 @@ class users extends \pl\fe\matter\base {
 		 * 清除所有加入的人
 		 */
 		$rst = $this->model()->delete('xxt_wall_enroll', "wid='$id'");
+		/**
+		*解除关联活动
+		*/
+
+		$modelWall->update(
+				'xxt_wall',
+				array('data_schemas' => '','source_app' => ''),
+				"id='{$id}'"
+			);
 
 		return new \ResponseData($rst);
 	}

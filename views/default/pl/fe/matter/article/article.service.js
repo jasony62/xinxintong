48d611e65166f042b10cd596420fe1ef69bf410a
Truth <2764411477@qq.com>
2017-01-03 16:35:52 +0800

    Issue log (#92)
    * add cus and kcfinder/cus folder
    
    * move addressbook folder to fe
    
    * recover master
    
    * delete file
    
    * 恢复无关代码
    
    * 去除多余问号
    
    * 修改Log页面分页的样式
    
    * 完成coin页面的分页逻辑
    
    * 撤出log页面的样式修改
    
    * 完成代码拆分

--- views/default/pl/fe/matter/article/article.service.js
+++ views/default/pl/fe/matter/article/article.service.js
@@ -125,4 +125,33 @@ angular.module('service.article', ['ui.bootstrap', 'ui.xxt']).provider('srvLog',
             }
         };
     }];
+}).provider('srvCoin',function(){
+    this.$get = ['$q', 'http2', function($q, http2) {
+        return {
+            list: function(articleSiteId, articleId, page) {
+                var defer = $q.defer(),
+                    url;
+                if (!page || !page._j) {
+                    angular.extend(page, {
+                        at: 1,
+                        size: 30,
+                        orderBy: 'time',
+                        _j: function() {
+                            var p;
+                            p = '&page=' + this.at + '&size=' + this.size;
+                            p += '&orderby=' + this.orderBy;
+                            return p;
+                        }
+                    });
+                }
+                url = '/rest/pl/fe/matter/article/coin/logs?site='+ articleSiteId + '&id=' + articleId + page._j();
+                http2.get(url, function(rsp) {
+                    rsp.data.total && (page.total = rsp.data.total);
+                    defer.resolve(rsp.data.logs);
+                });
+
+                return defer.promise;
+            }
+        };
+    }];
 });

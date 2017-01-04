2919a0ec6594ea4e082b7c5bb0683424315a292b
aYellowApple <2764411477@qq.com>
2017-01-04 16:03:18 +0800

    修改参数位置

--- views/default/pl/fe/site/message/received.js
+++ views/default/pl/fe/site/message/received.js
@@ -1,17 +1,17 @@
 define(['main'], function(ngApp) {
-    ngApp.provider.controller('ctrlUser', ['$scope', 'http2', function($scope, http2) {
+    ngApp.provider.controller('ctrlReceived', ['$scope', 'http2', function($scope, http2) {
         $scope.page = { current: 1, size: 30, keyword: '' };
         $scope.doSearch = function(page) {
             if (page) {
                 $scope.page.current = page;
             } else
                 page = $scope.page.current;
-            var param = '?page=' + page + '&size=' + $scope.page.size;
+            var param = '?page=' + page + '&size=' + $scope.page.size + '&site=' + $scope.siteId;
             if ($scope.page.keyword && $scope.page.keyword.length > 0)
-                param += '&keyword=' + $scope.page.keyword += '&site=' + $scope.siteId;
-            http2.get('/rest/pl/fe/site/user/messages/get' + param, function(rsp) {
+                param += '&keyword=' + $scope.page.keyword;
+            http2.get('/rest/pl/fe/site/message/get' + param, function(rsp) {
                 $scope.messages = rsp.data[0];
-                rsp.data[1] && ($scope.page.total = rsp.data[1]);
+                $scope.page.total = rsp.data[1];
             });
         };
         $scope.viewUser = function(openid) {

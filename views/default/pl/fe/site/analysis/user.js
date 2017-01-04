c8caea8755d03aa0ef7729a733c9d9648c31bb78
aYellowApple <2764411477@qq.com>
2017-01-04 08:43:20 +0800

    用户行为分析代码迁移完成

--- views/default/pl/fe/site/analysis/user.js
+++ views/default/pl/fe/site/analysis/user.js
@@ -1,3 +1,34 @@
 define(['main'], function(ngApp) {
-    ngApp.provider.controller('ctrlUser', ['$scope', 'http2', function($scope, http2) {}]);
+    ngApp.provider.controller('ctrlUser', ['$scope', 'http2', function($scope, http2) {
+        $scope.page = {
+            at: 1,
+            size: 30,
+            total: 0,
+            param: function() {
+                return 'page=' + this.at + '&size=' + this.size;
+            }
+        };
+        $scope.orderby = 'read';
+        $scope.fetch = function(page) {
+            var url;
+            page && ($scope.page.at = page);
+            url = '/rest/pl/fe/site/analysis/userActions';
+            url += '?orderby=' + $scope.orderby;
+            url += '&startAt=' + $scope.startAt;
+            url += '&endAt=' + $scope.endAt;
+            url += '&' + $scope.page.param();
+            http2.get(url, function(rsp) {
+                $scope.users = rsp.data.users;
+                $scope.page.total = rsp.data.total;
+            });
+        };
+        $scope.viewUser = function(openid) {
+            location.href = '/rest/pl/fe/site/user?openid=' + openid;
+        };
+        $scope.$on('xxt.tms-datepicker.change', function(event, data) {
+            $scope[data.state] = data.value;
+            $scope.fetch(1);
+        });
+        $scope.fetch(1);
+    }]);
 });

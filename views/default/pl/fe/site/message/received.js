6e5a7a05eabf1999834e689f4852446f3d18408c
aYellowApple <2764411477@qq.com>
2017-01-04 15:13:00 +0800

    解决未被追踪的文件问题

---
+++ views/default/pl/fe/site/message/received.js
@@ -0,0 +1,22 @@
+define(['main'], function(ngApp) {
+    ngApp.provider.controller('ctrlUser', ['$scope', 'http2', function($scope, http2) {
+        $scope.page = { current: 1, size: 30, keyword: '' };
+        $scope.doSearch = function(page) {
+            if (page) {
+                $scope.page.current = page;
+            } else
+                page = $scope.page.current;
+            var param = '?page=' + page + '&size=' + $scope.page.size;
+            if ($scope.page.keyword && $scope.page.keyword.length > 0)
+                param += '&keyword=' + $scope.page.keyword += '&site=' + $scope.siteId;
+            http2.get('/rest/pl/fe/site/user/messages/get' + param, function(rsp) {
+                $scope.messages = rsp.data[0];
+                rsp.data[1] && ($scope.page.total = rsp.data[1]);
+            });
+        };
+        $scope.viewUser = function(openid) {
+            location.href = '/rest/mp/user?openid=' + openid;
+        };
+        $scope.doSearch();
+    }]);
+});

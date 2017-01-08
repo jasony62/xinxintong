505eb6fd54f13974b16669d1cb80112cb73176d3
aGreenApple <2764411477@qq.com>
2017-01-08 11:54:41 +0800

    添加wall文件夹

---
+++ views/default/site/fe/matter/wall/index.js
@@ -0,0 +1,19 @@
+if (/MicroMessenger/.test(navigator.userAgent)) {
+    signPackage.jsApiList = ['hideOptionMenu', 'onMenuShareTimeline', 'onMenuShareAppMessage'];
+    wx.config(signPackage);
+}
+var app = angular.module('xxt', []);
+app.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+}]);
+app.controller('wallCtrl',['$scope','$http','$location', function($scope,$http,$location){
+  var ls = $location.search();
+    $scope.id = ls.app;
+    $scope.siteId = ls.siteid;
+  $scope.open = function(wall) {
+      location.href = '/rest/site/fe/matter/wall/main?site=' + $scope.siteId + '&app=' + wall.id;
+  };
+  $http.get('/rest/site/fe/matter/wall/wallList?site=' + $scope.siteId + '&app=' + $scope.id).success(function(rsp){
+      $scope.walls = rsp.data;
+  })
+}]);

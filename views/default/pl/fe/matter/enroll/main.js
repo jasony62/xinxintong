a9c83f46cf3264a50378f02890ce231a9b432644
aYellowApple <2764411477@qq.com>
2017-01-10 09:45:28 +0800

    登记活动管理员添加完成

--- views/default/pl/fe/matter/enroll/main.js
+++ views/default/pl/fe/matter/enroll/main.js
@@ -1,7 +1,7 @@
 define(['frame'], function(ngApp) {
     'use strict';
     ngApp.provider.controller('ctrlMain', ['$scope', 'http2', function($scope, http2) {}]);
-    ngApp.provider.controller('ctrlReceiver', ['$scope', 'http2', '$interval', function($scope, http2, $interval) {
+    ngApp.provider.controller('ctrlReceiver', ['$scope', 'http2', '$interval', '$uibModal', function($scope, http2, $interval, $uibModal) {
         var baseURL = '/rest/pl/fe/matter/enroll/receiver/';
         $scope.qrcodeShown = false;
         $scope.qrcode = function(snsName) {
@@ -54,6 +54,74 @@ define(['frame'], function(ngApp) {
                 $scope.receivers.splice($scope.receivers.indexOf(receiver), 1);
             });
         };
+        $scope.choose = function() {
+            $uibModal.open({
+                 templateUrl: 'chooseUser.html',
+                 controller: 'chooseUserCtrl',
+            }).result.then(function(data){
+                var url;
+                url = '/rest/pl/fe/matter/enroll/receiver/add';
+                url += '?site=' + $scope.siteId;
+                url += '&id=' + $scope.id;
+                http2.post(url, data, function(rsp){
+                    $scope.receivers = rsp.data;
+                });
+            })
+        };
+        ngApp.provider.controller('chooseUserCtrl', ['$scope', '$uibModalInstance', '$location', 'http2', function($scope, $mi, $location, http2) {
+            var ls = $location.search();
+            $scope.id = ls.id;
+            $scope.siteId = ls.site;
+            $scope.page = {
+                at: 1,
+                size: 15,
+                total: 0,
+                param: function() {
+                    return 'page=' + this.at + '&size=' + this.size;
+               }
+            };
+            $scope.doSearch = function(page){
+                var url;
+                page && ($scope.page.at = page);
+                url = '/rest/pl/fe/matter/enroll/receiver/qymem';
+                url += '?site=' + $scope.siteId;
+                url += '&' + $scope.page.param();
+                http2.get(url, function(rsp) {
+                    $scope.users = rsp.data.data;
+                    $scope.page.total = rsp.data.total;
+                });
+            }
+            $scope.isSelected = function(id){
+                return $scope.selected.indexOf(id)>=0;
+            }
+            $scope.selected = [];
+            var updateSelected = function(action,option){
+                if(action == 'add' && $scope.selected.indexOf(option) == -1){
+                    $scope.selected.push(option);
+
+                }
+                if(action == 'remove' && $scope.selected.indexOf(option)!=-1){
+                    var idx = $scope.selected.indexOf(option);
+                    $scope.selected.splice(idx,1);
+                }
+            }
+            $scope.updateSelection = function($event, data){
+                var checkbox = $event.target;
+                var action = (checkbox.checked ? 'add':'remove');
+                var option = {
+                    nickname:data.nickname,
+                    uid:data.userid
+                };
+                updateSelected(action,option);
+            }
+            $scope.ok = function() {
+                $mi.close($scope.selected);
+            };
+            $scope.cancel = function() {
+                $mi.dismiss();
+            };
+            $scope.doSearch(1);
+        }]);
         http2.get(baseURL + 'list?site=' + $scope.siteId + '&app=' + $scope.id, function(rsp) {
             $scope.receivers = rsp.data;
         });

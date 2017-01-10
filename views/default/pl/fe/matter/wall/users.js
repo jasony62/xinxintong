a4299bbd5e217ca927767062b7bceeab8707fede
aYellowApple <2764411477@qq.com>
2017-01-10 15:20:09 +0800

    首次完成导入微企用户

--- views/default/pl/fe/matter/wall/users.js
+++ views/default/pl/fe/matter/wall/users.js
@@ -78,55 +78,88 @@ define(['frame'], function(ngApp) {
                 controller: ['$scope', '$uibModalInstance', function($scope2, $mi) {
                     $scope2.data = {
                         app : '',
-                        appType : 'wx'
-                    } ;
-                    $scope2.$watch('data.appType',function(newValus){
-                        var url ;
-                        $scope.syncType = 'department';
-                        if(newValus === 'wx') {
-                            url = '/rest/pl/fe/site/sns/wx/user/list?page=1&size=999&site=' + $scope.siteId;
-                        }else if(newValus === 'yx') {
-                            url = '/rest/pl/fe/site/sns/yx/user/list?page=1&size=999&site=' + $scope.siteId;
-                        }else if(newValus === 'qy') {
-                            url = '/rest/site/fe/user/member/syncLog?site=' + $scope.siteId + '&type=syncFromQy' + '&page=1&size=999';
-                        }
-                        if(newValus == 'qy'){
-                            http2.post(url,{syncType:$scope.syncType},function(rsp){
-                                $scope2.apps = rsp.data.apps;
-                            })
-                        }else{
+                        appType : 'qy'
+                    };
+                    $scope2.page = {
+                        at: 1,
+                        size: 2,
+                        total: 0,
+                        param: function() {
+                            return 'page=' + this.at + '&size=' + this.size;
+                       }
+                    };
+                    $scope2.selected = [];
+                    $scope2.doRequest = function(page){
+                        page && ($scope2.page.at = page);
+                        $scope2.$watch('data.appType',function(newValus){
+                            var url ;
+                            if(newValus === 'wx') {
+                                url = '/rest/pl/fe/site/sns/wx/user/list?site=' + $scope.siteId + '&' + $scope2.page.param();
+                            }else if(newValus === 'yx') {
+                                url = '/rest/pl/fe/site/sns/yx/user/list?site=' + $scope.siteId + '&' + $scope2.page.param();
+                            }else if(newValus === 'qy') {
+                                url = '/rest/pl/fe/matter/wall/users/importSns?site=' + $scope.siteId + '&type=qy' + '&' + $scope2.page.param();
+                            }
                             http2.get(url , function(rsp) {
-                                $scope2.apps = rsp.data.data;
+                                $scope2.publicUsers = rsp.data.data;
+                                $scope2.page.total = rsp.data.total;
                             });
+                        });
+                    }
+                    if(!$scope2.depet){
+                        $scope2.doRequest(1);
+                    }else{
+                        $scope.updateInput = function($event,data){
+                            $scope2.doRequest = function(page){
+                                var url;
+                                url = '/rest/pl/fe/matter/wall/users/importSns?site=' + $scope.siteId;
+                                url += '&type=qy' + '&' + $scope2.page.param();
+                                http2.post(url,{dept:data},function(rsp){
+                                    $scope.publicUsers = rsp.data.data;
+                                    $scope.page.total = rsp.data.total;
+                                })
+                            }
+                            $scope2.doRequest(1);
                         }
-
-                    });
-
+                    }
+                    $scope2.isSelected = function(id){
+                        return $scope.selected.indexOf(id)>=0;
+                    }
+                    var updateSelected = function(action,option){
+                        if(action == 'add' && $scope.selected.indexOf(option) == -1){
+                            $scope.selected.push(option);
+                        }
+                        if(action == 'remove' && $scope.selected.indexOf(option)!=-1){
+                            var idx = $scope.selected.indexOf(option);
+                            $scope.selected.splice(idx,1);
+                        }
+                    }
+                    $scope2.updateSelection = function($event, data){
+                        var checkbox = $event.target;
+                        var action = (checkbox.checked ? 'add':'remove');
+                        var option = {
+                            openid:data.openid,
+                            nickname:data.nickname,
+                            headimgurl:data.headimgurl
+                        };
+                        updateSelected(action,option);
+                    }
                     $scope2.close = function() {
                         $mi.dismiss();
                     };
                     $scope2.ok = function() {
                         if ($scope2.data) {
-                            $mi.close($scope2.data);
+                            $mi.close($scope2.selected);
                         } else {
                             $mi.dismiss();
                         }
                     };
                 }]
             }).result.then(function(data) {
-                    var params;
-                    if(data.app){
-                        params = {
-                            app: data.app.id,
-                            appType: data.appType,
-                        };
-                        data.appType === 'signin' && (params.includeEnroll = data.includeEnroll);
-                        http2.post('/rest/pl/fe/matter/wall/users/import?site=' + $scope.siteId + '&app=' + $scope.id, params, function(rsp) {
-                            $scope.wall.publicApp = data.app;
-                            $scope.doSearch();
-                        });
-                    }
+                http2.post('/rest/pl/fe/matter/wall/users/userJoin?site=' + $scope.siteId + '&app=' + $scope.id, data, function(rsp) {
+                    $scope.doSearch();
                 });
+            });
         };
         //刷新
         $scope.doSearch = function() {

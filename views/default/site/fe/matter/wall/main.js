398d32de633e58573cf1470effdbea551de1147e
aYellowApple <2764411477@qq.com>
2017-01-10 10:24:54 +0800

    修改调取接口后没反应的问题

--- views/default/site/fe/matter/wall/main.js
+++ views/default/site/fe/matter/wall/main.js
@@ -40,7 +40,7 @@ app.controller('wallCtrl',['$scope','$http','$location',function($scope,$http,$l
         });
     };
     $scope.unjoin = function() {
-        $http.get('/rest/site/fe/matter/wall/quit?site='+ $scope.siteId + '&app=' + $scope.id).success(function(rsp){
+        $http.get('/rest/site/fe/matter/wall/quit?site='+ $scope.siteId + '&app=' + $scope.id,function(rsp){
             if (angular.isString(rsp)) {
                 alert(rsp);
                 return;

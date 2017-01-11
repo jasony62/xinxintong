a06fc32faae30fb0a47ef61868fc99bd8504fe3b
aYellowApple <2764411477@qq.com>
2017-01-11 17:21:44 +0800

    添加缺少的括号

--- views/default/pl/fe/matter/wall/users.js
+++ views/default/pl/fe/matter/wall/users.js
@@ -164,6 +164,7 @@ define(['frame'], function(ngApp) {
         $scope.doSearch = function() {
             http2.get('/rest/pl/fe/matter/wall/users/list?id=' + $scope.id + '&site=' + $scope.siteId, function(rsp) {
                 $scope.users = rsp.data;
+            });
         };
         $scope.doSearch();
         //同步用户

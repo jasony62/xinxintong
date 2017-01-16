9b017dc274affa7255b4a37c31e64ded196410da
liao1a <153887111@qq.com>
2017-01-16 09:28:54 +0800

    修改2

--- clock.php
+++ clock.php
@@ -4,11 +4,11 @@
  * local timezone
  */
 date_default_timezone_set('Asia/Shanghai');
-header('Content-Type: text/html; charset=utf-8');
 
 $rst = new stdClass;
 $rst->err_code = 0;
 $rst->err_msg = 'success';
 $rst->data = time();
 
+header('Content-type: application/json');
 echo json_encode($rst);

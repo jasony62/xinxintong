a626b3728ac41b893969de8842cea8cc45baa65c
lqqllt <lqqllt@163.com>
2017-02-15 21:06:58 +0800

    Yx (#146)
    * 添加自定义文件
    Signed-off-by: lt <lqqllt@163.com>
    
    * 添加配置信息
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * test
    Signed-off-by: lt <lqqllt@163.com>
    
    * ok
    Signed-off-by: lt <lqqllt@163.com>
    
    * 测试
    Signed-off-by: lt <lqqllt@163.com>
    
    * 测试
    Signed-off-by: lt <lqqllt@163.com>
    
    * 忽略
    Signed-off-by: lt <lqqllt@163.com>
    
    * 忽略
    Signed-off-by: lt <lqqllt@163.com>
    
    * 忽略测试文件
    Signed-off-by: lt <lqqllt@163.com>
    
    * 将addressbook移到pl/fe/matter/下面 移动了对应的视图
    Signed-off-by: lt <lqqllt@163.com>
    
    * 将mp/app下边的addressbook移动到pl/fe/matter/addressbook
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改了参数
    Signed-off-by: lt <lqqllt@163.com>
    
    * 恢复无关代码
    Signed-off-by: lt <lqqllt@163.com>
    
    * 删除.gitignore文件
    Signed-off-by: lt <lqqllt@163.com>
    
    * 恢复无关的代码
    Signed-off-by: lt <lqqllt@163.com>
    
    * 恢复无关代码
    Signed-off-by: lt <lqqllt@163.com>
    
    * ok
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 恢复无关代码
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 解决登记活动输入内容换行时获取不到的问题
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 解决获取字符串不能转对象的问题
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 解决登记活动从数据库取不到数据的问题
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改bug能搜索特殊字符
    Signed-off-by: lt <lqqllt@163.com>
    
    * 增加了从数据库取数据解码html实体标签的方法
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lt <lqqllt@163.com>
    
    * Merge branch 'master' of git://github.com/jasony62/xinxintong
    
    # Conflicts:
    #	tms/tms_model.php
    
    Signed-off-by: lintao li <lintao li>
    
    * Signed-off-by: lintao li <lintao li>
    
    * 修改了易信找不到文章回复空白的bug
    Signed-off-by: lqqllt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lqqllt <lqqllt@163.com>

--- models/reply/fullsearch.php
+++ models/reply/fullsearch.php
@@ -9,15 +9,16 @@ require_once dirname(__FILE__).'/base.php';
 class fullsearch_model extends MultiArticleReply {
 
     private $keyword;
+    private $site;
+    private $num;
 
     public function __construct($call, $keyword) 
     {
         parent::__construct($call, null);
         $this->keyword = $keyword;
+        $this->site=$this->call['siteid'];
         $num=\TMS_APP::model('matter\article2')->fullsearch_num($this->call['siteid'], $this->keyword);
-        $_SESSION['num']=$num;
-        $_SESSION['site']=$this->call['siteid'];
-        $_SESSION['keyword']=  $this->keyword;
+        $this->num=$num;      
     }
 
     protected function loadMatters() 
@@ -29,11 +30,15 @@ class fullsearch_model extends MultiArticleReply {
         return $matters;
     }  
     /**
-     * 生成回复消息
+     * 生成回复消息，找不到文章的时候回复文本提示
      */
     public function exec() {
 		$matters = $this->loadMatters();
-		$r = $this->cardResponse($matters);
+		if($matters){
+			$r = $this->cardResponse($matters);
+		}else{
+			$r = $this->textResponse('找不到包含【'.$this->keyword.'】的文章，请更换关键词继续搜索。');
+		}
 		die($r);
      }
     
@@ -48,13 +53,13 @@ class fullsearch_model extends MultiArticleReply {
 		$r .= '<ArticleCount>' . count($matters) . '</ArticleCount>';
 		$r .= '<Articles>';
 		$r .= $this->article_reply($matters);
-		$num=$_SESSION['num'];
+		$num=$this->num;
 		if($num>5){
 			$r.="<item>";
 			$r.="<Title><![CDATA[查看更多]]></Title>";
 			$r.="<Description><![CDATA[查看更多]]></Description>";
 			$r.="<PicUrl><![CDATA[http://xinxintong.oss-cn-hangzhou.aliyuncs.com/9dc76342bbd2d4444748416b3ede427d/%E5%9B%BE%E7%89%87/%E5%A4%B4%E5%9B%BE/%E6%90%9C%E7%B4%A2%E5%9B%BE%E6%A0%87.jpg]]></PicUrl>";
-			$r.="<Url><![CDATA[http://".$_SERVER['HTTP_HOST']."/rest/site/fe/matter/article/search?site=".$_SESSION['site']."&keyword=".$_SESSION['keyword']."]]></Url>";
+			$r.="<Url><![CDATA[http://".$_SERVER['HTTP_HOST']."/rest/site/fe/matter/article/search?site=".$this->site."&keyword=".$this->keyword."]]></Url>";
 			$r.="</item>";
 		}
 		$r .= '</Articles>';

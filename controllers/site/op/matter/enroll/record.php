5d50c4cf267ca9ccf9fac974135a7b88cf5fb1de
liao1a <153887111@qq.com>
2017-02-17 20:15:11 +0800

    Enroll bug 214 (#151)
    * url地址
    
    * 页面地址变动
    
    * 添加被删除的构造
    
    * 修改路径（1）
    
    * 移动
    
    * 移动model
    
    * 移动（1）
    
    * 移动并更改路径（1）
    
    * 数组键写的有问题
    
    * 1
    
    * update code
    
    * 删除不必要的文件
    
    * 登记活动结果审核页面中的删除功能
    
    * 修改1
    
    * 结果审核页中修改日期失败的问题
    
    * 回滚代码
    
    * 修改登记活动结果审核页中修改提交后的逻辑，与管理端从查看数据时的修改提交的逻辑一样

--- controllers/site/op/matter/enroll/record.php
+++ controllers/site/op/matter/enroll/record.php
@@ -41,103 +41,39 @@ class record extends \site\op\base {
 		}
 
 		$record = $this->getPostJson();
-		$model = $this->model();
-		$current = time();
+		$modelEnl = $this->model('matter\enroll');
+		$modelRec = $this->model('matter\enroll\record');
 
-		$app = $this->model('matter\enroll')->byId($app, ['cascaded' => 'N']);
-		$schemas = json_decode($app->data_schemas);
-		$schemasById = [];
-		foreach ($schemas as $schema) {
-			$schemasById[$schema->id] = $schema;
+		$app = $modelEnl->byId($app, ['cascaded' => 'N']);
+
+		/* 更新记录数据 */
+		$updated = new \stdClass;
+		$updated->enroll_at = time();
+		if (isset($record->comment)) {
+			$updated->comment = $record->comment;
+		}
+		if (isset($record->tags)) {
+			$updated->tags = $record->tags;
+			$modelEnl->updateTags($app->id, $record->tags);
+		}
+		if (isset($record->verified)) {
+			$updated->verified = $record->verified;
+		}
+		$modelEnl->update('xxt_enroll_record', $updated, "enroll_key='$ek'");
+
+		/* 记录登记数据 */
+		$result = $modelRec->setData(null, $site, $app, $ek, $record->data);
+
+		if (isset($updated->verified) && $updated->verified === 'Y') {
+			$this->_whenVerifyRecord($app, $ek);
 		}
 
-		//
-		$model->update(
-			'xxt_enroll_record',
-			['enroll_at' => $current],
-			"enroll_key='$ek'"
-		);
-		foreach ($record as $k => $v) {
-			if (in_array($k, ['verified', 'tags', 'comment'])) {
-				$model->update(
-					'xxt_enroll_record',
-					[$k => $v],
-					"enroll_key='$ek'"
-				);
-				// 更新记录的标签时，要同步更新活动的标签，实现标签在整个活动中有效
-				if ($k === 'tags') {
-					$this->model('matter\enroll')->updateTags($app->id, $v);
-				}
-				if ($k === 'verified' && $v === 'Y') {
-					$this->_whenVerifyRecord($app, $ek);
-				}
-			} else if ($k === 'data' and is_object($v)) {
-				$dbData = new \stdClass;
-				foreach ($v as $cn => $cv) {
-					$schema = $schemasById[$cn];
-					if (is_array($cv) && isset($cv[0]->imgSrc)) {
-						/* 上传图片 */
-						$vv = array();
-						$fsuser = $this->model('fs/user', $site);
-						foreach ($cv as $img) {
-							if (preg_match("/^data:.+base64/", $img->imgSrc)) {
-								$rst = $fsuser->storeImg($img);
-								if (false === $rst[0]) {
-									return new \ResponseError($rst[1]);
-								}
-								$vv[] = $rst[1];
-							} else {
-								$vv[] = $img->imgSrc;
-							}
-						}
-						$cv = implode(',', $vv);
-						$dbData->{$cn} = $cv;
-					} else if ($schema->type === 'score') {
-						$dbData->{$cn} = $cv;
-						$cv = json_encode($cv);
-					} else if (is_string($cv)) {
-						$cv = $model->escape($cv);
-						$dbData->{$cn} = $cv;
-					} else if (is_object($cv) || is_array($cv)) {
-						/*多选题*/
-						$cv = implode(',', array_keys(array_filter((array) $cv, function ($i) {return $i;})));
-						$dbData->{$cn} = $cv;
-					}
-					/*检查数据项是否存在，如果不存在就先创建一条*/
-					$q = array(
-						'count(*)',
-						'xxt_enroll_record_data',
-						"enroll_key='$ek' and name='$cn'",
-					);
-					if (1 === (int) $model->query_val_ss($q)) {
-						$model->update(
-							'xxt_enroll_record_data',
-							array('value' => $cv),
-							"enroll_key='$ek' and name='$cn'"
-						);
-					} else {
-						$cd = [
-							'aid' => $app->id,
-							'enroll_key' => $ek,
-							'name' => $cn,
-							'value' => $cv,
-						];
-						$model->insert('xxt_enroll_record_data', $cd, false);
-					}
-				}
-				//
-				$record->data = $dbData;
-				$dbData = $model->toJson($dbData);
-				$model->update(
-					'xxt_enroll_record',
-					['data' => $dbData],
-					"enroll_key='$ek'"
-				);
-			}
-		}
-		// 记录操作日志
-		//$app->type = 'enroll';
-		//$this->model('matter\log')->matterOp($site, $user, $app, 'update', $record);
+		/* 记录操作日志 */
+		// $app->type = 'enroll';
+		// $this->model('matter\log')->matterOp($site, $user, $app, 'update', $record);
+
+		/* 返回完整的记录 */
+		$record = $modelRec->byId($ek);
 
 		return new \ResponseData($record);
 	}

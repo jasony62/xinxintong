5cd80e4ee8e6bccebf8924f49b55c8b77d54d211
lqqllt <lqqllt@163.com>
2017-03-02 15:22:43 +0800

    修改一些bug Signed-off-by: lqqllt <lqqllt@163.com>

--- controllers/pl/fe/matter/article/favor.php
+++ controllers/pl/fe/matter/article/favor.php
@@ -11,7 +11,7 @@ class favor extends \pl\fe\matter\base {
 	 *
 	 */
 	public function list_action($site, $id, $page=1, $size=30) {
-		$p=['*','xxt_site_favor',"siteid='$site' and matter_id='$id'"];
+		$p=['*','xxt_site_favor',"siteid='$site' and matter_type='article' and matter_id='$id'"];
 		$p2['r']['o'] = ($page - 1) * $size;
 		$p2['r']['l'] = $size;
 		$p2['o'] = 'id desc';

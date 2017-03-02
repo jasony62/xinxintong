f820a3d5389c757cd3eeaecdaabba3e7b1527674
Jason Young <jasony62@qq.com>
2017-03-02 20:46:40 +0800

    update code

--- models/sns/wx/proxy.php
+++ models/sns/wx/proxy.php
@@ -34,7 +34,7 @@ class proxy_model extends \sns\proxybase {
 	 */
 	public function join($data) {
 		if (empty($data['signature']) || empty($data['timestamp']) || empty($data['nonce']) || empty($data['echostr'])) {
-			return array(false, 'failed-0');
+			return array(false, 'wx proxy failed-0');
 		}
 		$signature = $data['signature'];
 		$timestamp = $data['timestamp'];
@@ -65,7 +65,7 @@ class proxy_model extends \sns\proxybase {
 
 			return array(true, $echostr);
 		} else {
-			return array(false, 'failed-1');
+			return array(false, 'wx proxy failed-1');
 		}
 	}
 	/**

78582017d3f634270c2680a5faf7b96b095b4ab1
lqqllt <lqqllt@163.com>
2017-03-03 11:33:02 +0800

    解决微信公众号菜单选择文本发布报错的问题 Signed-off-by: lqqllt <lqqllt@163.com>

--- controllers/pl/fe/site/sns/wx/menu.php
+++ controllers/pl/fe/site/sns/wx/menu.php
@@ -547,6 +547,8 @@ class menu extends \pl\fe\base {
 				 */
 				if ($button->matter_type === 'article') {
 					$url = $this->model('matter\article2')->getEntryUrl($site, $button->matter_id);
+				} else if (in_array($button->matter_type, ['text','group'])) {
+					throw new MenuInvalidException("选择【{$button->matter_type}】的素材类型不支持。");
 				} else {
 					$url = $this->model('matter\\' . $button->matter_type)->getEntryUrl($site, $button->matter_id);
 				}

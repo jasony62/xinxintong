d4d4fb8b69139f839e06f5ab9dbcc229c834d79c
Jason Young <jasony62@qq.com>
2017-03-04 22:08:37 +0800

    update code

--- schema/update/update2017030301.php
+++ schema/update/update2017030301.php
@@ -3,12 +3,11 @@ require_once '../../db.php';
 
 $sqls = array();
 //
-$sql = "alter table xxt_mission_matter drop primary key";
-$sql = "alter table xxt_mission_matter add id int not null auto_increment first, add primary key(id)";
-$sql = "alter table xxt_mission_matter add is_public char(1) not null default 'Y' after matter_type";
-$sql = "alter table xxt_mission_matter change seq seq int not null default 65535";
-$sql = "update xxt_mission_matter set seq=65535 where seq=0";
-$sqls[] = $sql;
+$sqls[] = "alter table xxt_mission_matter drop primary key";
+$sqls[] = "alter table xxt_mission_matter add id int not null auto_increment first, add primary key(id)";
+$sqls[] = "alter table xxt_mission_matter add is_public char(1) not null default 'Y' after matter_type";
+$sqls[] = "alter table xxt_mission_matter change seq seq int not null default 65535";
+$sqls[] = "update xxt_mission_matter set seq=65535 where seq=0";
 //
 foreach ($sqls as $sql) {
 	if (!$mysqli->query($sql)) {

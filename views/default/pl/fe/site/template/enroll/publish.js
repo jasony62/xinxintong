8c92a0f650593e6360f5726d608ffbde738780b4
aYellowApple <2764411477@qq.com>
2017-03-07 13:59:24 +0800

    迁移enroll文件夹

---
+++ views/default/pl/fe/site/template/enroll/publish.js
@@ -0,0 +1,96 @@
+define(['frame'], function(ngApp) {
+    'use strict';
+    ngApp.provider.controller('ctrlPublish', ['$scope', 'mediagallery', '$timeout', 'srvEnrollApp', 'srvTempApp', function($scope, mediagallery, $timeout, srvEnrollApp, srvTempApp) {
+        $scope.setPic = function() {
+            var options = {
+                callback: function(url) {
+                    $scope.app.pic = url + '?_=' + (new Date() * 1);
+                    srvEnrollApp.update('pic');
+                }
+            };
+            mediagallery.open($scope.app.siteid, options);
+        };
+        $scope.removePic = function() {
+            $scope.app.pic = '';
+            srvEnrollApp.update('pic');
+        };
+        /**
+         * 访问控制规则
+         */
+        $scope.isInputPage = function(pageName) {
+            if (!$scope.app) {
+                return false;
+            }
+            for (var i in $scope.app.pages) {
+                if ($scope.app.pages[i].name === pageName && $scope.app.pages[i].type === 'I') {
+                    return true;
+                }
+            }
+            return false;
+        };
+        $scope.applyToHome = function() {
+            var url = '/rest/pl/fe/matter/home/apply?site=' + $scope.app.siteid + '&type=enroll&id=' + $scope.app.id;
+            http2.get(url, function(rsp) {
+                noticebox.success('完成申请！');
+            });
+        };
+        $scope.cancelAsTemplate = function() {
+            var url = '/rest/pl/fe/matter/home/apply?site=' + $scope.app.siteid + '&type=enroll&id=' + $scope.app.id;
+            http2.get(url, function(rsp) {
+                noticebox.success('完成撤销！');
+            });
+        };
+        $scope.remove = function() {
+            if (window.confirm('确定删除活动？')) {
+                srvEnrollApp.remove().then(function() {
+                    if ($scope.app.mission) {
+                        location = "/rest/pl/fe/matter/mission?site=" + $scope.app.siteid + "&id=" + $scope.app.mission.id;
+                    } else {
+                        location = '/rest/pl/fe/site/console?site=' + $scope.app.siteid;
+                    }
+                });
+            }
+        };
+    }]);
+
+    ngApp.provider.controller('ctrlPreview', ['$scope', 'srvEnrollApp', 'srvTempApp', function($scope, srvEnrollApp, srvTempApp) {
+        function refresh() {
+            $scope.previewURL = previewURL + '&openAt=' + params.openAt + '&page=' + params.page.name + '&_=' + (new Date() * 1);
+        }
+        var previewURL, params;
+        $scope.params = params = {
+            openAt: 'ontime',
+        };
+        $scope.showPage = function(page) {
+            params.page = page;
+        };
+        /*srvTempApp.tempEnrollGet().then(function(app) {
+            if (app.pages && app.pages.length) {
+                $scope.gotoPage = function(page) {
+                    var url = "/rest/pl/fe/matter/enroll/page";
+                    url += "?site=" + app.siteid;
+                    url += "&id=" + app.id;
+                    url += "&page=" + page.name;
+                    location.href = url;
+                };
+                previewURL = '/rest/site/fe/matter/enroll/preview?site=' + app.siteid + '&app=' + app.id + '&start=Y';
+                params.page = app.pages[0];
+                $scope.$watch('params', function() {
+                    refresh();
+                }, true);
+                $scope.$watch('app.use_site_header', function(nv, ov) {
+                    nv !== ov && refresh();
+                });
+                $scope.$watch('app.use_site_footer', function(nv, ov) {
+                    nv !== ov && refresh();
+                });
+                $scope.$watch('app.use_mission_header', function(nv, ov) {
+                    nv !== ov && refresh();
+                });
+                $scope.$watch('app.use_mission_header', function(nv, ov) {
+                    nv !== ov && refresh();
+                });
+            }
+        });*/
+    }]);
+});

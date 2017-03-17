88408a58c916bce1c15d6e731d0eeaf4bd9d6969
aYellowApple <2764411477@qq.com>
2017-03-17 15:34:15 +0800

    修改notice接口

--- views/default/pl/fe/matter/group/lib/group.service.js
+++ views/default/pl/fe/matter/group/lib/group.service.js
@@ -531,14 +531,16 @@ provider('srvGroupApp', function() {
         }
     }];
 }).provider('srvGroupNotice', function() {
-    this.$get = ['$q', 'http2', function($q, http2){
+    this.$get = ['$q', 'http2', 'srvGroupApp', function($q, http2, srvGroupApp){
         return {
-            detail: function(batch, app) {
+            detail: function(batch) {
                 var defer = $q.defer(),
-                    url;
-                url = '/rest/pl/fe/matter/group/notice/logList?batch=' + batch.id + '&aid=' + batch.aid;
-                http2.get(url, function(rsp) {
-                    defer.resolve(rsp.data);
+                        url;
+                srvGroupApp.get().then(function(oApp) {
+                    url = '/rest/pl/fe/matter/group/notice/logList?batch=' + batch.id + '&aid=' + oApp.id;
+                    http2.get(url, function(rsp) {
+                        defer.resolve(rsp.data);
+                    });
                 });
 
                 return defer.promise;

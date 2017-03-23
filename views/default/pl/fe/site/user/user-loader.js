c5f90f2fe657f77707987741f867cc54a5687279
lishuai <1076283698@qq.com>
2017-03-23 13:33:29 +0800

    details

--- views/default/pl/fe/site/user/user-loader.js
+++ views/default/pl/fe/site/user/user-loader.js
@@ -1,10 +1,7 @@
-/**
- * Created by lishuai on 2017/3/22.
- */
 require.config({
     paths: {
         "domReady": '/static/js/domReady',
-        "main": '/views/default/pl/fe/site/user/home',
+        "main": '/views/default/pl/fe/site/user/user',
     },
     urlArgs: "bust=" + (new Date() * 1)
 });
--- views/default/pl/fe/site/user/user.js
+++ views/default/pl/fe/site/user/user.js
@@ -1,9 +1,9 @@
 define(['require'], function(require) {
     'use strict';
-    var ngApp = angular.module('app', ['ngRoute', 'ui.bootstrap']);
-    ngApp.config(['$locationProvider', '$routeProvider', '$controllerProvider',  function($lp, $rp, $cp) {
+    var ngApp = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ui.tms', 'ui.xxt', 'service.matter']);
+    ngApp.config(['$locationProvider', '$routeProvider', '$controllerProvider', 'srvSiteProvider', function($lp, $rp, $cp, srvSiteProvider) {
         var RouteParam = function(name, loadjs) {
-            var baseURL = '/views/default/pl/fe/site/home/';
+            var baseURL = '/views/default/pl/fe/site/user/';
             this.templateUrl = baseURL + name + '.html?_=' + (new Date() * 1);
             this.controller = 'ctrl' + name[0].toUpperCase() + name.substr(1);
             if (loadjs) {
@@ -18,24 +18,35 @@ define(['require'], function(require) {
                 };
             }
         };
-        //var siteId = location.search.match(/site=([^&]*)/)[1];
-        ////srvSiteProvider.config(siteId);
+        var siteId = location.search.match(/site=([^&]*)/)[1];
+        srvSiteProvider.config(siteId);
         ngApp.provider = {
             controller: $cp.register
         };
-        $rp.when('/rest/pl/fe/site/user/main', new RouteParam('main', true))
-            .when('/rest/pl/fe/site/user/history', new RouteParam('history', true))
-            .when('/rest/pl/fe/site/user/message', new RouteParam('message', true))
-            .otherwise(new RouteParam('main', true));
+        $rp.when('/rest/pl/fe/site/user/details', new RouteParam('details', true))
+            //.when('/rest/pl/fe/site/home/analysis', new RouteParam('analysis', true))
+            .otherwise(new RouteParam('details', true));
         $lp.html5Mode(true);
     }]);
-    ngApp.controller('ctrlUser', ['$scope',  function($scope) {
+    ngApp.controller('ctrlUser', ['$scope', 'srvSite', function($scope, srvSite) {
         $scope.subView = '';
+        $scope.$root.catelogs = [];
+        $scope.catelog = null;
         $scope.$on('$locationChangeSuccess', function(event, currentRoute) {
             var subView = currentRoute.match(/([^\/]+?)\?/);
-            $scope.subView = subView[1] === 'user' ? 'page' : subView[1];
+            $scope.subView = subView[1] === 'user' ? 'details' : subView[1];
         });
-
+        //?
+        $scope.$root.$watchCollection('catelogs', function(catelogs) {
+            if (catelogs && catelogs.length) {
+                $scope.catelog = catelogs[0];
+            }
+        });
+        srvSite.get().then(function(site) {
+            $scope.site = site;
+        });
+        $scope.siteid = '79feceb6363510a25c13bb56416c15c9';
+        $scope.userid = '58d21dd709099';
     }]);
     /***/
     require(['domReady!'], function(document) {

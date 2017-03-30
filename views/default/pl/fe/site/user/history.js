666962e1972e453fc14409b1758bacf7e34d4058
lishuai <1076283698@qq.com>
2017-03-30 13:23:23 +0800

    history

--- views/default/pl/fe/site/user/history.js
+++ views/default/pl/fe/site/user/history.js
@@ -1,97 +1,78 @@
 /**
  * Created by lishuai on 2017/3/23.
  */
-define(['frame'], function(ngApp) {
+define(['frame'], function (ngApp) {
+
     'use strict';
-
-    ngApp.provider.controller('ctrlHistory',['$scope', 'http2', function($scope, http2){
+    ngApp.provider.controller('ctrlHistory', ['$scope', 'http2', function ($scope, http2) {
         var page,
-			baseURL = '/rest/pl/fe/site/user/';
-		$scope.page = page = {
-			at: 1,
-			site: 30,
-			j: function(){
-				return '&at=' + this.at + '&site=' + this.site;
-			}
-		};
-		$scope.historys = {
-			enroll: {
-				title: '活动记录',
-				content:[],
-			},
-			read: {
-				title: '阅读记录',
-				content:[],
-			},
-			favor: {
-				title: '收藏记录',
-				content:[],
-			}
-		};
-		//获取活动记录
-		//http2.get('/rest/site/fe/user/history/appList?site='+$scope.siteId+'&uid='+$scope.userId+page.j(), function(rsp){
-			//$scope.history.enroll.content = rsp.data.app;
-			$scope.historys.enroll.content = [{
-				"matter_id": "588428b27e7b4",
-				"matter_type": "enroll",
-				"matter_title": "登记1",
-				"operate_at": "1490579741"
-			}, {
-				"matter_id": "58d8723beff6f",
-				"matter_type": "enroll",
-				"matter_title": "登记2",
-				"operate_at": "1490580259"
-			}, {
-				"matter_id": "58d87292c6419",
-				"matter_type": "enroll",
-				"matter_title": "登记3",
-				"operate_at": "1490580316"
-			}];
-		//});
-		//获取阅读记录
-		//http2.get('/rest/site/fe/user/history/appList?site='+$scope.siteId+'&uid='+$scope.userId+page.j(), function(rsp){
-			//$scope.history.read.content = rsp.data.app;
-			$scope.historys.read.content = [{
-				"matter_id": "588428b27e7b4",
-				"matter_type": "enroll",
-				"matter_title": "阅读1",
-				"operate_at": "1490579741"
-			}, {
-				"matter_id": "58d8723beff6f",
-				"matter_type": "enroll",
-				"matter_title": "阅读2",
-				"operate_at": "1490580259"
-			}, {
-				"matter_id": "58d87292c6419",
-				"matter_type": "enroll",
-				"matter_title": "阅读3",
-				"operate_at": "1490580316"
-			}];
-		//});
+            baseURL = '/rest/pl/fe/site/user/';
+        $scope.historys = {
+            enroll: {
+                title: '活动记录',
+                content: [],
+                total:0
+            },
+            read: {
+                title: '阅读记录',
+                content: [],
+                total:0
 
-		//获取收藏记录
-		//http2.get('/rest/site/fe/user/favor/list?site='+$scope.siteId+'&uid'+$scope.userId+page.j(), function(rsp){
-			//$scope.history.favor.content = rsp.data.app;
-			$scope.historys.favor.content = [{
-				"matter_id": "588428b27e7b4",
-				"matter_type": "enroll",
-				"matter_title": "收藏1",
-				"operate_at": "1490579741"
-			}, {
-				"matter_id": "58d8723beff6f",
-				"matter_type": "enroll",
-				"matter_title": "收藏2",
-				"operate_at": "1490580259"
-			}, {
-				"matter_id": "58d87292c6419",
-				"matter_type": "enroll",
-				"matter_title": "收藏3",
-				"operate_at": "1490580316"
-			}];
-		//});
-		//管理员打开活动
-		//$scope.openApp = function(app){
-		//	location.href = '/rest/pl/fe/matter/' + app.matter_type + '?id=' + app.matter_id + '&site=' + $scope.siteId;
-		//}
+            },
+            favor: {
+                title: '收藏记录',
+                content: [],
+                total:0
+            }
+        };
+        $scope.page = page = {
+            at: 1,
+            site: 30,
+            type : '活动记录',
+            total : $scope.historys.enroll.total,
+            j: function () {
+                return '&at=' + this.at + '&site=' + this.site;
+            }
+        };
+        //获取活动记录
+        $scope.enrollList = function () {
+            http2.get('/rest/site/fe/user/history/appList?site=' + $scope.siteId + '&uid=' + $scope.userId + page.j(), function (rsp) {
+                $scope.historys.enroll.content = rsp.data.apps;
+                rsp.data.total ? $scope.historys.enroll.total = rsp.data.total : 0;
+            });
+        };
+        //获取阅读记录
+        $scope.readList = function () {
+            http2.get('/rest/pl/fe/site/user/readList?site=' + $scope.siteId + '&uid=' + $scope.userId + page.j(), function (rsp) {
+                $scope.historys.read.content = rsp.data.matters;
+                rsp.data.total ? $scope.historys.read.total = rsp.data.total : 0;
+            });
+        };
+        //获取收藏记录
+        $scope.favorList = function () {
+            http2.get('/rest/site/fe/user/favor/list?site=' + $scope.siteId + '&uid' + $scope.userId + page.j(), function (rsp) {
+                $scope.historys.favor.content = rsp.data.matters;
+                rsp.data.total ? $scope.historys.favor.total = rsp.data.total : 0;
+            });
+        };
+        $scope.enrollList();
+        $scope.readList();
+        $scope.favorList();
+        //分页
+        $scope.doSearch = function(){
+            $scope.page.type === '活动记录' ? $scope.enrollList() : $scope.page.type === '阅读记录'? $scope.readList(): $scope.favorList();
+        };
+        //切换tal页签 初始化分页，修改type,修改page
+        $scope.init = function (type) {
+            if(type===$scope.page.type){return;}
+            $scope.page.at = 1;
+            $scope.page.type = type;
+            $scope.page.total = type === '活动记录' ? $scope.historys.enroll.total : type === '阅读记录'? $scope.historys.read.total: $scope.historys.favor.total;
+        };
+        //管理员打开活动
+        //$scope.openApp = function(app){
+        //	location.href = '/rest/pl/fe/matter/' + app.matter_type + '?id=' + app.matter_id + '&site=' + $scope.siteId;
+        //}
     }])
+
 });

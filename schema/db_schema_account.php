2a48fab651b62779d9f2b5fcd1c3715fc995d992
lqqllt <lqqllt@163.com>
2017-03-30 11:26:51 +0800

    修改一些bug Signed-off-by: lqqllt <lqqllt@163.com>

--- schema/db_schema_account.php
+++ schema/db_schema_account.php
@@ -57,7 +57,7 @@ if (!$mysqli->query($sql)) {
 $sqls[]="CREATE TABLE `xxt_account_topmatter` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `siteid` varchar(32) NOT NULL,
-  `userid` varchar(32) NOT NULL COMMENT '最后一次置顶操作的用户',
+  `userid` varchar(32) NOT NULL COMMENT '置顶操作的用户',
   `top` enum('0','1') NOT NULL DEFAULT '0' COMMENT '置顶',
   `top_at` int(11) NOT NULL COMMENT '置顶时间',
   `matter_id` varchar(40) NOT NULL,

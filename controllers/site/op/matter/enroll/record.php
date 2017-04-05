13457d2a58852b8f7c2b563633be5d8ee15250a9
liao1a <153887111@qq.com>
2017-04-05 10:11:12 +0800

    修改1

--- controllers/site/op/matter/enroll/record.php
+++ controllers/site/op/matter/enroll/record.php
@@ -188,7 +188,7 @@ class record extends \site\op\base {
 	 * 返回指定登记项的活动登记名单
 	 *
 	 */
-	public function list4Schema_action($site, $app, $schema, $page = 1, $size = 10) {
+	public function list4Schema_action($site, $app, $rid = null, $schema, $page = 1, $size = 10) {
 		if (!$this->checkAccessToken()) {
 			return new \InvalidAccessToken();
 		}
@@ -201,6 +201,9 @@ class record extends \site\op\base {
 			'page' => $page,
 			'size' => $size,
 		];
+		if (!empty($rid)) {
+			$options['rid'] = $rid;
+		}
 
 		// 登记活动
 		$modelApp = $this->model('matter\enroll');

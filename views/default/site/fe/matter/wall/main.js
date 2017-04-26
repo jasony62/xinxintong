edb005c6e8d4b38a175234328bb21a2832cd2db5
aYellowApple <2764411477@qq.com>
2017-04-26 13:24:10 +0800

    修改信息墙关闭不了界面的问题

--- views/default/site/fe/matter/wall/main.js
+++ views/default/site/fe/matter/wall/main.js
@@ -7,8 +7,8 @@ app.controller('wallCtrl',['$scope','$http','$location',function($scope,$http,$l
         $scope.id = ls.app;
         $scope.siteId = ls.site;
     if (/MicroMessenger/.test(navigator.userAgent)) {
-        document.addEventListener('WeixinJSBridgeReady', function(){
-            WeixinJSBridge.call('hideOptionMenu');
+        document.addEventListener('wx', function(){
+            wx.hideOptionMenu();
         }, false);
     } else if (/YiXin/.test(navigator.userAgent)) {
         document.addEventListener('YixinJSBridgeReady', function() {
@@ -21,7 +21,7 @@ app.controller('wallCtrl',['$scope','$http','$location',function($scope,$http,$l
         $scope.status = rsp.data.wallUser;
     })
     $scope.join = function() {
-        $http.get('/rest/site/fe/matter/wall/join?site='+ $scope.siteId + '&app=' + $scope.id ,function(rsp){
+        $http.get('/rest/site/fe/matter/wall/join?site='+ $scope.siteId + '&app=' + $scope.id).success(function(rsp) {
             if (angular.isString(rsp)) {
                 alert(rsp);
                 return;
@@ -32,7 +32,7 @@ app.controller('wallCtrl',['$scope','$http','$location',function($scope,$http,$l
             }
             if (rsp.err_code == 0) {
                 if (/MicroMessenger/.test(navigator.userAgent)) {
-                    WeixinJSBridge.invoke('closeWindow',{},function(res){});
+                    wx.closeWindow();
                 } else if (/YiXin/.test(navigator.userAgent)) {
                     YixinJSBridge.invoke('closeWindow',{},function(res){});
                 }
@@ -40,7 +40,7 @@ app.controller('wallCtrl',['$scope','$http','$location',function($scope,$http,$l
         });
     };
     $scope.unjoin = function() {
-        $http.get('/rest/site/fe/matter/wall/quit?site='+ $scope.siteId + '&app=' + $scope.id,function(rsp){
+        $http.get('/rest/site/fe/matter/wall/quit?site='+ $scope.siteId + '&app=' + $scope.id).success(function(rsp) {
             if (angular.isString(rsp)) {
                 alert(rsp);
                 return;
@@ -51,7 +51,7 @@ app.controller('wallCtrl',['$scope','$http','$location',function($scope,$http,$l
             }
             if (rsp.err_code == 0) {
                 if (/MicroMessenger/.test(navigator.userAgent)) {
-                    WeixinJSBridge.invoke('closeWindow',{},function(res){});
+                    wx.closeWindow();
                 } else if (/YiXin/.test(navigator.userAgent)) {
                     YixinJSBridge.invoke('closeWindow',{},function(res){});
                 }

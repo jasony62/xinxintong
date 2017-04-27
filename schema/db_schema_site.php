90825b88cfac692dc9028a11e0f492462ecd2783
liao1a <153887111@qq.com>
2017-04-27 09:21:55 +0800

    Article copy 0424 (#258)
    * url地址
    
    * 页面地址变动
    
    * 添加被删除的构造
    
    * 修改路径（1）
    
    * 移动
    
    * 移动model
    
    * 移动（1）
    
    * 移动并更改路径（1）
    
    * 数组键写的有问题
    
    * 1
    
    * update code
    
    * 删除不必要的文件
    
    * 創建模版所需的数据库表增加登记活动模板表
    
    * Revert "創建模版所需的数据库表增加登记活动模板表"
    
    This reverts commit fb0e098ad0a57076aaabc6e69c93fcca4b6a0093.
    
    * 在签到活动中增加发送消息的功能
    
    * Revert "在签到活动中增加发送消息的功能"
    
    This reverts commit e856720411d7138e1deecb8945672a7fd6c25402.
    
    * 重做单图文复制功能，增加引用的支持
    
    * 增加团队收藏指定素材的功能
    
    * 修改逻辑

--- schema/db_schema_site.php
+++ schema/db_schema_site.php
@@ -251,6 +251,24 @@ if (!$mysqli->query($sql)) {
 	header('HTTP/1.0 500 Internal Server Error');
 	echo 'database error(xxt_site_favor): ' . $mysqli->error;
 }
+/**
+ * 团队收藏记录
+ */
+$sql = "create table if not exists xxt_site_friend_favor(";
+$sql .= "id int not null auto_increment";
+$sql .= ",siteid varchar(32) not null";
+$sql .= ",creater varchar(40) not null";
+$sql .= ",creater_name varchar(255) not null";
+$sql .= ",favor_at int not null";
+$sql .= ",from_siteid varchar(32) not null";
+$sql .= ",matter_id varchar(40) not null";
+$sql .= ",matter_type varchar(20) not null";
+$sql .= ",matter_title varchar(70) not null";
+$sql .= ",primary key(id)) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+if (!$mysqli->query($sql)) {
+	header('HTTP/1.0 500 Internal Server Error');
+	echo 'database error(xxt_site_friend_favor): ' . $mysqli->error;
+}
 /**************************/
 /**
  * 自定义用户信息

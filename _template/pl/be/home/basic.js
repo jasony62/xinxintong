4fe11138f62325fecd126b1c94f8e458a0ff3a7f
Truth <2764411477@qq.com>
2017-04-27 11:08:22 +0800

    Style site0420 (#255)
    * 样式修改
    
    * 初步完成平台首页的样式修改

--- _template/pl/be/home/basic.js
+++ _template/pl/be/home/basic.js
@@ -1,7 +1,16 @@
 ngApp.provider.controller('ctrlHome', ['$scope', '$http', '$uibModal', function($scope, $http, $uibModal) {
-    function listSites() {
-        $http.get('/rest/home/listSite').success(function(rsp) {
+    var page;
+    $scope.page = page = {
+        at: 1,
+        size: 5,
+        j: function() {
+            return '&page=' + this.at + '&size=' + this.size;
+        }
+    }
+    function listSites(number) {
+        $http.get('/rest/home/listSite'+ '?page=1' + '&size=' + number).success(function(rsp) {
             $scope.sites = rsp.data.sites;
+            $scope.sites.total = rsp.data.total;
         });
     }
 
@@ -10,17 +19,33 @@ ngApp.provider.controller('ctrlHome', ['$scope', '$http', '$uibModal', function(
             $scope.templates = rsp.data;
         });
     }
+    $scope.moreMatters = function(matterType) {
+        $scope.page.size = $scope.page.size + 5;
+        switch (matterType) {
+            case 'article':
+                $scope.listArticles($scope.page.size);
+                break;
+            case 'app':
+                $scope.listSites($scope.page.size);
+                break;
+            case 'site':
+                $scope.listApps($scope.page.size);
+                break;
+        }
+    }
     $scope.openMatter = function(matter) {
         location.href = matter.url;
     };
-    $scope.listApps = function() {
-        $http.get('/rest/home/listApp').success(function(rsp) {
+    $scope.listApps = function(number) {
+        $http.get('/rest/home/listApp'+ '?page=1' + '&size=' + number).success(function(rsp) {
             $scope.apps = rsp.data.matters;
+            $scope.apps.total = rsp.data.total;
         });
     };
-    $scope.listArticles = function() {
-        $http.get('/rest/home/listArticle').success(function(rsp) {
+    $scope.listArticles = function(number) {
+        $http.get('/rest/home/listArticle'+ '?page=1' + '&size=' + number).success(function(rsp) {
             $scope.articles = rsp.data.matters;
+            $scope.articles.total = rsp.data.total;
         });
     };
     $scope.listChannels = function() {
@@ -40,10 +65,13 @@ ngApp.provider.controller('ctrlHome', ['$scope', '$http', '$uibModal', function(
     $http.get('/rest/home/listMatterTop?type=article&page=1&size=3').success(function(rsp) {
         $scope.topArticles = rsp.data.matters;
     });
-    listSites();
+    document.querySelector('#gototop').addEventListener('click', function() {
+        document.querySelector('body').scrollTop = 0;
+    });
+    listSites(5);
     listTemplates();
-    $scope.listApps();
-    $scope.listArticles();
+    $scope.listApps(5);
+    $scope.listArticles(5);
     $scope.listChannels();
 }]);
 ngApp.provider.controller('ctrlCarousel', function($scope) {

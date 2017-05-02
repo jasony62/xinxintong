ac95cd8b317a9ea299bb9bcce9f024c5ef1c2c8f
liao1a <153887111@qq.com>
2017-05-02 13:54:34 +0800

    merge/master

---
+++ asset/js/xxt.ui.forward.js
@@ -0,0 +1,111 @@
+'use strict';
+require('../css/xxt.ui.modal.css');
+
+require('./xxt.ui.page.js');
+require('./xxt.ui.modal.js');
+
+var ngMod = angular.module('forward.ui.xxt', ['page.ui.xxt', 'modal.ui.xxt']);
+ngMod.service('tmsForward', ['$rootScope', '$http', '$q', 'tmsDynaPage', 'tmsModal', function($rootScope, $http, $q, tmsDynaPage, tmsModal) {
+    function bySite(oMatter) {
+        var url, defer;
+        defer = $q.defer();
+        url = '/rest/pl/fe/site/forward/sitesByUser?site=' + oMatter.siteid + '&id=' + oMatter.id + '&type=' + oMatter.type + '&_=' + (new Date() * 1);
+        $http.get(url).success(function(rsp) {
+            if (rsp.err_code != 0) {
+                return;
+            }
+            defer.resolve(rsp.data);
+        });
+        return defer.promise;
+    }
+    this.open = function(oMatter) {
+        var template;
+        template = '<div class="modal-header"><span class="modal-title">转发到指定团队主页</span></div>';
+        template += '<div class="modal-body">';
+        template += '<div ng-repeat="site in mySites">';
+        template += '<span>{{site.name}}</span>';
+        template += '<div class="checkbox" ng-repeat="chn in site.homeChannels">';
+        template += '<label>';
+        template += '<input type=\'checkbox\' ng-true-value="\'Y\'" ng-false-value="\'N\'" ng-model=\'chn._selected\' ng-change="choose(site,chn)">';
+        template += '<span>{{chn.title}}</span>';
+        template += '</label>';
+        template += '</div>'
+        template += '<div ng-if="site.homeChannels.length===0"><a href="" ng-click="createChannel(site)">创建</a>团队主页频道，转发内容到团队主页</div>';
+        template += '</div>'
+        template += '<div ng-if="mySites.length===0"><a href="" ng-click="createSite()">创建</a>团队，转发内容到团队主页</div>';
+        template += '</div>';
+        template += '<div class="modal-footer"><button ng-click="cancel()">关闭</button><button ng-click="ok()">确定</button></div>';
+        tmsModal.open({
+            template: template,
+            controller: ['$http', '$scope', '$tmsModalInstance', function($http, $scope2, $mi) {
+                var aSelected = [];
+                bySite(oMatter).then(function(sites) {
+                    var mySites = sites;
+                    mySites.forEach(function(site) {
+                        site._selected = site._recommended;
+                    });
+                    $scope2.mySites = mySites;
+                });
+                $scope2.createChannel = function(site) {
+                    $http.post('/rest/pl/fe/matter/channel/create?site=' + site.id, {}).success(function(rsp) {
+                        var oChannel = rsp.data;
+                        $http.post('/rest/pl/fe/site/setting/page/addHomeChannel?site=' + site.id, oChannel).success(function(rsp) {
+                            site.homeChannels.push(rsp.data);
+                        });
+                    });
+                };
+                $scope2.createSite = function() {
+                    $http.get('/rest/pl/fe/site/create').success(function(rsp) {
+                        var site = rsp.data;
+                        site._selected = 'N';
+                        site.homeChannels = [];
+                        $scope2.mySites = [site];
+                    });
+                };
+                $scope2.choose = function(oSite, oChannel) {
+                    if (oChannel._selected === 'Y') {
+                        oChannel.siteid = oSite.id;
+                        aSelected.push(oChannel);
+                    } else {
+                        aSelected.splice(aSelected.indexOf(oChannel), 1);
+                    }
+                };
+                $scope2.ok = function() {
+                    var aTargets = [];
+                    if (aSelected.length) {
+                        aSelected.forEach(function(oChannel) {
+                            aTargets.push({ siteid: oChannel.siteid, channelId: oChannel.channel_id });
+                        });
+                        $http.post('/rest/pl/fe/site/forward/push?id=' + oMatter.id + '&type=' + oMatter.type, aTargets).success(function() {
+                            $mi.close();
+                        });
+                    }
+                };
+                $scope2.cancel = function() {
+                    $mi.dismiss();
+                };
+            }]
+        });
+    };
+    this.showSwitch = function(oUser, oMatter) {
+        var _this = this,
+            eSwitch;
+        eSwitch = document.createElement('div');
+        eSwitch.classList.add('tms-switch', 'tms-switch-forward');
+        eSwitch.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            $rootScope.$apply(function() {
+                if (!oUser.loginExpire) {
+                    tmsDynaPage.openPlugin('http://' + location.host + '/rest/site/fe/user/login?site=' + oMatter.siteid).then(function(data) {
+                        oUser.loginExpire = data.loginExpire;
+                        _this.open(oMatter);
+                    });
+                } else {
+                    _this.open(oMatter);
+                }
+            });
+        }, true);
+        document.body.appendChild(eSwitch);
+    };
+}]);
---
+++ models/matter/article/copy.php
@@ -0,0 +1,92 @@
+<?php
+namespace matter\article;
+/**
+ * 复制单图文
+ */
+class copy_model extends \TMS_MODEL {
+	/**
+	 * 跨团队转发单图文
+	 *
+	 * @param int $id
+	 * @param array [{siteid:'',channelId:''}]
+	 *
+	 */
+	public function forward($id, $aTargetSites, $oUser, $fromSiteId = null) {
+		$modelArt = $this->model('matter\article2');
+		$modelArt->setOnlyWriteDbConn(true);
+		$modelLog = $this->model('matter\log');
+		$modelTag = $this->model('tag');
+		$modelChn = $this->model('matter\channel');
+		$modelChn->setOnlyWriteDbConn(true);
+
+		$copied = $modelArt->byId($id);
+		empty($fromSiteId) && $fromSiteId = $copied->siteid;
+
+		/*获取原图文的内容标签*/
+		$tags = $modelTag->tagsByRes($copied->id, 'article', 0);
+		$current = time();
+
+		$newArticle = new \stdClass;
+		$newArticle->creater = $oUser->id;
+		$newArticle->creater_src = 'A';
+		$newArticle->creater_name = $modelArt->escape($oUser->name);
+		$newArticle->create_at = $current;
+		$newArticle->modifier = $oUser->id;
+		$newArticle->modifier_src = 'A';
+		$newArticle->modifier_name = $modelArt->escape($oUser->name);
+		$newArticle->modify_at = $current;
+		$newArticle->author = $modelArt->escape($oUser->name);
+		$newArticle->summary = $modelArt->escape($copied->summary);
+		/* 引用情况下，不复制文章的正文 */
+		//$newArticle->body = $modelArt->escape($copied->body);
+		$newArticle->body = '';
+		$newArticle->hide_pic = $copied->hide_pic;
+		$newArticle->url = $copied->url;
+		$newArticle->can_siteuser = $copied->can_siteuser;
+		$newArticle->from_siteid = $modelArt->escape($fromSiteId);
+		$newArticle->from_id = $modelArt->escape($id);
+		$newArticle->title = $modelArt->escape($copied->title);
+
+		/* 复制到其他团队 */
+		$newArticle->from_mode = 'C';
+		foreach ($aTargetSites as $targetSite) {
+			$targetSiteId = $modelArt->escape($targetSite->siteid);
+			$newArticle->siteid = $targetSiteId;
+			if ($copied->siteid === $targetSiteId) {
+				continue;
+			}
+			if (isset($newArticle->type)) {
+				unset($newArticle->type);
+			}
+			if (isset($newArticle->id)) {
+				unset($newArticle->id);
+			}
+			$newArticle->id = $modelArt->insert('xxt_article', $newArticle, true);
+
+			/*建立图文和内容标签之间的关系*/
+			if (!empty($tags)) {
+				foreach ($tags as $tag) {
+					//此操作如果放在循环外，第二次循环会出现$tag->id，因为在save方法中被加上了
+					unset($tag->id);
+				}
+				$modelTag->save($targetSiteId, $newArticle->id, 'article', 0, $tags, null);
+			}
+			$newArticle->type = 'article';
+
+			/* 放入指定的频道 */
+			if (!empty($targetSite->channelId)) {
+				$modelChn->addMatter($targetSite->channelId, $newArticle, $oUser->id, $oUser->name);
+			}
+
+			/* 记录操作日志 */
+			$modelLog->matterOp($targetSiteId, $oUser, $newArticle, 'C');
+
+			/* 增加原图文的复制数 */
+			if ($copied->siteid !== $targetSiteId) {
+				$modelArt->update("update xxt_article set copy_num = copy_num +1 where id = $id");
+			}
+		}
+
+		return true;
+	}
+}

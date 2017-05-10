09a6567c15be5e1d00ee4e6651b0b3695fc538c6
liao1a <153887111@qq.com>
2017-05-10 10:39:55 +0800

    修改1

--- schema/update/update2017050911.php
+++ schema/update/update2017050911.php
@@ -3,13 +3,13 @@ require_once '../../db.php';
 
 $sqls = array();
 //
-$sqls[] = "alter table xxt_site_favor add site_name varchar(50) not null after unionid";
+$sqls[] = "alter table xxt_site_favor add site_name varchar(50) not null after siteid";
 $sqls[] = "update xxt_site_favor f,xxt_site s set f.site_name = s.name where f.siteid=s.id";
 
-$sqls[] = "alter table xxt_site_friend_favor add from_site_name varchar(50) not null after matter_id";
+$sqls[] = "alter table xxt_site_friend_favor add from_site_name varchar(50) not null after from_siteid";
 $sqls[] = "update xxt_site_friend_favor f,xxt_site s set f.from_site_name = s.name where f.from_siteid=s.id";
 
-$sqls[] = "alter table xxt_site_contribute add from_site_name varchar(50) not null after creater";
+$sqls[] = "alter table xxt_site_contribute add from_site_name varchar(50) not null after from_siteid";
 $sqls[] = "update xxt_site_contribute c,xxt_site s set c.from_site_name = s.name where c.from_siteid=s.id";
 //
 foreach ($sqls as $sql) {

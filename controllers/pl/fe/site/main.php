20cb1475fc9f56ecf03ea72294a99744976dec3f
liao1a <153887111@qq.com>
2017-05-11 17:08:04 +0800

    增加1

--- controllers/pl/fe/site/main.php
+++ controllers/pl/fe/site/main.php
@@ -145,6 +145,10 @@ class main extends \pl\fe\base {
 			if (!empty($site->home_carousel)) {
 				$site->home_carousel = json_decode($site->home_carousel);
 			}
+			/* 团队群二维码 */
+			if (!empty($site->home_qrcode_group)) {
+				$site->home_qrcode_group = json_decode($site->home_qrcode_group);
+			}
 
 			return new \ResponseData($site);
 		} else {

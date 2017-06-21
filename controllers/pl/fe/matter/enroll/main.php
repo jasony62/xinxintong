4e10c6694e0fcab9c0b69d8e824be38d7009b109
lqqllt <lqqllt@163.com>
2017-06-21 15:02:16 +0800

    修改一些bug Signed-off-by: lqqllt <lqqllt@163.com>

--- controllers/pl/fe/matter/enroll/main.php
+++ controllers/pl/fe/matter/enroll/main.php
@@ -526,13 +526,14 @@ class main extends \pl\fe\matter\base {
 		$current = time();
 		$newapp = [];
 		$site = $this->model('site')->byId($site, ['fields' => 'id,heading_pic']);
-
+		
 		/* 从站点或任务获得的信息 */
 		if (empty($mission)) {
 			$newapp['pic'] = $site->heading_pic;
 			$newapp['summary'] = '';
 			$newapp['use_mission_header'] = 'N';
 			$newapp['use_mission_footer'] = 'N';
+			$mission=null;
 		} else {
 			$modelMis = $this->model('matter\mission');
 			$mission = $modelMis->byId($mission);
@@ -543,12 +544,13 @@ class main extends \pl\fe\matter\base {
 			$newapp['use_mission_footer'] = 'Y';
 		}
 		$appId = uniqid();
-
+		$customConfig=isset($config->customConfig) ? $config->customConfig : null;
 		empty($config->scenario) && $newapp['scenario'] = $scenario;
 		/* 登记数量限制 */
 		if (isset($config->count_limit)) {
 			$newapp['count_limit'] = $config->count_limit;
 		}
+
 		if (!empty($config->pages) && !empty($config->entryRule)) {
 			$this->_addPageByTemplate($user, $site, $mission, $appId, $config, $customConfig);
 			/*进入规则*/
@@ -564,6 +566,7 @@ class main extends \pl\fe\matter\base {
 		} else {
 			$entryRule = $this->_addBlankPage($user, $site->id, $appId);
 		}
+
 		if (empty($entryRule)) {
 			return new \ResponseError('没有获得页面进入规则');
 		}
@@ -587,6 +590,9 @@ class main extends \pl\fe\matter\base {
 		$modelApp = $this->model('matter\enroll');
 		$modelApp->setOnlyWriteDbConn(true);
 		$modelApp->insert('xxt_enroll', $newapp, false);
+		/* 保存数据 */
+		$records=$config->records;
+		$this->_persist($site->id,$appId,$records);
 
 		$app = $modelApp->byId($appId);
 		/* 记录操作日志 */
@@ -595,7 +601,7 @@ class main extends \pl\fe\matter\base {
 		if (isset($mission->id)) {
 			$modelMis->addMatter($user, $site->id, $mission->id, $app);
 		}
-
+		
 		return new \ResponseData($app);
 	}
 	/**
@@ -1446,6 +1452,7 @@ class main extends \pl\fe\matter\base {
 		 */
 		foreach ($pages as $page) {
 			$ap = $modelPage->add($user, $site->id, $app, (array) $page);
+
 			/**
 			 * 处理页面数据定义
 			 */
@@ -1493,6 +1500,7 @@ class main extends \pl\fe\matter\base {
 				$pageSchemas,
 				"aid='$app' and id={$ap->id}"
 			);
+
 			/* 填充页面 */
 			if (!empty($page->code)) {
 				$code = $page->code;
@@ -1648,20 +1656,39 @@ class main extends \pl\fe\matter\base {
 		if (false === ($user = $this->accountUser())) {
 			return new \ResponseTimeout();
 		}
-		$app = $this->model('matter\enroll')->byId($app);
 
+		$modelEnroll=\TMS_APP::M('matter\enroll');
+		$oApp = $modelEnroll->byId($app);
 		$template = new \stdClass;
 		/* setting */
-		!empty($app->scenario) && $template->scenario = $app->scenario;
-		$template->count_limit = $app->count_limit;
+		!empty($oApp->scenario) && $template->scenario = $oApp->scenario;
+		$template->count_limit = $oApp->count_limit;
 
 		/* schema */
-		$template->schema = json_decode($app->data_schemas);
+		$template->schema = json_decode($oApp->data_schemas);
+	
+		/* pages */
+		$template->pages=$oApp->pages;
+ 		
+ 		/* entry_rule */
+ 		$template->entryRule=$oApp->entry_rule;
+
+		/* records */
+		$records=$modelEnroll->query_objs_ss([
+			'id,userid,openid,nickname,data',
+			'xxt_enroll_record',
+			['siteid'=>$site,'aid'=>$app]
+		]);
+
+		foreach ($records as &$rec) {
+			$rec->data=json_decode($rec->data);
+		}
+		$template->records=$records;
 
 		$template = \TMS_MODEL::toJson($template);
 		header("Cache-Control: public");
 		header("Content-Description: File Transfer");
-		header('Content-disposition: attachment; filename=' . $app->title . '.json');
+		header('Content-disposition: attachment; filename=' . $oApp->title . '.json');
 		header("Content-Type: text/plain");
 		header('Content-Length: ' . strlen($template));
 		die($template);

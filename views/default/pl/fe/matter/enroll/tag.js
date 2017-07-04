3efd5db380979470fd763a151552a252076ee5f5
aYellowApple <2764411477@qq.com>
2017-07-04 15:14:58 +0800

    添加完成基本信息

--- views/default/pl/fe/matter/enroll/tag.js
+++ views/default/pl/fe/matter/enroll/tag.js
@@ -1,15 +1,34 @@
 define(['frame'], function(ngApp) {
     'use strict';
     ngApp.provider.controller('ctrlTag',['$scope', '$http', function($scope, $http) {
+        $scope.scopeNames = {
+            'U': '参与者',
+            'I': '发起人'
+        };
         $scope.page = {
             at: 1,
             size: 12,
             j: function() {
                 return '&page=' + this.at + '&size=' + this.size;
             }
-        }
+        };
         $scope.create = function() {
             console.log(1);
-        }
+        };
+        $scope.doSearch = function(page) {
+
+        };
+        $scope.edit = function(tag, index) {
+
+        };
+        $scope.remove = function(tag, index) {
+
+        };
+        $scope.up = function(tag, index) {
+
+        };
+        $scope.down = function(tag, index) {
+
+        };
     }]);
 });

ca46415f8b2c951562b3dfbe6268026f02135170
aYellowApple <2764411477@qq.com>
2017-07-05 14:17:12 +0800

    完善页面

--- views/default/pl/fe/matter/enroll/tag.js
+++ views/default/pl/fe/matter/enroll/tag.js
@@ -12,13 +12,20 @@ define(['frame'], function(ngApp) {
                 return '&page=' + this.at + '&size=' + this.size;
             }
         };
-        $scope.create = function() {
-            console.log(1);
+        $scope.createTag = function() {
+            var newTags;
+            if ($scope.model.newtag) {
+                newTags = $scope.model.newtag.replace(/\s/, ',');
+                newTags = newTags.split(',');
+                $http.post('/rest/site/fe/matter/enroll/tag/create?site=' + $scope.app.siteid + '&app=' + $scope.app.id, newTags).then(function(rsp) {
+                    rsp.data.forEach(function(oNewTag) {
+                        $scope.tags.push(oNewTag);
+                    });
+                });
+                $scope.model.newtag = '';
+            }
         };
         $scope.doSearch = function(page) {
-
-        };
-        $scope.edit = function(tag, index) {
 
         };
         $scope.remove = function(tag, index) {
@@ -29,6 +36,32 @@ define(['frame'], function(ngApp) {
         };
         $scope.down = function(tag, index) {
 
+        };
+        $scope.save = function() {
+            var filter = 'ID:' + $scope.app.id,
+                posted = [],
+                url, tag;
+
+            for (var k in $scope.tags) {
+                tag = $scope.tags[k];
+                if (tag.id || tag.content != 0) {
+                    var data;
+                    data = {
+                        act: tag.act,
+                        actor_delta: tag.actor_delta,
+                        matter_type: 'enroll',
+                        matter_filter: filter
+                    };
+                    tag.id && (data.id = tag.id);
+                    posted.push(data);
+                }
+            }
+            url = '/rest/pl/fe/matter/enroll/coin/saveRules?site=' + $scope.app.siteid;
+            http2.post(url, posted, function(rsp) {
+                for (var k in rsp.data) {
+                    $scope.tags[k].id = rsp.data[k];
+                }
+            });
         };
     }]);
 });

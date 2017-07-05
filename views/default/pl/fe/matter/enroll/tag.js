5a0e9c19ce943f1466b5b818c166d906d86c70eb
aYellowApple <2764411477@qq.com>
2017-07-05 18:50:47 +0800

    初步完成标签

--- views/default/pl/fe/matter/enroll/tag.js
+++ views/default/pl/fe/matter/enroll/tag.js
@@ -1,11 +1,13 @@
 define(['frame'], function(ngApp) {
     'use strict';
-    ngApp.provider.controller('ctrlTag',['$scope', '$http', function($scope, $http) {
+    ngApp.provider.controller('ctrlTag',['$scope', '$http', 'srvEnrollApp', 'noticebox', function($scope, $http, srvEnrollApp, noticebox){
+        var page;
         $scope.scopeNames = {
             'U': '参与者',
-            'I': '发起人'
+            'I': '发起人',
+            'P': '管理者'
         };
-        $scope.page = {
+        $scope.page = page = {
             at: 1,
             size: 12,
             j: function() {
@@ -14,54 +16,55 @@ define(['frame'], function(ngApp) {
         };
         $scope.createTag = function() {
             var newTags;
-            if ($scope.model.newtag) {
-                newTags = $scope.model.newtag.replace(/\s/, ',');
+            if ($scope.newtag) {
+                newTags = $scope.newtag.replace(/\s/, ',');
                 newTags = newTags.split(',');
-                $http.post('/rest/site/fe/matter/enroll/tag/create?site=' + $scope.app.siteid + '&app=' + $scope.app.id, newTags).then(function(rsp) {
-                    rsp.data.forEach(function(oNewTag) {
+                $http.post('/rest/pl/fe/matter/enroll/tag/create?app=' + $scope.app.id, newTags).then(function(rsp) {
+                    console.log(rsp.data);
+                    rsp.data.data.forEach(function(oNewTag) {
                         $scope.tags.push(oNewTag);
                     });
                 });
-                $scope.model.newtag = '';
+                $scope.newtag = '';
             }
         };
-        $scope.doSearch = function(page) {
-
-        };
-        $scope.remove = function(tag, index) {
-
-        };
-        $scope.up = function(tag, index) {
-
-        };
-        $scope.down = function(tag, index) {
-
-        };
-        $scope.save = function() {
-            var filter = 'ID:' + $scope.app.id,
-                posted = [],
-                url, tag;
-
-            for (var k in $scope.tags) {
-                tag = $scope.tags[k];
-                if (tag.id || tag.content != 0) {
-                    var data;
-                    data = {
-                        act: tag.act,
-                        actor_delta: tag.actor_delta,
-                        matter_type: 'enroll',
-                        matter_filter: filter
-                    };
-                    tag.id && (data.id = tag.id);
-                    posted.push(data);
-                }
-            }
-            url = '/rest/pl/fe/matter/enroll/coin/saveRules?site=' + $scope.app.siteid;
-            http2.post(url, posted, function(rsp) {
-                for (var k in rsp.data) {
-                    $scope.tags[k].id = rsp.data[k];
-                }
+        $scope.doSearch = function() {
+            $http.get('/rest/pl/fe/matter/enroll/tag/get?app=' + $scope.app.id + page.j()).then(function(rsp) {
+                $scope.tags = rsp.data.data.tags;
+                $scope.page.total = rsp.data.data.total;
             });
         };
+        $scope.removeTag = function(tag) {
+            if(window.confirm('确认删除')) {
+                $http.get('/rest/pl/fe/matter/enroll/tag/remove?tag=' + tag.id).then(function(rsp) {
+                    var i = $scope.tags.indexOf(tag);
+                    $scope.tags.splice(i, 1);
+                    $scope.page.total = $scope.page.total - 1;
+                });
+            }
+        };
+        $scope.modify = function(tag) {
+            update(tag, {label:tag.label});
+        }
+        $scope.upTag = function(tag, index) {
+            if (index === 0) return;
+            $scope.tags.splice(index, 1);
+            $scope.tags.splice(--index, 0, tag);
+            update(tag, {seq: 'U'});
+        };
+        $scope.downTag = function(tag, index) {
+            if (index === $scope.tags.length - 1)  return;
+            $scope.tags.splice(index, 1);
+            $scope.tags.splice(++index, 0, tag);
+            update(tag, {seq: 'D'});
+        };
+        $scope.update = function(tag, args) {
+            $http.post('/rest/pl/fe/matter/enroll/tag/update?tag=' + tag.id, args).then(function(rsp) {
+                console.log(rsp.data);
+            });
+        }
+        srvEnrollApp.get().then(function(app) {
+            $scope.doSearch();
+        });
     }]);
 });

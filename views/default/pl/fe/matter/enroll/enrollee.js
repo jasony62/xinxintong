d72bd547f9f4be2ddb7f5698d6619de9c51a4333
aYellowApple <2764411477@qq.com>
2017-07-11 18:08:05 +0800

    完成区分通讯录

--- views/default/pl/fe/matter/enroll/enrollee.js
+++ views/default/pl/fe/matter/enroll/enrollee.js
@@ -1,16 +1,39 @@
 define(['frame'], function(ngApp) {
     'use strict';
-    ngApp.provider.controller('ctrlEnrollee', ['$scope', 'http2', function($scope, http2) {
+    ngApp.provider.controller('ctrlEnrollee', ['$scope', 'http2', 'srvEnrollRecord', function($scope, http2, srvEnrollRecord) {
         var mschemas, oCriteria, page;
         $scope.mschemas = mschemas = [];
-        $scope.criteria = oCriteria = {};
+        $scope.criteria = oCriteria = {
+            allSelected: 'N',
+            selected: {},
+            reset: function() {
+                this.allSelected = 'N';
+                this.selected = {};
+            }
+        };
+        $scope.$watch('criteria.allSelected', function(nv) {
+            var index = 0;
+            if(nv == 'Y') {
+                while (index < $scope.members.length) {
+                    $scope.criteria.selected[index++] = true;
+                }
+            }else if(nv == 'N') {
+                $scope.criteria.selected = {};
+            }
+        });
+        $scope.export = function() {
+
+        };
+        $scope.notify = function(isBatch) {
+            srvEnrollRecord.notify(isBatch ? $scope.criteria : undefined);
+        };
         $scope.page = page = {
             at: 1,
             size: 20,
             j: function() {
                 return '&page=' + this.at + '&size=' + this.size;
             }
-        }
+        };
         $scope.seachEnrollee = function() {
             http2.post('/rest/pl/fe/matter/enroll/user/byMschema?site=' + $scope.app.siteid + '&app=' + $scope.app.id + '&mschema=' + oCriteria.mschema.id + page.j(), {}, function(rsp) {
                 $scope.members = rsp.data.members;
@@ -20,6 +43,10 @@ define(['frame'], function(ngApp) {
         $scope.$watch('app.entry_rule', function(oRule) {
             if (!oRule) return;
             if (oRule.scope !== 'member') {
+                http2.get('/rest/pl/fe/matter/enroll/user/enrollee?app=' + $scope.app.id, function(rsp) {
+                    $scope.members = rsp.data.users;
+                    $scope.page.total = rsp.data.total;
+                });
                 return;
             }
             var mschemaIds = Object.keys(oRule.member);

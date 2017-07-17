47c7b875a6f95bd60bb4787c6eb168735fd01666
aYellowApple <2764411477@qq.com>
2017-07-17 16:38:49 +0800

    添加我的站内消息页面

---
+++ views/default/site/fe/user/message/main.js
@@ -0,0 +1,25 @@
+define(['require', 'angular'], function(require, angular) {
+    'use strict';
+    var siteId, ngApp;
+    siteId = location.search.match('site=(.*)')[1];
+    ngApp = angular.module('app', ['ngSanitize', 'ui.bootstrap', 'service.matter']);
+    ngApp.controller('ctrlMain', ['$scope', '$http', 'srvUserNotice', function($scope, $http, srvUserNotice) {
+        $scope.closeNotice = function(log) {
+            srvUserNotice.closeNotice(log).then(function(rsp) {
+                $scope.notice.logs.splice($scope.notice.logs.indexOf(log), 1);
+                $scope.notice.page.total--;
+            });
+        };
+        srvUserNotice.uncloseList().then(function(result) {
+            $scope.notice = result;
+        });
+        $http.get('/rest/site/fe/get?site=' + siteId).success(function(rsp) {
+            $scope.site = rsp.data;
+            window.loading.finish();
+        });
+    }]);
+    /* bootstrap angular app */
+    require(['domReady!'], function(document) {
+        angular.bootstrap(document, ["app"]);
+    });
+});

79a8685dce58dd6ca4259cd3d49e95a88d49ade3
aYellowApple <2764411477@qq.com>
2017-07-18 10:07:17 +0800

    完善个人中心的消息

--- views/default/site/fe/user/message/main.js
+++ views/default/site/fe/user/message/main.js
@@ -11,18 +11,44 @@ define(['require', 'angular'], function(require, angular) {
         };
     }]);
     ngApp.controller('ctrlMain', ['$scope', 'http2', 'srvUserNotice', function($scope, http2, srvUserNotice) {
+        var page, filter;
+        $scope.page = page = {
+            at: 1,
+            size: 10,
+            j: function() {
+                return 'page=' + this.at + '&size=' + this.size;
+            }
+        };
+        $scope.filter = filter = {
+            type: 'all'
+        };
+        $scope.doSearch = function() {
+            var url;
+            if(filter.type=='all') {
+                url = '/rest/site/fe/user/message/list?' + page.j();
+                http2.get(url, function(rsp) {
+                    $scope.notice = rsp.data;
+                });
+            }else {
+                srvUserNotice.uncloseList().then(function(result) {
+                    $scope.notice = result;
+                    $scope.page.total = result.page.total;
+                });
+            }
+        };
         $scope.closeNotice = function(log) {
             srvUserNotice.closeNotice(log).then(function(rsp) {
                 $scope.notice.logs.splice($scope.notice.logs.indexOf(log), 1);
                 $scope.notice.page.total--;
             });
         };
-        srvUserNotice.uncloseList().then(function(result) {
-            $scope.notice = result;
-        });
         http2.get('/rest/site/fe/get?site=' + siteId, function(rsp) {
             $scope.site = rsp.data;
             window.loading.finish();
+        });
+        $scope.$watch('filter.type', function(nv) {
+            if(!nv) return;
+            $scope.doSearch();
         });
     }]);
     /* bootstrap angular app */

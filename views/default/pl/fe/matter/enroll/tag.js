6546cebbc3a0d6be08e0ea282d5c7f7d55a769cb
aYellowApple <2764411477@qq.com>
2017-07-25 16:53:57 +0800

    添加标签筛选

--- views/default/pl/fe/matter/enroll/tag.js
+++ views/default/pl/fe/matter/enroll/tag.js
@@ -1,13 +1,13 @@
 define(['frame'], function(ngApp) {
     'use strict';
     ngApp.provider.controller('ctrlTag',['$scope', '$http', 'srvEnrollApp', 'noticebox', function($scope, $http, srvEnrollApp, noticebox){
-        var page;
+        var page, oPage;
         $scope.scopeNames = {
             'S': '参与者',
             'P': '发起人',
             'O': '管理者'
         };
-        $scope.page = page = {
+        $scope.oPage = $scope.page = page = oPage = {
             at: 1,
             size: 12,
             j: function() {
@@ -20,7 +20,6 @@ define(['frame'], function(ngApp) {
                 newTags = $scope.newtag.replace(/\s/, ',');
                 newTags = newTags.split(',');
                 $http.post('/rest/pl/fe/matter/enroll/tag/create?app=' + $scope.app.id, newTags).then(function(rsp) {
-                    console.log(rsp.data);
                     rsp.data.data.forEach(function(oNewTag) {
                         $scope.tags.push(oNewTag);
                     });
@@ -32,6 +31,26 @@ define(['frame'], function(ngApp) {
             $http.get('/rest/pl/fe/matter/enroll/tag/get?app=' + $scope.app.id + page.j()).then(function(rsp) {
                 $scope.tags = rsp.data.data.tags;
                 $scope.page.total = rsp.data.data.total;
+            });
+        };
+        $scope.list4Schema = function(event, tag) {
+            event.preventDefault();
+            event.stopPropagation();
+            var url;
+            url = '/rest/pl/fe/matter/enroll/data/list4Schema?site=' + $scope.app.siteid + '&app=' + $scope.app.id;
+            url += '&page=' + oPage.at + '&size=' + oPage.size;
+            $http.post(url, {tag:tag.id}).then(function(result) {
+                oPage.total = result.data.total;
+                result.data.records.forEach(function(oRecord) {
+                    if (oRecord.tag) {
+                        oRecord.tag.forEach(function(index, tagId) {
+                            if (oApp._tagsById[index]) {
+                                oRecord.tag[tagId] = oApp._tagsById[index];
+                            }
+                        });
+                    }
+                });
+                $scope.records = result.data.records;
             });
         };
         $scope.removeTag = function(tag) {
@@ -64,6 +83,7 @@ define(['frame'], function(ngApp) {
             });
         }
         srvEnrollApp.get().then(function(app) {
+            $scope.app = app;
             $scope.doSearch();
         });
     }]);

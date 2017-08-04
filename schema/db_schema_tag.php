11c88900459c6d080b4ce6ca7e37c249de69b40e
liao1a <153887111@qq.com>
2017-08-04 15:43:22 +0800

    删除xxt_matter_tag

--- schema/db_schema_tag.php
+++ schema/db_schema_tag.php
@@ -19,18 +19,5 @@ if (!$mysqli->query($sql)) {
 	header('HTTP/1.0 500 Internal Server Error');
 	echo 'database error: ' . $mysqli->error;
 }
-/*
- * 素材分类标签
- */
-$sql = 'create table if not exists xxt_matter_tag(';
-$sql .= 'id int not null auto_increment';
-$sql .= ",siteid varchar(32) not null default ''";
-$sql .= ',title varchar(255) not null';
-$sql .= ",matter_type varchar(20)";
-$sql .= ",sub_type int not null default 0";
-$sql .= ',primary key(id)';
-if (!$mysqli->query($sql)) {
-	header('HTTP/1.0 500 Internal Server Error');
-	echo 'database error: ' . $mysqli->error;
-}
+
 echo 'finish tag.' . PHP_EOL;

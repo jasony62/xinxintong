97d2419b40129ce9be2c9a4aa0848215a9fc4efc
lqqllt <lqqllt@163.com>
2017-08-22 09:57:24 +0800

    List2 (#421)
    * 添加自定义文件
    Signed-off-by: lt <lqqllt@163.com>
    
    * 添加配置信息
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * test
    Signed-off-by: lt <lqqllt@163.com>
    
    * ok
    Signed-off-by: lt <lqqllt@163.com>
    
    * 测试
    Signed-off-by: lt <lqqllt@163.com>
    
    * 测试
    Signed-off-by: lt <lqqllt@163.com>
    
    * 忽略
    Signed-off-by: lt <lqqllt@163.com>
    
    * 忽略
    Signed-off-by: lt <lqqllt@163.com>
    
    * 忽略测试文件
    Signed-off-by: lt <lqqllt@163.com>
    
    * 将addressbook移到pl/fe/matter/下面 移动了对应的视图
    Signed-off-by: lt <lqqllt@163.com>
    
    * 将mp/app下边的addressbook移动到pl/fe/matter/addressbook
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改了参数
    Signed-off-by: lt <lqqllt@163.com>
    
    * 恢复无关代码
    Signed-off-by: lt <lqqllt@163.com>
    
    * 删除.gitignore文件
    Signed-off-by: lt <lqqllt@163.com>
    
    * 恢复无关的代码
    Signed-off-by: lt <lqqllt@163.com>
    
    * 恢复无关代码
    Signed-off-by: lt <lqqllt@163.com>
    
    * ok
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 恢复无关代码
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 解决登记活动输入内容换行时获取不到的问题
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 解决获取字符串不能转对象的问题
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 解决登记活动从数据库取不到数据的问题
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * Signed-off-by: lt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改bug能搜索特殊字符
    Signed-off-by: lt <lqqllt@163.com>
    
    * 增加了从数据库取数据解码html实体标签的方法
    Signed-off-by: lt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lt <lqqllt@163.com>
    
    * Merge branch 'master' of git://github.com/jasony62/xinxintong
    
    # Conflicts:
    #	tms/tms_model.php
    
    Signed-off-by: lintao li <lintao li>
    
    * Signed-off-by: lintao li <lintao li>
    
    * 列表页添加了用户列表名单
    
    Signed-off-by: lqqllt <lqqllt@163.com>
    
    * 修改一些bug
    
    Signed-off-by: lqqllt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lqqllt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lqqllt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lqqllt <lqqllt@163.com>
    
    * 修改一些bug
    
    Signed-off-by: lqqllt <lqqllt@163.com>
    
    * 修改一些bug
    Signed-off-by: lqqllt <lqqllt@163.com>

--- controllers/site/fe/matter/enroll/record.php
+++ controllers/site/fe/matter/enroll/record.php
@@ -690,6 +690,119 @@ class record extends base {
 		return new \ResponseData($rst);
 	}
 	/**
+	 *
+	 *
+	 * 列出填写人名单列表
+	 *
+	 *
+	 */
+	public function enrolleeList_action($site, $app, $owner='U',$schema_id, $page=1, $size=30) {
+		$modelEnl=$this->model('matter\enroll');
+		$oApp = $modelEnl->byId($app, ['cascaded' => 'N']);
+		if (false === $oApp) {
+			return new \ObjectNotFoundError();
+		}
+		$oUser = $this->who;
+		//参与者列表
+		$modelRnd=$this->model('matter\enroll\round');
+		$rnd=$modelRnd->getActive($oApp);
+		$rid=!empty($rnd) ? $rnd->rid : '';
+		//全部或分组
+		switch ($owner) {
+			case 'G':
+				$modelUsr = $this->model('matter\enroll\user');
+				$options = ['fields' => 'group_id'];
+				$oEnrollee = $modelUsr->byId($oApp, $oUser->uid, $options);
+				$group_id=isset($oEnrollee->group_id) ? $oEnrollee->group_id : '';
+				break;
+			default:
+				break;
+		}
+		//设定范围
+		$entry=$oApp->entry_rule->scope;
+		$q1=[
+			'*',
+			'xxt_enroll_user',
+			['siteid'=>$site, 'aid'=>$app, 'rid'=>$rid]
+		];
+		isset($group_id) && $q1[2]['group_id']=$group_id;
+		$q2['o']="id asc";
+		$q2['r']=['o' => ($page - 1) * $size, 'l' => $size];
+		if($users=$modelEnl->query_objs_ss($q1,$q2)){
+			foreach ($users as &$user) {
+				//添加分组信息
+				$dataSchemas=$oApp->dataSchemas;
+				foreach ($dataSchemas as $value) {
+					if($value->id=='_round_id'){
+						$ops=$value->ops;
+					}
+				}
+				if(isset($ops) && $user->group_id){
+					foreach ($ops as $p) {
+						if($user->group_id==$p->v){
+							$user->group=$p;
+						}
+					}
+				}
+				switch ($entry) {
+					case 'member':
+						//通信录的信息
+						if(empty($schema_id)){
+							return new \ResponseError('传入的通信录ID参数不能为空！');
+						}
+						
+						$addressbook=$modelEnl->query_obj_ss([
+							'*',
+							'xxt_site_member',
+							['siteid'=>$site,'userid'=>$user->userid,'schema_id'=>$schema_id]
+						]);
+			
+						if($addressbook) {
+							if(isset($schema_id)){
+								$schema=$modelEnl->query_obj_ss(['id,title,extattr','xxt_site_member_schema',['id'=>$schema_id]]);
+							}
+							$extattr=json_decode($addressbook->extattr);
+							$addressbook->schema_title=$schema->title;
+							if(!empty((array) $extattr)){
+								$schema->extattr=json_decode($schema->extattr);
+								$attr=array();
+								foreach ($schema->extattr as $v2) {
+									if(isset($extattr->{$v2->id})){
+										$addressbook->{$v2->id}=$extattr->{$v2->id};
+									}
+								}
+								$addressbook->extattr=$extattr;
+							}
+							$addressbook->enroll_num=$user->enroll_num;
+							$addressbook->remark_other_num=$user->remark_other_num;
+							$addressbook->like_other_num=$user->like_other_num;
+						}
+						$user->mschema=$addressbook;
+						break;
+					case 'sns':
+						//公众号的信息
+						$sns=$modelEnl->query_obj_ss([
+							'assoc_id,wx_openid,yx_openid,qy_openid,uname,headimgurl,ufrom,uid,unionid,nickname',
+							'xxt_site_account',
+							['siteid'=>$site,'uid'=>$user->userid]
+						]);
+						$user->sns=$sns;
+						break;
+					default:
+						# code...
+						break;
+				}
+			}
+		}
+
+		$result=new \stdClass;
+		$result->records=$users;
+		$q1[0]='count(*)';
+		$result->total=$modelEnl->query_val_ss($q1);
+
+		return new \ResponseData($result);	
+	}
+	/**
 	 * 点赞登记记录中的某一个题
 	 *
 	 * @param string $ek

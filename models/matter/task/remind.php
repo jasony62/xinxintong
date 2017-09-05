5403890dc88b36ee361492408e3a1a54e4c55362
liao1a <153887111@qq.com>
2017-09-05 08:47:18 +0800

    第一个版本用的batch中的方法，但是发现不能设定创建人

--- models/matter/task/remind.php
+++ models/matter/task/remind.php
@@ -10,6 +10,61 @@ class remind_model extends \TMS_MODEL {
 	 * @param
 	 */
 	public function exec($oMatter, $arguments = null) {
+		if ($oMatter->type === 'enroll') {
+			$modelEnl = $this->model('matter\enroll');
+			$oMatter = $modelEnl->byId($oMatter->id, ['cascaded' => 'N']);
+			if (false === $oMatter) {
+				return [false, '指定的活动不存在'];
+			}
+
+			$modelRec = $this->model('matter\enroll\record');
+			/* 获得活动的进入链接 */
+			$appURL = $oMatter->entryUrl;
+			$modelQurl = $this->model('q\url');
+			$noticeURL = $modelQurl->urlByUrl($oMatter->siteid, $appURL);
+			$params = new \stdClass;
+			$params->url = $noticeURL;
+
+			$options = [
+				'rid' => null,
+			];
+			$users = $modelRec->enrolleeByApp($oMatter, $options);
+			if (count($users) === 0) {
+				return [false, '没有填写人'];
+			}
+
+			/*获取模板消息id*/
+			$oNotice = $this->model('site\notice')->byName($oMatter->siteid, 'timer.enroll.remind', ['onlySite' => false]);
+			if ($oNotice === false) {
+				return [false, '没有指定事件的模板消息'];
+			}
+			$oTmplConfig = $this->model('matter\tmplmsg\config')->byId($oNotice->tmplmsg_config_id);
+
+			$tmplmsgId = $oTmplConfig->msgid;
+
+			$receivers = [];
+			$receiverUnique = [];//去重
+			foreach ($users as $user) {
+				if(in_array($user->userid, $receiverUnique)){
+					continue;
+				}
+				$receiverUnique[] = $user->userid;
+
+				$receiver = new \stdClass;
+				isset($user->enroll_key) && $receiver->assoc_with = $user->enroll_key;
+				$receiver->userid = $user->userid;
+				$receivers[] = $receiver;
+			}
+
+			// $user = $this->accountUser();
+			// $modelTmplBat = $this->model('matter\tmplmsg\batch');
+			// $creater = new \stdClass;
+			// $creater->uid = $user->id;
+			// $creater->name = $user->name;
+			// $creater->src = 'pl';
+			// $modelTmplBat->send($oMatter->siteid, $tmplmsgId, $creater, $receivers, $params, ['send_from' => 'enroll:' . $oMatter->id]);
+		}
+
 		return [true];
 	}
 }

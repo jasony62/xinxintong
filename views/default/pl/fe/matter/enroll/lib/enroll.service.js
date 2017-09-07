6f9536717c0179d41574aff78afecd2f2c834c3e
aYellowApple <2764411477@qq.com>
2017-09-07 10:50:26 +0800

    修改冲突的键

--- views/default/pl/fe/matter/enroll/lib/enroll.service.js
+++ views/default/pl/fe/matter/enroll/lib/enroll.service.js
@@ -45,7 +45,7 @@ define(['require', 'schema', 'page'], function(require, schemaLib, pageLib) {
                     rid: '',
                     verified: ''
                 },
-                orderby: {
+                order: {
                     orderby: '',
                     schemaId: ''
                 },

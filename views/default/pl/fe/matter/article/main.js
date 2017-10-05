c4bdd0558f45888afa2234d677876e888881e4ed
Jason Young <jasony62@qq.com>
2017-10-05 13:16:43 +0800

    update code

--- views/default/pl/fe/matter/article/main.js
+++ views/default/pl/fe/matter/article/main.js
@@ -1,31 +1,8 @@
 define(['frame'], function(ngApp) {
-    ngApp.provider.controller('ctrlMain', ['$scope', '$uibModal', 'http2', 'noticebox', 'srvSite', 'mediagallery', 'noticebox', 'srvApp', 'cstApp', 'tmsThumbnail', '$timeout', 'srvTag', function($scope, $uibModal, http2, noticebox, srvSite, mediagallery, noticebox, srvApp, cstApp, tmsThumbnail, $timeout, srvTag) {
-        (function() {
-            new ZeroClipboard(document.querySelectorAll('.text2Clipboard'));
-        })();
-        $scope.downloadQrcode = function(url) {
-            $('<a href="' + url + '" download="' + $scope.editing.title + '二维码.png"></a>')[0].click();
-        };
-        var tinymceEditor, modifiedData = {};
+    ngApp.provider.controller('ctrlMain', ['$scope', '$uibModal', 'http2', 'noticebox', 'srvSite', 'mediagallery', 'noticebox', 'srvApp', 'tmsThumbnail', '$timeout', 'srvTag', function($scope, $uibModal, http2, noticebox, srvSite, mediagallery, noticebox, srvApp, tmsThumbnail, $timeout, srvTag) {
+        var modifiedData = {};
 
         $scope.modified = false;
-        $scope.innerlinkTypes = cstApp.innerlink;
-        window.onbeforeunload = function(e) {
-            var message;
-            if ($scope.modified) {
-                message = '修改还没有保存，是否要离开当前页面？',
-                    e = e || window.event;
-                if (e) {
-                    e.returnValue = message;
-                }
-                return message;
-            }
-            if ($scope.editing) {
-                if (!$scope.editing.pic && !$scope.editing.thumbnail) {
-                    tmsThumbnail.thumbnail($scope.editing);
-                }
-            }
-        };
         $scope.submit = function() {
             http2.post('/rest/pl/fe/matter/article/update?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id, modifiedData, function() {
                 modifiedData = {};
@@ -66,173 +43,7 @@ define(['frame'], function(ngApp) {
         $scope.choosePhase = function() {
             srvApp.choosePhase();
         };
-        $scope.$on('tinymce.multipleimage.open', function(event, callback) {
-            var options = {
-                callback: callback,
-                multiple: true,
-                setshowname: true
-            };
-            mediagallery.open($scope.editing.siteid, options);
-        });
-        $scope.embedMatter = function() {
-            var options = {
-                matterTypes: $scope.innerlinkTypes,
-                singleMatter: true
-            };
-            if ($scope.editing.mission) {
-                options.mission = $scope.editing.mission;
-            }
-            srvSite.openGallery(options).then(function(result) {
-                var editor = tinymce.get('body1'),
-                    dom = editor.dom,
-                    selection = editor.selection,
-                    sibling, domMatter, fn, style;
-
-                style = "cursor:pointer";
-                if (selection && selection.getNode()) {
-                    /*选中了页面上已有的元素*/
-                    sibling = selection.getNode();
-                    if (sibling !== editor.getBody()) {
-                        while (sibling.parentNode !== editor.getBody()) {
-                            sibling = sibling.parentNode;
-                        }
-                        angular.forEach(result.matters, function(matter) {
-                            fn = "openMatter($event,'" + matter.id + "','" + result.type + "')";
-                            domMatter = dom.create('p', {
-                                'wrap': 'matter'
-                            }, dom.createHTML('span', {
-                                "ng-click": fn,
-                                "style": style
-                            }, dom.encode(matter.title)));
-                            dom.insertAfter(domMatter, sibling);
-                            selection.setCursorLocation(domMatter, 0);
-                        });
-                    } else {
-                        /*没有选中页面上的元素*/
-                        angular.forEach(result.matters, function(matter) {
-                            fn = "openMatter($event,'" + matter.id + "','" + result.type + "')";
-                            domMatter = dom.add(editor.getBody(), 'p', {
-                                'wrap': 'matter'
-                            }, dom.createHTML('span', {
-                                "ng-click": fn,
-                                "style": style
-                            }, dom.encode(matter.title)));
-                            selection.setCursorLocation(domMatter, 0);
-                        });
-                    }
-                    editor.focus();
-                }
-            });
-        };
-        var insertLink = function(data) {
-            var editor, dom, html;
-            if (data.url.length > 0) {
-                editor = tinymce.get('body1');
-                dom = editor.dom;
-                html = dom.createHTML('p', {},
-                    dom.createHTML('a', {
-                            style: 'display:block',
-                            href: data.url,
-                        }, dom.encode(data.text)));
-                editor.insertContent('<p>&nbsp;</p>' + html + '<p>&nbsp;</p>');
-            }
-        }
-        $scope.embedLink = function() {
-            $uibModal.open({
-                templateUrl: 'insertMedia.html',
-                controller: ['$uibModalInstance', '$scope', function($mi, $scope) {
-                    $scope.embedType = 'link';
-                    $scope.data = {
-                        url: '',
-                        text: ''
-                    };
-                    $scope.cancel = function() {
-                        $mi.dismiss()
-                    };
-                    $scope.ok = function() {
-                        $mi.close($scope.data)
-                    };
-                }],
-                backdrop: 'static',
-            }).result.then(function(data) {
-                insertLink(data);
-            });
-        }
-        var insertVideo = function(url) {
-            var editor, dom, html;
-            if (url.length > 0) {
-                editor = tinymce.get('body1');
-                dom = editor.dom;
-                html = dom.createHTML('p', {},
-                    dom.createHTML(
-                        'video', {
-                            style: 'width:100%',
-                            controls: "controls",
-                        },
-                        dom.createHTML(
-                            'source', {
-                                src: url,
-                                type: "video/mp4",
-                            }
-                        )
-                    )
-                );
-                editor.insertContent('<p>&nbsp;</p>' + html + '<p>&nbsp;</p>');
-            }
-        };
-        $scope.embedVideo = function() {
-            $uibModal.open({
-                templateUrl: 'insertMedia.html',
-                controller: ['$uibModalInstance', '$scope', function($mi, $scope) {
-                    $scope.embedType = 'video';
-                    $scope.data = {
-                        url: ''
-                    };
-                    $scope.cancel = function() {
-                        $mi.dismiss()
-                    };
-                    $scope.ok = function() {
-                        $mi.close($scope.data)
-                    };
-                }],
-                backdrop: 'static',
-            }).result.then(function(data) {
-                insertVideo(data.url);
-            });
-        };
-        var insertAudio = function(url) {
-            var editor, dom, html;
-            if (url.length > 0) {
-                editor = tinymce.get('body1');
-                dom = editor.dom;
-                html = dom.createHTML('p', {}, dom.createHTML('audio', {
-                    src: url,
-                    controls: "controls",
-                }));
-                editor.insertContent('<p>&nbsp;</p>' + html + '<p>&nbsp;</p>');
-            }
-        };
-        $scope.embedAudio = function() {
-            $uibModal.open({
-                templateUrl: 'insertMedia.html',
-                controller: ['$uibModalInstance', '$scope', function($mi, $scope) {
-                    $scope.embedType = 'audio';
-                    $scope.data = {
-                        url: ''
-                    };
-                    $scope.cancel = function() {
-                        $mi.dismiss()
-                    };
-                    $scope.ok = function() {
-                        $mi.close($scope.data)
-                    };
-                }],
-                backdrop: 'static',
-            }).result.then(function(data) {
-                insertAudio(data.url);
-            });
-        };
-        $scope.tagMatter = function(subType){
+        $scope.tagMatter = function(subType) {
             var oTags;
             if (subType === 'C') {
                 oTags = $scope.oTagC;
@@ -241,51 +52,6 @@ define(['frame'], function(ngApp) {
             }
             srvTag._tagMatter($scope.editing, oTags, subType);
         };
-        $scope.delAttachment = function(index, att) {
-            http2.get('/rest/pl/fe/matter/article/attachment/del?site=' + $scope.editing.siteid + '&id=' + att.id, function success(rsp) {
-                $scope.editing.attachments.splice(index, 1);
-            });
-        };
-        $scope.downloadUrl = function(att) {
-            return '/rest/site/fe/matter/article/attachmentGet?site=' + $scope.editing.siteid + '&articleid=' + $scope.editing.id + '&attachmentid=' + att.id;
-        };
-        $scope.$watch('editing', function(editing) {
-            if (!editing) return;
-            if (tinymceEditor) {
-                tinymceEditor.setContent(editing.body);
-            }
-            var r = new Resumable({
-                target: '/rest/pl/fe/matter/article/attachment/upload?site=' + $scope.editing.siteid + '&articleid=' + $scope.editing.id,
-                testChunks: false,
-            });
-            r.assignBrowse(document.getElementById('addAttachment'));
-            r.on('fileAdded', function(file, event) {
-                $scope.$apply(function() {
-                    noticebox.progress('开始上传文件');
-                });
-                r.upload();
-            });
-            r.on('progress', function(file, event) {
-                $scope.$apply(function() {
-                    noticebox.progress('正在上传文件：' + Math.floor(r.progress() * 100) + '%');
-                });
-            });
-            r.on('complete', function() {
-                var f, lastModified, posted;
-                f = r.files.pop().file;
-                lastModified = f.lastModified ? f.lastModified : (f.lastModifiedDate ? f.lastModifiedDate.getTime() : 0);
-                posted = {
-                    name: f.name,
-                    size: f.size,
-                    type: f.type,
-                    lastModified: lastModified,
-                    uniqueIdentifier: f.uniqueIdentifier,
-                };
-                http2.post('/rest/pl/fe/matter/article/attachment/add?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id, posted, function success(rsp) {
-                    $scope.editing.attachments.push(rsp.data);
-                });
-            });
-        });
         // 更改缩略图
         $scope.$watch('editing.title', function(title, oldTitle) {
             if ($scope.editing && title && oldTitle) {
@@ -296,26 +62,5 @@ define(['frame'], function(ngApp) {
                 }
             }
         });
-        $scope.$on('tinymce.instance.init', function(event, editor) {
-            tinymceEditor = editor;
-            if ($scope.editing) {
-                editor.setContent($scope.editing.body);
-            }
-        });
-        $scope.$on('tinymce.content.change', function(event, changed) {
-            var content;
-            content = tinymceEditor.getContent();
-            if (content !== $scope.editing.body) {
-                $scope.editing.body = content;
-                modifiedData['body'] = encodeURIComponent(content);
-                $scope.modified = true;
-            }
-        });
-        $scope.applyToHome = function() {
-            var url = '/rest/pl/fe/matter/home/apply?site=' + $scope.editing.siteid + '&type=article&id=' + $scope.editing.id;
-            http2.get(url, function(rsp) {
-                noticebox.success('完成申请！');
-            });
-        };
     }]);
 });

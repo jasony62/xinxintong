ebf3ca94d598b56b03c68b5152eb15aef745e4fa
Jason Young <jasony62@qq.com>
2017-10-23 13:45:21 +0800

    update code

--- controllers/site/home.php
+++ controllers/site/home.php
@@ -28,9 +28,12 @@ class home extends base {
 			$modelCode = $this->model('code\page');
 			$home_page = $modelCode->lastPublishedByName($oSite->id, $oSite->home_page_name, ['fields' => 'id,create_at']);
 
-			$templateDirHtml = TMS_APP_TEMPLATE . '/pl/fe/site/page/home/' . $template . '.html';
-			$templateDirCss = TMS_APP_TEMPLATE . '/pl/fe/site/page/home/' . $template . '.css';
-			$templateDirJs = TMS_APP_TEMPLATE . '/pl/fe/site/page/home/' . $template . '.js';
+			$templateDir = file_exists(TMS_APP_TEMPLATE . '/pl/fe/site/page/home') ? TMS_APP_TEMPLATE : TMS_APP_TEMPLATE_DEFAULT;
+			$templateDir .= '/pl/fe/site/page/home';
+
+			$templateDirHtml = $templateDir . '/' . $template . '.html';
+			$templateDirCss = $templateDir . '/' . $template . '.css';
+			$templateDirJs = $templateDir . '/' . $template . '.js';
 			$createAtTemplateHtml = filemtime($templateDirHtml);
 			$createAtTemplateCss = filemtime($templateDirCss);
 			$createAtTemplateJs = filemtime($templateDirJs);

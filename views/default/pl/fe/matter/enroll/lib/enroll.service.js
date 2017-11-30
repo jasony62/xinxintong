151ab457403526816d1e6515ee317d4ef6ef278c
aGreenApple <2764411477@qq.com>
2017-11-30 14:56:08 +0800

    修改改_checked报错

--- views/default/pl/fe/matter/enroll/lib/enroll.service.js
+++ views/default/pl/fe/matter/enroll/lib/enroll.service.js
@@ -1365,7 +1365,7 @@ define(['require', 'schema', 'page'], function(require, schemaLib, pageLib) {
      * op round
      */
     ngModule.provider('srvOpEnrollRound', function() {
-        var _siteId, _appId, _accessId, _rounds, _oPage,
+        var _siteId, _appId, _accessId, _rounds, _oPage, _checked,
             _RestURL = '/rest/site/op/matter/enroll/round/',
             RoundState = ['新建', '启用', '停止'];
 

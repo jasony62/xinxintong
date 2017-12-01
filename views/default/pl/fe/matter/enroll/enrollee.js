c0e8ad070de19977ae52fca3bbc24f9889747a61
aGreenApple <2764411477@qq.com>
2017-12-01 11:36:01 +0800

    添加登记活动缺席记录

--- views/default/pl/fe/matter/enroll/enrollee.js
+++ views/default/pl/fe/matter/enroll/enrollee.js
@@ -2,7 +2,7 @@ define(['frame'], function(ngApp) {
     'use strict';
     ngApp.provider.controller('ctrlEnrollee', ['$scope', 'http2', 'srvEnrollRecord', '$q', '$uibModal', function($scope, http2, srvEnrollRecord, $q, $uibModal) {
         function _absent() {
-            http2.get('/rest/pl/fe/matter/enroll/user/absent?site=' + $scope.app.siteid + '&app=' + $scope.app.id, function(rsp) {
+            http2.get('/rest/pl/fe/matter/enroll/user/absent?site=' + $scope.app.siteid + '&app=' + $scope.app.id + '&rid=' + _oCriteria.rid, function(rsp) {
                 $scope.absentUsers = rsp.data.users;
             });
         }
@@ -33,6 +33,52 @@ define(['frame'], function(ngApp) {
                 this.selected = {};
                 this.count = 0;
             }
+        };
+        $scope.editCause = function(user) {
+            $uibModal.open({
+                templateUrl: 'editCause.html',
+                controller: ['$scope', '$uibModalInstance', 'http2', function($scope2, $mi, http2){
+                    $scope2.cause = '';
+                    $scope2.app = $scope.app;
+                    $scope2.criteria = _oCriteria;
+                    $scope2.page = {
+                        at: 1,
+                        size: 5,
+                        j: function() {
+                            return '&page=' + this.at + '&size=' + this.size;
+                        }
+                    };
+                    $scope2.doSearchRound = function() {
+                        http2.get('/rest/pl/fe/matter/enroll/round/list?site=' + $scope.app.siteid + '&app=' + $scope.app.id + $scope2.page.j(), function(rsp) {
+                            $scope2.rounds = rsp.data.rounds;
+                            $scope2.page.total = rsp.data.total;
+                        });
+                    }
+                    $scope2.cancel = function() {
+                        $mi.dismiss();
+                    };
+                    $scope2.ok = function() {
+                        var url, params = {
+                            rid: $scope2.criteria.rid,
+                            absent_cause: $scope2.cause
+                        };
+                        url = '/rest/pl/fe/matter/enroll/update?site=' + $scope.app.siteid + '&app=' + $scope.app.id;
+                        http2.post(url, {'userid': params}, function(rsp) {
+                            angular.forEach($scope2.rounds, function(round) {
+                                if(round.rid == $scope2.criteria.rid){
+                                    params.round_title = round.title;
+                                }
+                            });
+                            $mi.close(params);
+                        });
+                    };
+                    $scope2.doSearchRound();
+                }],
+                backdrop: 'static'
+            }).result.then(function(result){
+                user.absent_cause.round_title = result.round_title;
+                user.absent_cause.cause = result.absent_cause;
+            });
         };
         $scope.chooseOrderby = function(orderby) {
             _oCriteria.orderby = orderby;
@@ -78,6 +124,7 @@ define(['frame'], function(ngApp) {
                 backdrop: 'static',
             }).result.then(function(_oCriteria) {
                 $scope.searchEnrollee(1);
+                _absent();
             });
         };
         $scope.searchEnrollee = function(pageAt) {
@@ -86,6 +133,7 @@ define(['frame'], function(ngApp) {
             pageAt && (page.at = pageAt);
             url = '/rest/pl/fe/matter/enroll/user/enrollee?app=' + $scope.app.id + page.j();
             http2.post(url, _oCriteria, function(rsp) {
+                console.log(_oCriteria);
                 srvEnrollRecord.init($scope.app, $scope.page, $scope.criteria, rsp.data.users);
                 rsp.data.users.forEach(function(user) {
                     if (user.tmplmsg && user.tmplmsg.status) {

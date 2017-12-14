04bfe4f760165783a2f5227fc18ff1d00e4fee37
aGreenApple <2764411477@qq.com>
2017-12-14 17:40:54 +0800

    modify login link

--- asset/js/xxt.ui.forward.js
+++ asset/js/xxt.ui.forward.js
@@ -97,7 +97,7 @@ ngMod.service('tmsForward', ['$rootScope', '$http', '$q', 'tmsDynaPage', 'tmsMod
             event.stopPropagation();
             $rootScope.$apply(function() {
                 if (!oUser.loginExpire) {
-                    tmsDynaPage.openPlugin('http://' + location.host + '/rest/site/fe/user/login?site=' + oMatter.siteid).then(function(data) {
+                    tmsDynaPage.openPlugin('http://' + location.host + '/rest/site/fe/user/access?site=platform#login').then(function(data) {
                         oUser.loginExpire = data.loginExpire;
                         _this.open(oMatter);
                     });

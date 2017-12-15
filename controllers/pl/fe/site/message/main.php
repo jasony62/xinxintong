a7abd1e0c48026e9a9b62f6a6854eb96b8e2cd8d
liao1a <153887111@qq.com>
2017-12-15 08:26:59 +0800

    sql注入漏洞 (#504)
    * url地址
    
    * 页面地址变动
    
    * 添加被删除的构造
    
    * 修改路径（1）
    
    * 移动
    
    * 移动model
    
    * 移动（1）
    
    * 移动并更改路径（1）
    
    * 数组键写的有问题
    
    * 1
    
    * update code
    
    * 删除不必要的文件
    
    * 創建模版所需的数据库表增加登记活动模板表
    
    * Revert "創建模版所需的数据库表增加登记活动模板表"
    
    This reverts commit fb0e098ad0a57076aaabc6e69c93fcca4b6a0093.
    
    * 在签到活动中增加发送消息的功能
    
    * Revert "在签到活动中增加发送消息的功能"
    
    This reverts commit e856720411d7138e1deecb8945672a7fd6c25402.
    
    * 解决sql注入的问题

--- controllers/pl/fe/site/message/main.php
+++ controllers/pl/fe/site/message/main.php
@@ -30,20 +30,21 @@ class main extends \pl\fe\base {
      */
     public function get_action($site, $keyword='', $page=1, $size=30, $amount=null) 
     {
+        $model = $this->model();
         $q = array(
             'l.id,l.openid,l.nickname,l.create_at,l.data',
             'xxt_log_mpreceive l', 
             "l.mpid='$site' and l.type='text'"
         );
-        !empty($keyword) && $q[2] .= " and data like '%$keyword%'";
+        !empty($keyword) && $q[2] .= " and data like '%" . $model->escape($keyword) . "%'";
 
         $q2['o'] = 'create_at desc';
         $q2['r'] = array('o'=>($page-1)*$size, 'l'=>$size);
 
-        if ($messages = $this->model()->query_objs_ss($q, $q2)) {
+        if ($messages = $model->query_objs_ss($q, $q2)) {
             if (empty($amount)) {
                 $q[0] = 'count(*)';
-                $amount = (int)$this->model()->query_val_ss($q);
+                $amount = (int)$model->query_val_ss($q);
             }
             return new \ResponseData(array($messages, $amount)); 
         }

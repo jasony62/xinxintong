af812a5efbca038762d3f653308e591534628704
Jason Young <jasony62@qq.com>
2018-01-09 13:49:40 +0800

    update code

--- models/sns/wx/call/qrcode.php
+++ models/sns/wx/call/qrcode.php
@@ -50,8 +50,14 @@ class qrcode_model extends \TMS_MODEL {
 			'xxt_call_qrcode_wx',
 			['siteid' => $siteId, 'scene_id' => $sceneId],
 		];
-
 		$call = $this->query_obj_ss($q);
+		if (false === $call && $sceneId !== 'platform') {
+			$q = [
+				'*',
+				'xxt_call_qrcode_wx',
+				['siteid' => 'platform', 'scene_id' => $sceneId],
+			];
+		}
 
 		return $call;
 	}

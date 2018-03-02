7ab18b8919c714a13d278302f9a412f4e2d7569f
liao1a <153887111@qq.com>
2018-03-02 15:01:32 +0800

    22

--- controllers/api/invite.php
+++ controllers/api/invite.php
@@ -22,13 +22,14 @@ class invite extends base {
 			return new \ResponseError($checkRes[1]);
 		}
 
+		$model = $this->model();
 		$q = [
 			'a.token,l.matter_id,l.matter_type,l.userid,l.nickname,l.use_at',
 			'xxt_invite_access a,xxt_invite_log l',
-			"a.token = '" . $modelInv->escape($inviteToken) . "' and a.invite_log_id = l.id"
+			"a.token = '" . $model->escape($inviteToken) . "' and a.invite_log_id = l.id"
 		];
 
-		$user = $modelInv->query_obj_ss($q);
+		$user = $model->query_obj_ss($q);
 
 		return new \ResponseData($user);
 	}

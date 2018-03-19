5e0d34f0bb56ccc4ee39c5221dd7fde32b8ad161
littleSa <2764411477@qq.com>
2018-03-19 21:51:06 +0800

    Channel0315 (#578)
    * split file
    
    * add usercenter and invite
    
    * update code
    
    * modify logic
    
    * add invite
    
    * modify logic

--- views/default/pl/fe/matter/channel/main.js
+++ views/default/pl/fe/matter/channel/main.js
@@ -1,5 +1,5 @@
 define(['frame'], function(ngApp) {
-    ngApp.provider.controller('ctrlMain', ['$scope', 'http2', 'srvSite', 'noticebox', '$uibModal', 'srvTag', 'cstApp', function($scope, http2, srvSite, noticebox, $uibModal, srvTag, cstApp) {
+    ngApp.provider.controller('ctrlMain', ['$scope', 'http2', 'srvSite', '$uibModal', 'srvTag', 'cstApp', function($scope, http2, srvSite, $uibModal, srvTag, cstApp) {
         function arrangeMatters() {
             $scope.matters = $scope.editing.matters;
             if ($scope.editing.top_type) {
@@ -34,12 +34,6 @@ define(['frame'], function(ngApp) {
         $scope.matterTypes = cstApp.matterTypes;
         $scope.acceptMatterTypes = cstApp.acceptMatterTypes;
         $scope.volumes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-        $scope.applyToHome = function() {
-            var url = '/rest/pl/fe/matter/home/apply?site=' + $scope.siteId + '&type=channel&id=' + $scope.id;
-            http2.get(url, function(rsp) {
-                noticebox.success('完成申请！');
-            });
-        }
         $scope.update = function(name) {
             var modifiedData = {};
             modifiedData[name] = $scope.editing[name];
@@ -127,37 +121,6 @@ define(['frame'], function(ngApp) {
         };
         $scope.gotoMatter = function(matter) {
             location.href = '/rest/pl/fe/matter/' + matter.type + '?site=' + $scope.siteId + '&id=' + matter.id;
-        };
-        $scope.editPage = function(event, page) {
-            event.preventDefault();
-            event.stopPropagation();
-            var prop = page + '_page_name',
-                codeName = $scope.editing[prop];
-            if (codeName && codeName.length) {
-                location.href = '/rest/pl/fe/code?site=' + $scope.siteId + '&name=' + codeName;
-            } else {
-                http2.get('/rest/pl/fe/matter/channel/pageCreate?site=' + $scope.siteId + '&id=' + $scope.id + '&page=' + page, function(rsp) {
-                    $scope.editing[prop] = rsp.data.name;
-                    location.href = '/rest/pl/fe/code?site=' + $scope.siteId + '&name=' + rsp.data.name;
-                });
-            }
-        };
-        $scope.resetPage = function(event, page) {
-            event.preventDefault();
-            event.stopPropagation();
-            if (window.confirm('重置操作将覆盖已经做出的修改，确定重置？')) {
-                var codeName = $scope.editing[page + '_page_name'];
-                if (codeName && codeName.length) {
-                    http2.get('/rest/pl/fe/matter/channel/pageReset?site=' + $scope.siteId + '&id=' + $scope.id + '&page=' + page, function(rsp) {
-                        location.href = '/rest/pl/fe/code?site=' + $scope.siteId + '&name=' + codeName;
-                    });
-                } else {
-                    http2.get('/rest/pl/fe/matter/channel/pageCreate?site=' + $scope.siteId + '&id=' + $scope.id + '&page=' + page, function(rsp) {
-                        $scope.editing[prop] = rsp.data.name;
-                        location.href = '/rest/pl/fe/code?site=' + $scope.siteId + '&name=' + rsp.data.name;
-                    });
-                }
-            }
         };
         $scope.tagMatter = function(subType) {
             var oTags;

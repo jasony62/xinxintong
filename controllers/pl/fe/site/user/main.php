91691e29457bf9d6cf0f718eda1d0ecb9f6e0387
liao1a <153887111@qq.com>
2018-04-26 09:00:40 +0800

    bug

---
+++ controllers/pl/fe/site/user/main.php
@@ -0,0 +1,16 @@
+<?php
+namespace pl\fe\site\user;
+
+require_once dirname(dirname(dirname(__FILE__))) . '/base.php';
+/*
+ * 投稿活动主控制器
+ */
+class coin extends \pl\fe\base {
+	/**
+	 *
+	 */
+	public function index_action() {
+		\TPL::output('/pl/fe/site/frame');
+		exit;
+	}
+}

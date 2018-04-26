970ffa89e8f1a4072ebba262294a70f54f9936a1
aYellowApple <2764411477@qq.com>
2018-04-26 08:51:50 +0800

    modify user

--- views/default/pl/fe/user/doc.js
+++ views/default/pl/fe/user/doc.js
@@ -1,7 +1,11 @@
 define(['frame'], function(ngApp) {
     'use strict';
     ngApp.provider.controller('ctrlDoc', ['$scope', 'http2', function($scope, http2) {
-        var _oPage;
+        var _oPage, _oCriteria;
+        $scope.criteria = _oCriteria = {
+            start: '',
+            end: ''
+        };
         $scope.page = _oPage = {
             at: 1,
             size: 30,
@@ -11,11 +15,25 @@ define(['frame'], function(ngApp) {
         };
         //分页
         $scope.doSearch = function() {
-            http2.get('/rest/pl/fe/user/readList?site='+ $scope.siteId +'&uid=' + $scope.userId + _oPage.j(), function(rsp) {
+            var url;
+            url = '/rest/pl/fe/user/readList?site='+ $scope.siteId +'&uid=' + $scope.userId;
+            url += '&startAt=' + _oCriteria.start + '&endAt=' + _oCriteria.end;
+            url + _oPage.j();
+            http2.get(url, function(rsp) {
                 $scope.matters = rsp.data.logs;
                 _oPage.total = rsp.data.total || 0;
             });
         };
+        $scope.cancle = function() {
+            _oCriteria.start = _oCriteria.end = '';
+            $scope.doSearch();
+        }
+        $scope.$on('xxt.tms-datepicker.change', function(event, data) {
+            _oCriteria[data.state] = data.value;
+            if(_oCriteria.start&&_oCriteria.end) {
+                $scope.doSearch();
+            }
+        });
         $scope.doSearch();
     }])
 

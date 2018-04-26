b7c1901db9e077dbcde08e0452d519408d7fc5ff
aYellowApple <2764411477@qq.com>
2018-04-26 16:03:06 +0800

    user add detail

--- views/default/pl/fe/user/doc.js
+++ views/default/pl/fe/user/doc.js
@@ -1,6 +1,6 @@
 define(['frame'], function(ngApp) {
     'use strict';
-    ngApp.provider.controller('ctrlDoc', ['$scope', 'http2', function($scope, http2) {
+    ngApp.provider.controller('ctrlDoc', ['$scope', 'http2', '$uibModal', function($scope, http2, $uibModal) {
         var _oPage, _oCriteria;
         $scope.criteria = _oCriteria = {
             start: '',
@@ -27,7 +27,40 @@ define(['frame'], function(ngApp) {
         $scope.cancle = function() {
             _oCriteria.start = _oCriteria.end = '';
             $scope.doSearch();
-        }
+        };
+        $scope.detail = function(matter, type) {
+            $uibModal.open({
+                templateUrl: '/views/default/pl/fe/_module/statDetail.html?_=1',
+                controller: ['$scope', '$uibModalInstance', 'http2', function($scope, $mi, http2) {
+                    var criteria = {
+                        byOp: type,
+                        byUserId: matter.userid,
+                        start: _oCriteria.start,
+                        end: _oCriteria.end
+                    }
+                    $scope.page = {
+                        at: 1,
+                        size: 15,
+                        j: function() {
+                            return '&page=' + this.at + '&size=' + this.size;
+                        }
+                    };
+                    $scope.list = function() {
+                        var url;
+                        url = '/rest/pl/fe/user/userDetailLogs?matterId=' + matter.matter_id + '&matterType=' + matter.matter_type + $scope.page.j();
+                        http2.post(url, criteria, function(rsp) {
+                            $scope.logs = rsp.data.logs;
+                            $scope.page.total = rsp.data.total;
+                        });
+                    };
+                    $scope.cancle = function() {
+                        $mi.dismiss();
+                    };
+                    $scope.list();
+                }],
+                backdrop: 'static'
+            })
+        };
         $scope.$on('xxt.tms-datepicker.change', function(event, data) {
             _oCriteria[data.state] = data.value;
             if(_oCriteria.start || _oCriteria.end) {

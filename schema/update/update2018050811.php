966c8199698cfdc94faa14bf0b195113d82b7052
liao1a <153887111@qq.com>
2018-05-08 16:51:48 +0800

    缩小范围

--- schema/update/update2018050811.php
+++ schema/update/update2018050811.php
@@ -28,12 +28,13 @@ function replaceBodyPic($body, $image) {
 	return $body;
 }
 //
-$sql = "select id,pic,body from xxt_article where (pic <> '' or body <> '')";
+$sql = "select id,pic,body from xxt_article where (pic <> '' or body like '%<img src=%')";
 $db_result = $mysqli->query($sql);
 $objects = array();
 while ($obj = $db_result->fetch_object()) {
 	$objects[] = $obj;
 }
+$sum = 0;
 foreach ($objects as $object) {
 	$update = [];
 	if (!empty($object->pic)) {
@@ -44,6 +45,7 @@ foreach ($objects as $object) {
 		$update[] = "body = '" . $mysqli->real_escape_string($body) . "'";
 	}
 	if (!empty($update)) {
+		$sum++;
 		$set = implode(', ', $update);
 		$sql2 = "update xxt_article set {$set} where id = {$object->id}";
 		if (!$mysqli->query($sql2)) {
@@ -51,10 +53,9 @@ foreach ($objects as $object) {
 			echo 'database error: ' . $mysqli->error;
 			echo "----------";
 			echo $object->id;
-			echo "<br/>";
 		}
 	}
 }
 
-echo "<br/>";
+echo ">>>>>>>>>>" . $sum . ">>>>>>>>>>";
 echo "end update " . __FILE__ . PHP_EOL;

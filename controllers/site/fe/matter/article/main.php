a8bfeacd97a53ce7d7004f244ec5b3085bdec0b4
liao1a <153887111@qq.com>
2018-05-09 14:01:39 +0800

    单图文附件增加规则限制（制作了一部分）

--- controllers/site/fe/matter/article/main.php
+++ controllers/site/fe/matter/article/main.php
@@ -140,6 +140,8 @@ class main extends \site\fe\matter\base {
 		 */
 		$modelArticle = $this->model('matter\article');
 		$oArticle = $modelArticle->byId($articleid);
+		
+		$this->checkEntryRule($oArticle, true);
 		/**
 		 * 获取附件
 		 */

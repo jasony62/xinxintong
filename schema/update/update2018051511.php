21d832b47326d2d749103527631c078982eb1720
liao1a <153887111@qq.com>
2018-05-15 14:05:41 +0800

    11

--- schema/update/update2018051511.php
+++ schema/update/update2018051511.php
@@ -3,6 +3,7 @@ require_once '../../db.php';
 //
 $sql = "select * from xxt_enroll_record where data like '%\"member\":{%'";
 $db_result = $mysqli->query($sql);
+$num = 0;
 while ($obj = $db_result->fetch_object()) {
 	$sql2 = "select count(id) sum from xxt_enroll_record_data where enroll_key = '{$obj->enroll_key}' and schema_id = 'member'";
 	$db_result2 = $mysqli->query($sql2);
@@ -14,7 +15,9 @@ while ($obj = $db_result->fetch_object()) {
 		if (!$mysqli->query($sql3)) {
 			header('HTTP/1.0 500 Internal Server Error');
 			echo 'database error: ' . $mysqli->error . ';;';
+		} else {
+			$num++;
 		}
 	}
 }
-echo "end update " . __FILE__ . PHP_EOL;
+echo "end update " . __FILE__ . PHP_EOL . '--' . $num;

120726881c7cf8dff5fb61a58775a519681e618e
littleSa <2764411477@qq.com>
2018-05-25 11:10:27 +0800

    picviewer (#659)
    * add
    
    * first done picview
    
    * pack

--- views/default/site/fe/matter/enroll/topic.js
+++ views/default/site/fe/matter/enroll/topic.js
@@ -9,7 +9,7 @@ window.moduleAngularModules = ['repos.ui.enroll', 'tag.ui.enroll', 'topic.ui.enr
 
 var ngApp = require('./main.js');
 ngApp.oUtilSchema = require('../_module/schema.util.js');
-ngApp.controller('ctrlTopic', ['$scope', '$sce', '$q', '$uibModal', 'http2', 'tmsLocation', '$timeout', 'tmsDynaPage', 'noticebox', function($scope, $sce, $q, $uibModal, http2, LS, $timeout, tmsDynaPage, noticebox) {
+ngApp.controller('ctrlTopic', ['$scope', '$sce', '$q', '$uibModal', 'http2', 'tmsLocation', '$timeout', 'picviewer', 'noticebox', function($scope, $sce, $q, $uibModal, http2, LS, $timeout, picviewer, noticebox) {
     /* 是否可以对记录进行表态 */
     function fnCanAgreeRecord(oRecord, oUser) {
         if (oUser.is_leader) {
@@ -77,7 +77,11 @@ ngApp.controller('ctrlTopic', ['$scope', '$sce', '$q', '$uibModal', 'http2', 'tm
                     $scope.repos.push(oRecord);
                 });
             }
-            tmsDynaPage.loadScript(['/static/js/hammer.min.js', '/asset/js/xxt.ui.picviewer.js']);
+            $timeout(function() {
+                if(document.querySelectorAll('.data img')) {
+                    picviewer.init(document.querySelectorAll('.data img'));
+                }
+            });
             $scope.reposLoading = false;
             deferred.resolve(result);
         });

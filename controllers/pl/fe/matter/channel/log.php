748762fe308ee7607c88c72dbfdf0800c68748b7
liao1a <153887111@qq.com>
2018-06-08 07:48:06 +0800

    更改了单图文和频道日志的显示 (#672)
    * update code
    
    * 删除不必要的文件
    
    * 創建模版所需的数据库表增加登记活动模板表
    
    * Revert "創建模版所需的数据库表增加登记活动模板表"
    
    This reverts commit fb0e098ad0a57076aaabc6e69c93fcca4b6a0093.
    
    * 在签到活动中增加发送消息的功能
    
    * Revert "在签到活动中增加发送消息的功能"
    
    This reverts commit e856720411d7138e1deecb8945672a7fd6c25402.
    
    * 11
    
    * 11
    
    * 重做单图文和频道的日志
    
    * 22
    
    * 33
    
    * 55
    
    * 66
    
    * 77

--- controllers/pl/fe/matter/channel/log.php
+++ controllers/pl/fe/matter/channel/log.php
@@ -17,33 +17,27 @@ class log extends \pl\fe\matter\base {
 	 * 查询日志
 	 *
 	 */
-	public function userMatterAction_action($appId, $page = 1, $size = 30) {
+	public function userMatterAction_action($site, $appId, $page = 1, $size = 30) {
 		$modelLog = $this->model('matter\log');
 
 		$filter = $this->getPostJson();
 		$options = [];
-		if (empty($filter->byUserId)) {
-			return new \ResponseError('未指定用户');
+		if (!empty($filter->byEvent)) {
+			$options['byEvent'] = $modelLog->escape($filter->byEvent);
 		}
-		if (empty($filter->byOp)) {
-			return new \ResponseError('未指定用户行为');
+		if (!empty($filter->startAt)) {
+			$options['startAt'] = $modelLog->escape($filter->startAt);
 		}
-		$options['byUserId'] = $modelLog->escape($filter->byUserId);
-		$options['byOp'] = $modelLog->escape($filter->byOp);
-		
-		if (!empty($filter->start)) {
-			$options['start'] = $modelLog->escape($filter->start);
+		if (!empty($filter->endAt)) {
+			$options['endAt'] = $modelLog->escape($filter->endAt);
 		}
-		if (!empty($filter->end)) {
-			$options['end'] = $modelLog->escape($filter->end);
-		}
-		if (!empty($filter->shareby)) {
-			$options['shareby'] = $modelLog->escape($filter->shareby);
+		if (!empty($page) && !empty($size)) {
+			$options['paging'] = ['page' => $page, 'size' => $size];
 		}
 
-		$reads = $modelLog->userMatterAction($appId, 'channel', $options, $page, $size);
+		$data = $modelLog->listMatterAction($site, 'channel', $appId, $options);
 
-		return new \ResponseData($reads);
+		return new \ResponseData($data);
 	}
 	/**
 	 * 运营传播统计
@@ -78,27 +72,27 @@ class log extends \pl\fe\matter\base {
 	/**
 	 * 导出传播情况
 	 */
-	public function exportOperateStat_action($site, $appId, $start = '', $end = '', $shareby = '') {
+	public function exportOperateStat_action($site, $appId, $startAt = '', $endAt = '', $byEvent = '') {
+		if (empty($startAt)) {
+			die('未找到开始时间');
+		}
 		$modelAct = $this->model('matter\channel');
 		$oApp = $modelAct->byId($appId, ['fields' => 'id,title']);
 		if ($oApp === false) {
-			return new \ObjectNotFoundError();
+			die('指定频道不存在或已删除');
 		}
 
 		$options = [];
-		$options['groupby'] = 'r.userid';
-		if (!empty($start)) {
-			$options['start'] = $start;
+		$options['startAt'] = $startAt;
+		if (!empty($endAt)) {
+			$options['endAt'] = $endAt;
 		}
-		if (!empty($end)) {
-			$options['end'] = $end;
-		}
-		if (!empty($shareby)) {
-			$options['shareby'] = $shareby;
+		if (!empty($byEvent)) {
+			$options['byEvent'] = $byEvent;
 		}
 
 		$modelLog = $this->model('matter\log');
-		$logs = $modelLog->operateStat($site, $appId, 'channel', $options)->logs;
+		$logs = $modelLog->listMatterAction($site, 'channel', $appId, $options)->logs;
 
 		require_once TMS_APP_DIR . '/lib/PHPExcel.php';
 		// Create new PHPExcel object
@@ -111,24 +105,31 @@ class log extends \pl\fe\matter\base {
 			->setDescription($oApp->title);
 		$objActiveSheet = $objPHPExcel->getActiveSheet();
 		$columnNum1 = 0; //列号
-		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '昵称');
-		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '阅读数');
-		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '转发数');
-		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '分享数');
-		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '带来的阅读数');
-		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '带来的阅读人数');
+		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '时间');
+		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '用户名');
+		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '操作');
+		$objActiveSheet->setCellValueByColumnAndRow($columnNum1++, 1, '来源');
 		
 		// 转换数据
 		for ($j = 0, $jj = count($logs); $j < $jj; $j++) {
 			$log = $logs[$j];
 			$rowIndex = $j + 2;
 			$columnNum2 = 0; //列号
+			$actionAt = date('Y-m-d H:i:s', $log->action_at);
+			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $actionAt);
 			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $log->nickname);
-			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $log->readNum);
-			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $log->shareFNum);
-			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $log->shareTNum);
-			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $log->attractReadNum);
-			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $log->attractReaderNum);
+			if ($log->act_read > 0) {
+				$event = '阅读';
+			} else if ($log->act_share_timeline > 0) {
+				$event = '分享至朋友圈';
+			} else if ($log->act_share_friend > 0) {
+				$event = '转发给朋友';
+			} else {
+				$event = '未知';
+			}
+			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $event);
+			$originNickname = isset($log->origin_nickname)? $log->origin_nickname : '';
+			$objActiveSheet->setCellValueByColumnAndRow($columnNum2++, $rowIndex, $originNickname);
 		}
 		// 输出
 		header('Content-Type: application/vnd.ms-excel');

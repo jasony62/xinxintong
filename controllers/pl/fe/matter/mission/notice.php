33e2bef6ff6690f12ee61499317037e493bb4ca0
Jason Young <jasony62@qq.com>
2018-07-09 12:26:36 +0800

    update code

--- controllers/pl/fe/matter/mission/notice.php
+++ controllers/pl/fe/matter/mission/notice.php
@@ -18,7 +18,7 @@ class notice extends \pl\fe\matter\base {
 	 *
 	 * @param int $batch 通知批次id
 	 */
-	public function logList_action($batch) {
+	public function logList_action($batch, $onlySns = 'Y') {
 		if (false === ($oUser = $this->accountUser())) {
 			return new \ResponseTimeout();
 		}
@@ -30,24 +30,14 @@ class notice extends \pl\fe\matter\base {
 			'xxt_log_tmplmsg_detail de,xxt_site_account a',
 			"de.batch_id = $batch and a.siteid = de.siteid and a.uid = de.userid",
 		];
-
-		$logs = $modelTmplBat->query_objs_ss($q);
-		$result = new \stdClass;
-		$result->logs = $logs;
-
-		/* 和登记记录进行关联 */
-		if (count($logs)) {
-			$records = [];
-			foreach ($logs as $log) {
-				$record = new \stdClass;
-				$record->userid = $log->userid;
-				$record->nickname = $log->nickname;
-				$record->noticeStatus = $log->status;
-				$records[] = $record;
-			}
-			$result->records = $records;
+		if ($onlySns === 'Y') {
+			$q[2] .= " and a.openid<>''";
 		}
 
-		return new \ResponseData($result);
+		$logs = $modelTmplBat->query_objs_ss($q);
+		$oResult = new \stdClass;
+		$oResult->logs = $logs;
+
+		return new \ResponseData($oResult);
 	}
 }
--- views/default/pl/fe/matter/mission/notice.js
+++ views/default/pl/fe/matter/mission/notice.js
@@ -10,28 +10,26 @@ define(['frame'], function(ngApp) {
             $scope.batchId = batch;
             url = '/rest/pl/fe/matter/mission/notice/logList?batch=' + batch.id;
             http2.get(url, function(rsp) {
-                var records, noticeStatus, result;
+                var noticeStatus, result;
                 result = rsp.data;
-                $scope.logs = result.logs;
-                if (result.records && result.records.length) {
-                    records = result.records;
-                    records.forEach(function(record) {
-                        if (noticeStatus = record.noticeStatus) {
-                            record._noticeStatus = noticeStatus.split(':');
-                            record._noticeStatus[0] = record._noticeStatus[0] === 'success' ? '成功' : '失败';
+                if (result.logs && result.logs.length) {
+                    result.logs.forEach(function(oLog) {
+                        if (noticeStatus = oLog.noticeStatus) {
+                            oLog._noticeStatus = noticeStatus.split(':');
+                            oLog._noticeStatus[0] = oLog._noticeStatus[0] === 'success' ? '成功' : '失败';
                         }
                     });
-                    $scope.records = records;
                 }
+                $scope.logs = result.logs;
                 $scope.activeBatch = batch;
             });
         };
         $scope.choose = 'N';
         $scope.fail = function(isCheck) {
             if (isCheck == 'Y') {
-                $scope.records.forEach(function(item, index) {
+                $scope.logs.forEach(function(item, index) {
                     if (!(item.noticeStatus.indexOf('failed') < 0)) {
-                        $scope.records.splice(item, index);
+                        $scope.logs.splice(item, index);
                     }
                 });
             } else {

fab9713c9fea1eb3315a490f937a627a9fd4d9ba
Jason Young <jasony62@qq.com>
2018-08-21 21:17:45 +0800

    update code

--- static/js/ui-tms.js
+++ static/js/ui-tms.js
@@ -3,26 +3,6 @@
  * combox
  * editable
  */
-'use strict';
-(function() {
-    function doXhr(method, url, data) {
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, url, true);
-        xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-        xhr.setRequestHeader("Accept", "application/json");
-        xhr.send(JSON.stringify(data));
-    }
-    window.onerror = function(msg, url, line, column) {
-        var message = [
-            'Message: ' + msg,
-            'URL: ' + url,
-            'Line: ' + line,
-            'Column: ' + column,
-        ].join(' - ');
-        doXhr('post', '/rest/log/add', { src: 'js', msg: message });
-        console.log(message);
-    };
-})();
 angular.module('ui.tms', ['ngSanitize']).service('noticebox', ['$timeout', function($timeout) {
     var _boxId = 'tmsbox' + (new Date() * 1),
         _last = {

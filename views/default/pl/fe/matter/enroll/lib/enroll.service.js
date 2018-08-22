600c5d678ab131cde5cc4a7179009d0fac7d845d
Jason Young <jasony62@qq.com>
2018-08-22 20:15:48 +0800

    update code

--- views/default/pl/fe/matter/enroll/lib/enroll.service.js
+++ views/default/pl/fe/matter/enroll/lib/enroll.service.js
@@ -1179,9 +1179,6 @@ define(['require', 'schema', 'page'], function(require, schemaLib, pageLib) {
                                 $scope2.doSearchFromRnd(1);
                                 http2.get('/rest/pl/fe/matter/enroll/schema/compatible?app1=' + _appId + '&app2=' + oFromApp.id, function(rsp) {
                                     _oData.compatibleSchemas = rsp.data;
-                                    if (rsp.data.length === 0) {
-                                        noticebox.info('选中活动的题目和当前活动的题目不匹配，无法导入记录');
-                                    }
                                 });
                             }
                         });
@@ -1189,12 +1186,14 @@ define(['require', 'schema', 'page'], function(require, schemaLib, pageLib) {
                             if (oFromRnd) {
                                 http2.get('/rest/pl/fe/matter/enroll/record/countByRound?round=' + oFromRnd.rid, function(rsp) {
                                     _oData.countOfRecord = rsp.data;
-                                    if (rsp.data === 0) {
-                                        noticebox.info('选中活动的填写轮次的记录为空，无法导入记录');
-                                    }
                                 });
                             }
                         });
+                        $scope2.$watch('data', function(oNewData) {
+                            if (oNewData) {
+                                $scope2.executable = (!!oNewData.toRnd && !!oNewData.fromApp && !!oNewData.fromRnd && !!oNewData.compatibleSchemas && oNewData.compatibleSchemas.length > 0 && oNewData.countOfRecord > 0);
+                            }
+                        }, true);
                         $scope2.doSearchRnd();
                         $scope2.doSearchFromApp();
                     }],

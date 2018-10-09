1263d38adf5e7c6f982a67ddab0475d24a4ebfb3
Jason Young <jasony62@qq.com>
2018-10-09 11:31:07 +0800

    update code

--- views/default/pl/fe/matter/enroll/frame-loader.js
+++ views/default/pl/fe/matter/enroll/frame-loader.js
@@ -1,14 +1,6 @@
-var timestamp, minutes;
-timestamp = new Date();
-minutes = timestamp.getMinutes();
-minutes = Math.floor(minutes / 5) * 5;
-timestamp.setMinutes(minutes);
-timestamp.setMilliseconds(0);
-timestamp.setSeconds(0);
-require.config({
-    waitSeconds: 0,
-    paths: {
-        "domReady": '/static/js/domReady',
+var _oRawScripts;
+_oRawScripts = {
+    js: {
         "enrollSchema": '/views/default/pl/fe/matter/enroll/lib/enroll.schema',
         "enrollPage": '/views/default/pl/fe/matter/enroll/lib/enroll.page',
         "enrollService": '/views/default/pl/fe/matter/enroll/lib/enroll.service',
@@ -18,13 +10,83 @@ require.config({
         "wrap": '/views/default/pl/fe/matter/enroll/lib/wrap',
         "editor": '/views/default/pl/fe/matter/enroll/lib/editor',
         "frame": '/views/default/pl/fe/matter/enroll/frame',
+        "editorCtrl": '/views/default/pl/fe/matter/enroll/editor',
+        "enrolleeCtrl": '/views/default/pl/fe/matter/enroll/enrollee',
+        "entryCtrl": '/views/default/pl/fe/matter/enroll/entry',
+        "logCtrl": '/views/default/pl/fe/matter/enroll/log',
+        "mainCtrl": '/views/default/pl/fe/matter/enroll/main',
+        "noticeCtrl": '/views/default/pl/fe/matter/enroll/notice',
+        "pageCtrl": '/views/default/pl/fe/matter/enroll/page',
+        "previewCtrl": '/views/default/pl/fe/matter/enroll/preview',
+        "recordCtrl": '/views/default/pl/fe/matter/enroll/record',
+        "recycleCtrl": '/views/default/pl/fe/matter/enroll/recycle',
+        "remarkCtrl": '/views/default/pl/fe/matter/enroll/remark',
+        "ruleCtrl": '/views/default/pl/fe/matter/enroll/rule',
+        "schemaCtrl": '/views/default/pl/fe/matter/enroll/schema',
+        "statCtrl": '/views/default/pl/fe/matter/enroll/stat',
+        "timeCtrl": '/views/default/pl/fe/matter/enroll/time',
     },
-    urlArgs: function(id, url) {
-        if (/domReady/.test(id)) {
-            return '';
-        }
-        return "?bust=" + (timestamp * 1);
+    html: {
+        "editor": '/views/default/pl/fe/matter/enroll/editor',
+        "enrollee": '/views/default/pl/fe/matter/enroll/enrollee',
+        "entry": '/views/default/pl/fe/matter/enroll/entry',
+        "log": '/views/default/pl/fe/matter/enroll/log',
+        "main": '/views/default/pl/fe/matter/enroll/main',
+        "notice": '/views/default/pl/fe/matter/enroll/notice',
+        "page": '/views/default/pl/fe/matter/enroll/page',
+        "preview": '/views/default/pl/fe/matter/enroll/preview',
+        "record": '/views/default/pl/fe/matter/enroll/record',
+        "recycle": '/views/default/pl/fe/matter/enroll/recycle',
+        "remark": '/views/default/pl/fe/matter/enroll/remark',
+        "rule": '/views/default/pl/fe/matter/enroll/rule',
+        "schema": '/views/default/pl/fe/matter/enroll/schema',
+        "stat": '/views/default/pl/fe/matter/enroll/stat',
+        "time": '/views/default/pl/fe/matter/enroll/time',
     }
-});
-window.MATTER_TYPE = 'Enroll'; // 为了支持动态加载服务模块
-require(['frame']);
+}
+
+function _fnConfigRequire(oScriptTimes) {
+    var oPaths = {
+        "domReady": '/static/js/domReady'
+    };
+    for (var p in _oRawScripts.js) {
+        oPaths[p] = _oRawScripts.js[p];
+    }
+    require.config({
+        waitSeconds: 0,
+        paths: oPaths,
+        urlArgs: function(id, url) {
+            return oScriptTimes.js[id] ? ('?bust=' + oScriptTimes.js[id].time) : '';
+        }
+    });
+    for (var n in _oRawScripts.html) {
+        if (oScriptTimes.html && oScriptTimes.html[n]) {
+            oScriptTimes.html[n].path = _oRawScripts.html[n] + '';
+        }
+    }
+    window.ScriptTimes = oScriptTimes;
+    window.MATTER_TYPE = 'Enroll'; // 为了支持动态加载服务模块
+    require(['frame']);
+}
+/* 获得要加载文件的修改时间 */
+var xhr;
+xhr = new XMLHttpRequest();
+xhr.open('POST', '/rest/script/time', true);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        if (xhr.status >= 200 && xhr.status < 400) {
+            var oScriptTimes;
+            try {
+                oScriptTimes = JSON.parse(xhr.responseText);
+                if (oScriptTimes && typeof(oScriptTimes) === 'object') {
+                    _fnConfigRequire(oScriptTimes.data);
+                }
+            } catch (e) {
+                alert('local error:' + e.toString());
+            }
+        } else {
+            alert('http error:' + xhr.statusText);
+        }
+    };
+}
+xhr.send(JSON.stringify(_oRawScripts));

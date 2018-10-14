1d2f3ccf85f567a60da479ee8bb59f68dd71bc9e
Jason Young <jasony62@qq.com>
2018-10-14 18:46:19 +0800

    update code

--- views/default/pl/fe/matter/mission/frame-loader.js
+++ views/default/pl/fe/matter/mission/frame-loader.js
@@ -1,14 +1,6 @@
-var timestamp, minutes;
-timestamp = new Date();
-minutes = timestamp.getMinutes();
-minutes = Math.floor(minutes / 5) * 5;
-timestamp.setMinutes(minutes);
-timestamp.setMilliseconds(0);
-timestamp.setSeconds(0);
-require.config({
-    waitSeconds: 0,
-    paths: {
-        "domReady": '/static/js/domReady',
+var _oRawPathes;
+_oRawPathes = {
+    js: {
         "missionService": '/views/default/pl/fe/matter/mission/lib/mission.service',
         "enrollService": '/views/default/pl/fe/matter/enroll/lib/enroll.service',
         "signinService": '/views/default/pl/fe/matter/signin/lib/signin.service',
@@ -16,12 +8,79 @@ require.config({
         "schema": '/views/default/pl/fe/matter/enroll/lib/schema',
         "wrap": '/views/default/pl/fe/matter/enroll/lib/wrap',
         "frame": '/views/default/pl/fe/matter/mission/frame',
+        "appCtrl": '/views/default/pl/fe/matter/mission/app',
+        "coinCtrl": '/views/default/pl/fe/matter/mission/coin',
+        "coworkerCtrl": '/views/default/pl/fe/matter/mission/coworker',
+        "docCtrl": '/views/default/pl/fe/matter/mission/doc',
+        "enrolleeCtrl": '/views/default/pl/fe/matter/mission/enrollee',
+        "entryCtrl": '/views/default/pl/fe/matter/mission/entry',
+        "mainCtrl": '/views/default/pl/fe/matter/mission/main',
+        "mschemaCtrl": '/views/default/pl/fe/matter/mission/mschema',
+        "noticeCtrl": '/views/default/pl/fe/matter/mission/notice',
+        "recycleCtrl": '/views/default/pl/fe/matter/mission/recycle',
+        "overviewCtrl": '/views/default/pl/fe/matter/mission/overview',
+        "reportCtrl": '/views/default/pl/fe/matter/mission/report',
+        "timeCtrl": '/views/default/pl/fe/matter/mission/time',
     },
-    urlArgs: function(id, url) {
-        if (/domReady/.test(id)) {
-            return '';
-        }
-        return "?bust=" + (timestamp * 1);
+    html: {
+        "app": '/views/default/pl/fe/matter/mission/app',
+        "coin": '/views/default/pl/fe/matter/mission/coin',
+        "coworker": '/views/default/pl/fe/matter/mission/coworker',
+        "doc": '/views/default/pl/fe/matter/mission/doc',
+        "enrollee": '/views/default/pl/fe/matter/mission/enrollee',
+        "entry": '/views/default/pl/fe/matter/mission/entry',
+        "main": '/views/default/pl/fe/matter/mission/main',
+        "mschema": '/views/default/pl/fe/matter/mission/mschema',
+        "notice": '/views/default/pl/fe/matter/mission/notice',
+        "recycle": '/views/default/pl/fe/matter/mission/recycle',
+        "overview": '/views/default/pl/fe/matter/mission/overview',
+        "report": '/views/default/pl/fe/matter/mission/report',
+        "time": '/views/default/pl/fe/matter/mission/time',
     }
+}
+
+function _fnConfigRequire(oPathAndTimes) {
+    var oPaths = {
+        "domReady": '/static/js/domReady'
+    };
+    for (var p in _oRawPathes.js) {
+        oPaths[p] = _oRawPathes.js[p];
+    }
+    require.config({
+        waitSeconds: 0,
+        paths: oPaths,
+        urlArgs: function(id, url) {
+            return oPathAndTimes.js[id] ? ('?bust=' + oPathAndTimes.js[id].time) : '';
+        }
+    });
+    for (var n in _oRawPathes.html) {
+        if (oPathAndTimes.html && oPathAndTimes.html[n]) {
+            oPathAndTimes.html[n].path = _oRawPathes.html[n] + '';
+        }
+    }
+    define('frame/RouteParam', function() {
+        return function(name) {
+            if (oPathAndTimes && oPathAndTimes.html && oPathAndTimes.html[name]) {
+                this.templateUrl = oPathAndTimes.html[name].path + '.html?_=' + oPathAndTimes.html[name].time;
+            }
+            this.controller = 'ctrl' + name[0].toUpperCase() + name.substr(1);
+            this.reloadOnSearch = false;
+            this.resolve = {
+                load: function($q) {
+                    var defer = $q.defer();
+                    require([name + 'Ctrl'], function() {
+                        defer.resolve();
+                    });
+                    return defer.promise;
+                }
+            };
+        }
+    });
+    require(['frame']);
+}
+/* 获得要加载文件的修改时间 */
+angular.injector(['ng']).invoke(function($http) {
+    $http.post('/rest/script/time', _oRawPathes, { 'headers': { 'accept': 'application/json' } }).success(function(rsp) {
+        _fnConfigRequire(rsp.data);
+    });
 });
-require(['frame']);

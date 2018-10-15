60d7d58329c59aab335670d739560f81760b3166
Jason Young <jasony62@qq.com>
2018-10-15 08:33:11 +0800

    update code

--- views/default/pl/fe/matter/signin/frame-loader.js
+++ views/default/pl/fe/matter/signin/frame-loader.js
@@ -1,80 +1,77 @@
-var _oRawScripts;
-_oRawScripts = {
-    js: {
-        "enrollSchema": '/views/default/pl/fe/matter/enroll/lib/enroll.schema',
-        "enrollPage": '/views/default/pl/fe/matter/enroll/lib/enroll.page',
-        "signinService": '/views/default/pl/fe/matter/signin/lib/signin.service',
-        "page": '/views/default/pl/fe/matter/enroll/lib/page',
-        "schema": '/views/default/pl/fe/matter/enroll/lib/schema',
-        "wrap": '/views/default/pl/fe/matter/enroll/lib/wrap',
-        "editor": '/views/default/pl/fe/matter/enroll/lib/editor',
-        "frame": '/views/default/pl/fe/matter/signin/frame',
-        "accessCtrl": '/views/default/pl/fe/matter/signin/access',
-        "coinCtrl": '/views/default/pl/fe/matter/signin/coin',
-        "entryCtrl": '/views/default/pl/fe/matter/signin/entry',
-        "mainCtrl": '/views/default/pl/fe/matter/signin/main',
-        "noticeCtrl": '/views/default/pl/fe/matter/signin/notice',
-        "pageCtrl": '/views/default/pl/fe/matter/signin/page',
-        "previewCtrl": '/views/default/pl/fe/matter/signin/preview',
-        "recordCtrl": '/views/default/pl/fe/matter/signin/record',
-        "schemaCtrl": '/views/default/pl/fe/matter/signin/schema',
-    },
-    html: {
-        "access": '/views/default/pl/fe/matter/signin/access',
-        "coin": '/views/default/pl/fe/matter/signin/coin',
-        "entry": '/views/default/pl/fe/matter/signin/entry',
-        "main": '/views/default/pl/fe/matter/signin/main',
-        "notice": '/views/default/pl/fe/matter/signin/notice',
-        "page": '/views/default/pl/fe/matter/signin/page',
-        "preview": '/views/default/pl/fe/matter/signin/preview',
-        "record": '/views/default/pl/fe/matter/signin/record',
-        "schema": '/views/default/pl/fe/matter/signin/schema',
-    }
-}
-
-function _fnConfigRequire(oScriptTimes) {
-    var oPaths = {
-        "domReady": '/static/js/domReady'
-    };
-    for (var p in _oRawScripts.js) {
-        oPaths[p] = _oRawScripts.js[p];
-    }
-    require.config({
-        waitSeconds: 0,
-        paths: oPaths,
-        urlArgs: function(id, url) {
-            return oScriptTimes.js[id] ? ('?bust=' + oScriptTimes.js[id].time) : '';
-        }
+requirejs(['/static/js/tms.bootstrap.js'], function(tms) {
+    define("frame/const", {
+        notifyMatter: [{
+            value: 'tmplmsg',
+            title: '模板消息',
+            url: '/rest/pl/fe/matter'
+        }, {
+            value: 'article',
+            title: '单图文',
+            url: '/rest/pl/fe/matter'
+        }, {
+            value: 'news',
+            title: '多图文',
+            url: '/rest/pl/fe/matter'
+        }, {
+            value: 'channel',
+            title: '频道',
+            url: '/rest/pl/fe/matter'
+        }, {
+            value: 'enroll',
+            title: '登记活动',
+            url: '/rest/pl/fe/matter'
+        }],
+        innerlink: [{
+            value: 'article',
+            title: '单图文',
+            url: '/rest/pl/fe/matter'
+        }, {
+            value: 'news',
+            title: '多图文',
+            url: '/rest/pl/fe/matter'
+        }, {
+            value: 'channel',
+            title: '频道',
+            url: '/rest/pl/fe/matter'
+        }],
+        alertMsg: {
+            'schema.duplicated': '不允许重复添加登记项'
+        },
+        naming: {}
     });
-    for (var n in _oRawScripts.html) {
-        if (oScriptTimes.html && oScriptTimes.html[n]) {
-            oScriptTimes.html[n].path = _oRawScripts.html[n] + '';
+    var _oRawPathes;
+    _oRawPathes = {
+        js: {
+            "enrollSchema": '/views/default/pl/fe/matter/enroll/lib/enroll.schema',
+            "enrollPage": '/views/default/pl/fe/matter/enroll/lib/enroll.page',
+            "signinService": '/views/default/pl/fe/matter/signin/lib/signin.service',
+            "page": '/views/default/pl/fe/matter/enroll/lib/page',
+            "schema": '/views/default/pl/fe/matter/enroll/lib/schema',
+            "wrap": '/views/default/pl/fe/matter/enroll/lib/wrap',
+            "editor": '/views/default/pl/fe/matter/enroll/lib/editor',
+            "frame": '/views/default/pl/fe/matter/signin/frame',
+            "accessCtrl": '/views/default/pl/fe/matter/signin/access',
+            "coinCtrl": '/views/default/pl/fe/matter/signin/coin',
+            "entryCtrl": '/views/default/pl/fe/matter/signin/entry',
+            "mainCtrl": '/views/default/pl/fe/matter/signin/main',
+            "noticeCtrl": '/views/default/pl/fe/matter/signin/notice',
+            "pageCtrl": '/views/default/pl/fe/matter/signin/page',
+            "previewCtrl": '/views/default/pl/fe/matter/signin/preview',
+            "recordCtrl": '/views/default/pl/fe/matter/signin/record',
+            "schemaCtrl": '/views/default/pl/fe/matter/signin/schema',
+        },
+        html: {
+            "access": '/views/default/pl/fe/matter/signin/access',
+            "coin": '/views/default/pl/fe/matter/signin/coin',
+            "entry": '/views/default/pl/fe/matter/signin/entry',
+            "main": '/views/default/pl/fe/matter/signin/main',
+            "notice": '/views/default/pl/fe/matter/signin/notice',
+            "page": '/views/default/pl/fe/matter/signin/page',
+            "preview": '/views/default/pl/fe/matter/signin/preview',
+            "record": '/views/default/pl/fe/matter/signin/record',
+            "schema": '/views/default/pl/fe/matter/signin/schema',
         }
     }
-    window.ScriptTimes = oScriptTimes;
     window.MATTER_TYPE = 'Signin'; // 为了支持动态加载服务模块
-    require(['frame']);
-}
-
-/* 获得要加载文件的修改时间 */
-var xhr;
-xhr = new XMLHttpRequest();
-xhr.open('POST', '/rest/script/time', true);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        if (xhr.status >= 200 && xhr.status < 400) {
-            var oScriptTimes;
-            try {
-                oScriptTimes = JSON.parse(xhr.responseText);
-                if (oScriptTimes && typeof(oScriptTimes) === 'object') {
-                    _fnConfigRequire(oScriptTimes.data);
-                }
-            } catch (e) {
-                alert('local error:' + e.toString());
-            }
-        } else {
-            alert('http error:' + xhr.statusText);
-        }
-    };
-}
-xhr.send(JSON.stringify(_oRawScripts));
+    tms.bootstrap(_oRawPathes);
+});

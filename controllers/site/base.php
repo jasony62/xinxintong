cb0e398c027a940b5b87b5a1922ec1ea03a5e6f2
liao1a <153887111@qq.com>
2018-12-26 10:43:02 +0800

    33

--- controllers/site/base.php
+++ controllers/site/base.php
@@ -14,6 +14,25 @@ class base extends \TMS_CONTROLLER {
 
 		return $ruleAction;
 	}
+	/**		
+  	 * 客户端应用名称		
+  	 */		
+  	protected function &userAgent() {		
+  		if (isset($_SERVER['HTTP_USER_AGENT'])) {		
+  			$user_agent = $_SERVER['HTTP_USER_AGENT'];		
+  			if (preg_match('/yixin/i', $user_agent)) {		
+  				$ca = 'yx';		
+  			} elseif (preg_match('/MicroMessenger/i', $user_agent)) {		
+  				$ca = 'wx';		
+  			} else {		
+  				$ca = false;		
+  			}		
+  		} else {		
+  			$ca = false;		
+  		}		
+  		
+  		return $ca;		
+  	}
 	/**
 	 *
 	 */
@@ -37,23 +56,4 @@ class base extends \TMS_CONTROLLER {
 		\TPL::output('info');
 		exit;
 	}
-	/**		
-  	 * 客户端应用名称		
-  	 */		
-  	protected function &userAgent() {		
-  		if (isset($_SERVER['HTTP_USER_AGENT'])) {		
-  			$user_agent = $_SERVER['HTTP_USER_AGENT'];		
-  			if (preg_match('/yixin/i', $user_agent)) {		
-  				$ca = 'yx';		
-  			} elseif (preg_match('/MicroMessenger/i', $user_agent)) {		
-  				$ca = 'wx';		
-  			} else {		
-  				$ca = false;		
-  			}		
-  		} else {		
-  			$ca = false;		
-  		}		
-  		
-  		return $ca;		
-  	}
 }

da346cdb1d2a0f5fa522c70aee5733bcf3177c3e
Jason Young <jasony62@qq.com>
2019-01-10 11:15:51 +0800

    update code

---
+++ views/default/site/fe/matter/enroll/task.js
@@ -0,0 +1,27 @@
+'use strict';
+require('./enroll.public.css');
+require('./_asset/ui.round.js');
+
+window.moduleAngularModules = ['round.ui.enroll'];
+
+var ngApp = require('./main.js');
+ngApp.controller('ctrlTask', ['$scope', '$sce', '$q', '$uibModal', 'http2', 'tmsLocation', '$timeout', 'noticebox', 'enlRound', function($scope, $sce, $q, $uibModal, http2, LS, $timeout, noticebox, enlRound) {
+    var _oApp;
+    $scope.shiftRound = function(oRound) {
+        $scope.selectedRound = oRound;
+    };
+    $scope.$on('xxt.app.enroll.ready', function(event, params) {
+        _oApp = params.app;
+        var facRound = new enlRound(_oApp);
+        facRound.list().then(function(oResult) {
+            $scope.rounds = oResult.rounds;
+            if ($scope.rounds.length) $scope.shiftRound($scope.rounds[0]);
+        });
+        http2.get(LS.j('task/list', 'site', 'app')).then(function(rsp) {
+            if (rsp.data.question) {}
+            if (rsp.data.answer) {}
+            if (rsp.data.vote) {}
+            if (rsp.data.score) {}
+        });
+    });
+}]);

e34f558a0f349e463dfbed5dbbab49868bd633d5
liao1a <153887111@qq.com>
2019-01-17 15:31:19 +0800

    共享页搜索功能和记录中增加分类信息 (#756)
    * 創建模版所需的数据库表增加登记活动模板表
    
    * Revert "創建模版所需的数据库表增加登记活动模板表"
    
    This reverts commit fb0e098ad0a57076aaabc6e69c93fcca4b6a0093.
    
    * 在签到活动中增加发送消息的功能
    
    * Revert "在签到活动中增加发送消息的功能"
    
    This reverts commit e856720411d7138e1deecb8945672a7fd6c25402.
    
    * 11
    
    * 11
    
    * 重做单图文和频道的日志
    
    * 22
    
    * 33
    
    * 55
    
    * 66
    
    * 77
    
    * 修改连接素材如果外部链接是http时，在微信网页授权回调时的链接也应该为http
    
    * 删除不必要的处理
    
    * 修改记录转发的bug
    
    * 修改链接素材如果通过邀请访问，在微信上打开时填写邀请码以后第一次打开报参数不完整的错误
    
    * 修改bug
    
    * 管理端日志
    
    * 111
    
    * 邀请授权-初版
    
    * 修改
    
    * ceshi,
    
    * 修改
    
    * 11
    
    * 去除输出
    
    * 带来的bug
    
    * 11
    
    * 更改处理逻辑
    
    * 2222
    
    * 222
    
    * 33
    
    * 44
    
    * 删除oMarkUser的代码
    
    * 删除omarkUser2
    
    * 修改排序
    
    * search初步,缺少菜单和删除
    
    * 修改bug，增加筛选条件
    
    * 优化分页，初版定型
    
    * 修改
    
    * 222
    
    * 去除不必要的数据增加目录的显示
    
    * 11
    
    * bug与优化
    
    * 恢复
    
    * bug
    
    * bug

--- schema/db_schema_enroll.php
+++ schema/db_schema_enroll.php
@@ -880,5 +880,41 @@ if (!$mysqli->query($sql)) {
 	header('HTTP/1.0 500 Internal Server Error');
 	echo 'database error: ' . $mysqli->error;
 }
+/**
+ * 
+ */
+$sql = "create table if not exists xxt_enroll_search(";
+$sql .= "id int not null auto_increment";
+$sql .= ",siteid varchar(32) not null";
+$sql .= ",aid varchar(40) not null";
+$sql .= ",keyword varchar(255) not null";
+$sql .= ",user_num int not null default 0"; // 使用人数
+$sql .= ",used_num int not null default 0"; // 使用总数
+$sql .= ",agreed char(1) not null default ''"; // 是否推荐（Y：推荐）
+$sql .= ",state tinyint not null default 1"; //0:remove,1:normal
+$sql .= ",primary key(id)) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+if (!$mysqli->query($sql)) {
+	header('HTTP/1.0 500 Internal Server Error');
+	echo 'database error: ' . $mysqli->error;
+}
+/**
+ *
+ */
+$sql = "create table if not exists xxt_enroll_user_search(";
+$sql .= "id int not null auto_increment";
+$sql .= ",siteid varchar(32) not null";
+$sql .= ",aid varchar(40) not null";
+$sql .= ",userid varchar(40) not null default ''";
+$sql .= ",nickname varchar(255) not null default ''";
+$sql .= ",create_at int not null default 0"; 
+$sql .= ",last_use_at int not null default 0"; // 最后使用时间
+$sql .= ",search_id int not null default 0"; //
+$sql .= ",used_num int not null default 0"; // 使用总数
+$sql .= ",state tinyint not null default 1"; //0:remove,1:normal
+$sql .= ",primary key(id)) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+if (!$mysqli->query($sql)) {
+	header('HTTP/1.0 500 Internal Server Error');
+	echo 'database error: ' . $mysqli->error;
+}
 
 echo 'finish enroll.' . PHP_EOL;

7996f011e159387dfa1fcc13f57f7a0aad80f086
aYellowApple <2764411477@qq.com>
2019-02-13 21:19:36 +0800

    update code

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,6 @@
 'use strict';
 var ngApp = angular.module('app', ['ui.bootstrap']);
-ngApp.controller('ctrlApp', ['$scope', '$http', function($scope, $http) {
-    var str = '20190213201806';
-    console.log();
+ngApp.controller('ctrlApp', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
     var sessionId;
     $scope.dial = {
         'time_ready': '00:00',
@@ -12,6 +10,7 @@ ngApp.controller('ctrlApp', ['$scope', '$http', function($scope, $http) {
     };
 
     function getStatusRecievedTimeStamp(distance) {
+        distance = parseInt(distance)*1000;
         if (distance < 10000) return "00:0" + parseInt(distance / 1000);
         if (distance >= 10000 && distance < 60 * 1000) return "00:" + parseInt(distance / 1000);
         if (distance >= 60 * 1000 && distance < 60 * 60 * 1000) {
@@ -47,13 +46,13 @@ ngApp.controller('ctrlApp', ['$scope', '$http', function($scope, $http) {
                         $scope.status = rsp.data[0].status;
                         id = rsp.data[0].id;
                         if ($scope.status == '9') {
-                            $scope.time_ready = getStatusRecievedTimeStamp(rsp.data[0].duration);
+                            $scope.dial.time_ready = getStatusRecievedTimeStamp(rsp.data[0].duration);
                         } else if ($scope.status == '10') {
-                            $scope.time_ringing = getStatusRecievedTimeStamp(rsp.data[0].duration);
+                            $scope.dial.time_ringing = getStatusRecievedTimeStamp(rsp.data[0].duration);
                         } else if ($scope.status == '13') {
-                            $scope.time_callstart = getStatusRecievedTimeStamp(rsp.data[0].duration);
+                            $scope.dial.time_callstart = getStatusRecievedTimeStamp(rsp.data[0].duration);
                         } else if ($scope.status == '15') {
-                            $scope.time_callend = getStatusRecievedTimeStamp(rsp.data[0].duration);
+                            $scope.dial.time_callend = getStatusRecievedTimeStamp(rsp.data[0].duration);
                         }
                         if ($scope.status == '15') {
                             window.clearInterval(timer1);
@@ -61,11 +60,14 @@ ngApp.controller('ctrlApp', ['$scope', '$http', function($scope, $http) {
                                 getAddrUrl = '/rest/demo/getCallFileUrl?sessionid=' + sessionId;
                                 $http.get(getAddrUrl).success(function(rsp) {
                                     if (rsp.data) {
-                                        var startTime = rsp.data.startTime,
-                                            endTime = rsp.data.endTime;
-                                        rsp.data.startTime = startTime.substring(0,4)+'-'+startTime.substring(4,6)+'-'+startTime.substring(6,8)+' '+startTime.substring(8,10)+':'+startTime.substring(10,12)+':'+startTime.substring(12,14);
-                                        rsp.data.endTime = endTime.substring(0,4)+'-'+endTime.substring(4,6)+'-'+endTime.substring(6,8)+' '+endTime.substring(8,10)+':'+endTime.substring(10,12)+':'+endTime.substring(12,14);
+                                        var startTime = rsp.data.billed.startTime,
+                                            endTime = rsp.data.billed.endTime;
+                                        rsp.data.billed.startTime = startTime.substring(0,4)+'-'+startTime.substring(4,6)+'-'+startTime.substring(6,8)+' '+startTime.substring(8,10)+':'+startTime.substring(10,12)+':'+startTime.substring(12,14);
+                                        rsp.data.billed.endTime = endTime.substring(0,4)+'-'+endTime.substring(4,6)+'-'+endTime.substring(6,8)+' '+endTime.substring(8,10)+':'+endTime.substring(10,12)+':'+endTime.substring(12,14);
                                         $scope.record = rsp.data;
+                                        $timeout(function() {
+                                            document.querySelector('#audioAddr').src=rsp.data.fileUrl.voiceAddress;
+                                        });                                        
                                         window.clearInterval(timer2);
                                     }
                                 });

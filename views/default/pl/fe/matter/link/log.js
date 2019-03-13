79f623d8a506491806fe87cba2c54fe47166669d
aYellowApple <2764411477@qq.com>
2019-03-13 11:20:05 +0800

    update code

--- views/default/pl/fe/matter/link/log.js
+++ views/default/pl/fe/matter/link/log.js
@@ -1,25 +1,6 @@
 define(['frame'], function(ngApp) {
     ngApp.provider.controller('ctrlLog', ['$scope', 'http2', function($scope, http2) {
-        var oApp;
-        /*$scope.page = page = {
-            at: 1,
-            size: 30,
-            orderBy: 'time',
-            j: function() {
-                var p;
-                p = '&page=' + this.at + '&size=' + this.size;
-                p += '&orderby=' + this.orderBy;
-                return p;
-            }
-        };
-        $scope.read = function() {
-            var url = '/rest/pl/fe/matter/link/log/list?id=' + oApp.id + page.j();
-            http2.get(url).then(function(rsp) {
-                $scope.logs = rsp.data.logs;
-                $scope.page.total = rsp.data.total;
-            });
-        };*/
-        var read, download;
+        var oApp, read, download;
         $scope.read = read = {
             page: {
                 at: 1,

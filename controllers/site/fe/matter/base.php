605177e775ba7d4f1371431233bd3b278cd97a75
jasony62 <jasony62@qq.com>
2019-03-19 21:27:35 +0800

    update code

--- controllers/site/fe/matter/base.php
+++ controllers/site/fe/matter/base.php
@@ -308,6 +308,9 @@ class base extends \site\fe\base {
 					}
 					/* 缓存中的信息没有通过验证，记录调试日志 */
 					$this->model('log')->log($oUser->uid, 'enterAsSns', $snsUser->openid, $oMatter->type . ':' . $oMatter->id, isset($oUser->nickname) ? $oUser->nickname : '');
+					/* 如果缓存的用户数据没有关注，清空数据 */
+					unset($oUser->sns->{$snsName});
+					$this->setCookieUser($oMatter->siteid, $oUser);
 				} else {
 					$modelAcnt = $this->model('site\user\account');
 					$propSnsOpenid = $snsName . '_openid';

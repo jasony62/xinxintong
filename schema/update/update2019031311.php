8cbf670d274550a0122d6e00ae4efd8442c79852
liao1a <153887111@qq.com>
2019-04-04 10:21:48 +0800

    第三方配置

--- schema/update/update2019031311.php
+++ schema/update/update2019031311.php
@@ -8,12 +8,13 @@ $sqls = [];
 $sql = "create table if not exists xxt_account_third (";
 $sql .= "id int(11) unsigned NOT NULL AUTO_INCREMENT";
 $sql .= ",creator varchar(50) NOT NULL";
-$sql .= ",create_at int(11) NOT NULL";
-$sql .= ",appname varchar(50) NOT NULL default ''"; //第三方名称
+$sql .= ",creator_name varchar(100) not null default ''";
+$sql .= ",create_at int NOT NULL";
+$sql .= ",appname varchar(100) NOT NULL default ''"; //第三方名称
 $sql .= ",pic text null"; // head image.
 $sql .= ",appid varchar(100) NOT NULL default ''"; // 
 $sql .= ",appsecret varchar(100) NOT NULL default ''"; //
-$sql .= ",access_token varchar(100)";
+$sql .= ",access_token varchar(100) NOT NULL default ''";
 $sql .= ",access_token_expire_at int not null default 0";
 $sql .= ",state tinyint not null default 1";
 $sql .= ",primary key (id)) ENGINE=MyISAM DEFAULT CHARSET=utf8";
@@ -24,20 +25,22 @@ $sqls[] = $sql;
 $sql = "create table if not exists xxt_account_third_user (";
 $sql .= "id int(11) unsigned NOT NULL AUTO_INCREMENT";
 $sql .= ",third_id int(11) NOT NULL"; // 
-$sql .= ",openid varchar(255) not null";
+$sql .= ",openid varchar(100) not null";
 $sql .= ",reg_time int(11) NOT NULL";
 $sql .= ",headimgurl varchar(255) not null default ''";
 $sql .= ",nickname varchar(255) not null default ''";
 $sql .= ",sex tinyint not null default 0";
-$sql .= ",city varchar(255) not null default ''";
-$sql .= ",province varchar(255) not null default ''";
-$sql .= ",country varchar(255) not null default ''";
+$sql .= ",city varchar(50) not null default ''";
+$sql .= ",province varchar(50) not null default ''";
+$sql .= ",country varchar(50) not null default ''";
 $sql .= ",forbidden char(1) not null default 'N'";
 $sql .= ",unionid varchar(32) not null default '' comment '用户的注册id'";
 $sql .= ",primary key (id)) ENGINE=MyISAM DEFAULT CHARSET=utf8";
 $sqls[] = $sql;
 //
 $sqls[] = "INSERT INTO account_group(group_id,group_name,asdefault,p_mpgroup_create,p_mp_create,p_mp_permission,p_platform_manage) VALUES(101,'第三方登录用户',0,0,0,0,0)";
+//
+$sqls[] = "INSERT INTO xxt_account_third(creator,creator_name,create_at,appname,pic,appid,appsecret) VALUES('5771d91dcf713','aly'," . time() . ",'电信能力开放','/kcfinder/upload/c1aa4b1cb943c85ef98ca36db3d00620/图片/能力开放图标.jpg','20190403105121FZlTKz','86062df978b648afb903a2774cab443f')";
 //
 foreach ($sqls as $sql) {
 	if (!$mysqli->query($sql)) {

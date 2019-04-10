142d369f23f42065422bb0bf38322a2871501e7f
liao1a <153887111@qq.com>
2019-04-10 14:36:30 +0800

    22

--- ylyfinder/browse.php
+++ ylyfinder/browse.php
@@ -11,15 +11,7 @@
  *      @link http://kcfinder.sunhater.com
  */
 
-//天翼云的API服务器
-define('OOS_ENDPOINT', 'https://oos-js.ctyunapi.cn/');
-//Access Key 在天翼云门户网站-帐户管理-API密钥管理中获取
-define('OOS_ACCESS_KEY', '5b79c659ce70ace89ce2');
-//Access Secret 在天翼云门户网站-帐户管理-API密钥管理中获取
-define('OOS_ACCESS_SECRET', '4493e6cc8c992cb194cfdd8760949becc6029223');
-// bucket 容器
-define('OOS_BUCKET', 'ctsi-zsbssss1');
-// define('OOS_BUCKET', '');
+file_exists(dirname(__FILE__).'/cus/config.php') && include_once dirname(__FILE__).'/cus/config.php';
 
 require dirname(__FILE__)."/core/autoload.php";
 

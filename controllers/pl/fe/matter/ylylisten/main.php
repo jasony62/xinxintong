78293976b8be87f00858d63fa015917882531092
liao1a <153887111@qq.com>
2019-04-10 15:12:06 +0800

    3

--- controllers/pl/fe/matter/ylylisten/main.php
+++ controllers/pl/fe/matter/ylylisten/main.php
@@ -21,18 +21,20 @@ class main extends \pl\fe\matter\main_base {
 			['unionid' => $oUser->id]
 		];
 		$thirdUser = $this->model()->query_obj_ss($q);
-
-		// 
 		if ($thirdUser) {
 $thirdUser->openid = '3983f28ad03adb69fdf3-test';
 			$url = APP_PROTOCOL . APP_HTTP_HOST . "/ylyfinder/browse.php?lang=zh-cn&type=ylylisten&mpid=" . $thirdUser->openid . "&act=ylylisten";
-			$this->redirect($url);
+
+			\TPL::assign('APP_TITLE', '云录音调听');
+			\TPL::assign('ylyfinderURL', $url);
+			\TPL::output('/pl/fe/matter/ylylisten/frame');
+			die;
 		} else {
 			// 将返回地址存在cookie中
 			$callbackUrl = APP_PROTOCOL . APP_HTTP_HOST . '/rest/pl/fe';
 			$this->mySetcookie("_user_access_referer", $callbackUrl);
 
-			echo '<p>仅支持用能力开放平台账号登录的用户查看,请退出后选择"其他方式登录-电信能力开放平台"登录</p><a href="http://' . APP_HTTP_HOST . '/rest/site/fe/user?site=platform">点击跳转</a>';
+			echo '<p>仅支持用能力开放平台账号登录的用户查看,请退出后选择"其他方式登录-电信能力开放平台"登录</p><a href="' . APP_PROTOCOL . APP_HTTP_HOST . '/rest/site/fe/user?site=platform">点击跳转</a>';
 			die;
 		}
 	}

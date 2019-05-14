b950d6f83ecf09121d5e622683901456b90f9183
liao1a <153887111@qq.com>
2019-05-14 13:44:44 +0800

    删除前端代码

--- views/default/pl/fe/matter/ylylisten/preview.js
+++
@@ -1,7 +0,0 @@
-define(['frame'], function(ngApp) {
-    ngApp.provider.controller('ctrlPreview', ['$scope', 'http2', function($scope, http2) {
-    	http2.get('/rest/pl/fe/matter/ylylisten/get?site=' + $scope.siteId).then(function(rsp) {
-    		$scope.url = rsp.data.entryUrl;
-    	});
-    }]);
-});

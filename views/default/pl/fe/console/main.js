2210eb7fd00f9b673c8561efdef7d759b02102b7
aYellowApple <2764411477@qq.com>
2019-06-12 13:29:14 +0800

    debug interface

--- views/default/pl/fe/console/main.js
+++ views/default/pl/fe/console/main.js
@@ -147,18 +147,24 @@ define(['frame'], function(ngApp) {
         $scope.matterTags = function() {
             $scope.matterTagsFram(_oCriteria, filter2);
         };
-        $scope.clearCreater = function() {
-            _oCriteria.byCreater = "";
-        };
         var _oCriteria;
         $scope.criteria = _oCriteria = {
             orderBy: '',
             filter: {},
             bySite: '',
-            byCreater: '',
             byStar: 'N'
         };
-        $scope.filter = facListFilter.init(null, _oCriteria.filter);
+        function cbFilter(obj, key, value) {
+            switch(key) {
+                case 'title':
+                    _oCriteria.byTitle = value;
+                break;
+                case 'creator':
+                    _oCriteria.byCreator = value;
+                break;
+            }
+        };
+        $scope.filter = facListFilter.init(cbFilter, _oCriteria.filter);
         $scope.$watch('frameState.sid', function(nv) {
             if (!nv) return;
             _oCriteria.bySite = nv;
@@ -229,14 +235,11 @@ define(['frame'], function(ngApp) {
                 } else {
                     url = 'rest/pl/fe/matter/bySite?site=' + _oCriteria.bySite + '&category=app';
                     url += '&_=' + t;
-                    http2.post(url, { byTitle: _oCriteria.filter.keyword, byStar: _oCriteria.byStar, byCreater: _oCriteria.byCreater }, { page: _oPage }).then(function(rsp) {
+                    http2.post(url, { byTitle: _oCriteria.byTitle, byStar: _oCriteria.byStar, byCreator: _oCriteria.byCreator }, { page: _oPage }).then(function(rsp) {
                         $scope.matters = rsp.data.matters;
                     });
                 }
             }
-        };
-        $scope.clearCreater = function() {
-            _oCriteria.byCreater = "";
         };
         var _oCriteria;
         $scope.criteria = _oCriteria = {
@@ -244,10 +247,19 @@ define(['frame'], function(ngApp) {
             orderBy: '',
             filter: {},
             bySite: '',
-            byCreater: '',
             byStar: 'N'
         };
-        $scope.filter = facListFilter.init(null, _oCriteria.filter);
+        function cbFilter(obj, key, value) {
+            switch(key) {
+                case 'title':
+                    _oCriteria.byTitle = value;
+                break;
+                case 'creator':
+                    _oCriteria.byCreator = value;
+                break;
+            }
+        };
+        $scope.filter = facListFilter.init(cbFilter, _oCriteria.filter);
         //$scope.cleanFilterTag = function() {
         //    filter.byTags = filter2.byTags = '';
         //};
@@ -315,13 +327,13 @@ define(['frame'], function(ngApp) {
                 if (_oCriteria.matter.type) {
                     url = '/rest/pl/fe/matter/' + _oCriteria.matter.type + '/list?site=' + _oCriteria.bySite + '&_=' + t;
                     _oCriteria.matter.type == 'channel' && (url += '&cascade=N');
-                    http2.post(url, { byTitle: _oCriteria.filter.keyword, byStar: _oCriteria.byStar, byCreater: _oCriteria.byCreater }, { page: _oPage }).then(function(rsp) {
+                    http2.post(url, { byTitle: _oCriteria.byTitle, byStar: _oCriteria.byStar, byCreator: _oCriteria.byCreator }, { page: _oPage }).then(function(rsp) {
                         $scope.matters = rsp.data.docs || rsp.data.apps;
                     });
                 } else {
                     url = '/rest/pl/fe/matter/bySite?site=' + _oCriteria.bySite + '&category=doc&_=' + t;
                     _oCriteria.matter.type == 'channel' && (url += '&cascade=N');
-                    http2.post(url, { byTitle: _oCriteria.filter.keyword, byStar: _oCriteria.byStar, byCreater: _oCriteria.byCreater }, { page: _oPage }).then(function(rsp) {
+                    http2.post(url, { byTitle: _oCriteria.byTitle, byStar: _oCriteria.byStar, byCreator: _oCriteria.byCreator }, { page: _oPage }).then(function(rsp) {
                         $scope.matters = rsp.data.matters;
                     });
                 }
@@ -333,19 +345,25 @@ define(['frame'], function(ngApp) {
         $scope.cleanFilterTag = function() {
             filter.byTags = filter2.byTags = '';
         };
-        $scope.clearCreater = function() {
-            _oCriteria.byCreater = "";
-        };
         var _oCriteria;
         $scope.criteria = _oCriteria = {
             matter: { type: '' },
             orderBy: '',
             filter: {},
             bySite: '',
-            byCreater: '',
             byStar: 'N'
         };
-        $scope.filter = facListFilter.init(null, _oCriteria.filter);
+        function cbFilter(obj, key, value) {
+            switch(key) {
+                case 'title':
+                    _oCriteria.byTitle = value;
+                break;
+                case 'creator':
+                    _oCriteria.byCreator = value;
+                break;
+            }
+        };
+        $scope.filter = facListFilter.init(cbFilter, _oCriteria.filter);
         $scope.$watch('frameState.sid', function(nv) {
             if (!nv) return;
             _oCriteria.bySite = nv;

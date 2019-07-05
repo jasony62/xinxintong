8d2c34cbc3427ef170feb8cf69dee3ce257ef3fd
jasony62 <jasony62@qq.com>
2019-07-05 20:46:12 +0800

    update code

--- views/default/pl/fe/matter/enroll/enrollee.js
+++ views/default/pl/fe/matter/enroll/enrollee.js
@@ -1,18 +1,21 @@
-define(['frame'], function(ngApp) {
+define(['frame'], function (ngApp) {
     'use strict';
-    ngApp.provider.controller('ctrlEnrollee', ['$scope', 'http2', 'srvEnrollRecord', '$q', '$uibModal', 'tmsSchema', 'facListFilter', 'tmsRowPicker', function($scope, http2, srvEnrollRecord, $q, $uibModal, tmsSchema, facListFilter, tmsRowPicker) {
+    ngApp.provider.controller('ctrlEnrollee', ['$scope', 'http2', 'srvEnrollRecord', '$q', '$uibModal', 'tmsSchema', 'facListFilter', 'tmsRowPicker', function ($scope, http2, srvEnrollRecord, $q, $uibModal, tmsSchema, facListFilter, tmsRowPicker) {
         function _fnAbsent() {
-            http2.get('/rest/pl/fe/matter/enroll/user/undone?app=' + $scope.app.id + '&rid=' + _oCriteria.rid).then(function(rsp) {
+            http2.post('/rest/pl/fe/matter/enroll/user/undone?app=' + $scope.app.id, {
+                rids: _oCriteria.rids
+            }).then(function (rsp) {
                 var schemasById;
                 $scope.absentUsers = rsp.data.users;
+                $scope.absentRounds = rsp.data.rounds;
                 if (rsp.data.app) {
                     $scope.absentApp = rsp.data.app;
                     if ($scope.absentApp.dataSchemas) {
                         schemasById = {};
-                        $scope.absentApp.dataSchemas.forEach(function(oSchema) {
+                        $scope.absentApp.dataSchemas.forEach(function (oSchema) {
                             schemasById[oSchema.id] = oSchema;
                         });
-                        $scope.absentUsers.forEach(function(oUser) {
+                        $scope.absentUsers.forEach(function (oUser) {
                             tmsSchema.forTable(oUser, schemasById);
                         });
                     }
@@ -22,126 +25,133 @@ define(['frame'], function(ngApp) {
 
         var _oCriteria, _oRows, _oPage;
         $scope.category = 'enrollee';
-        $scope.page = _oPage = { size: 20 };
+        $scope.page = _oPage = {
+            size: 20
+        };
         $scope.criteria = _oCriteria = {
             orderby: 'enroll_num',
             //onlyEnrolled: 'Y',
-            rid: '',
-            turn_title: '全部轮次',
             filter: {}
         };
         $scope.tmsTableWrapReady = 'N';
         $scope.rows = _oRows = new tmsRowPicker();
-        $scope.$watch('rows.allSelected', function(nv) {
+        $scope.$watch('rows.allSelected', function (nv) {
             if ($scope.enrollees) {
                 _oRows.setAllSelected(nv, $scope.enrollees.length);
             }
         });
-        $scope.editCause = function(user) {
+        $scope.editCause = function (user) {
             $uibModal.open({
                 templateUrl: 'editCause.html',
-                controller: ['$scope', '$uibModalInstance', 'http2', function($scope2, $mi, http2) {
+                controller: ['$scope', '$uibModalInstance', 'http2', function ($scope2, $mi, http2) {
                     $scope2.cause = '';
                     $scope2.app = $scope.app;
-                    $scope2.cancel = function() {
+                    $scope2.cancel = function () {
                         $mi.dismiss();
                     };
-                    $scope2.ok = function() {
+                    $scope2.ok = function () {
                         var url, params = {};
                         params[user.userid] = {
                             rid: user.absent_cause.rid,
                             cause: $scope2.cause
                         }
                         url = '/rest/pl/fe/matter/enroll/update?site=' + $scope.app.siteid + '&app=' + $scope.app.id;
-                        http2.post(url, { 'absent_cause': params }).then(function(rsp) {
+                        http2.post(url, {
+                            'absent_cause': params
+                        }).then(function (rsp) {
                             $mi.close($scope2.cause);
                         });
                     };
                 }],
                 backdrop: 'static'
-            }).result.then(function(result) {
+            }).result.then(function (result) {
                 user.absent_cause.cause = result;
             });
         };
-        $scope.chooseOrderby = function(orderby) {
+        $scope.chooseOrderby = function (orderby) {
             _oCriteria.orderby = orderby;
             $scope.searchEnrollee(1);
         };
-        $scope.export = function() {
+        $scope.export = function () {
             var url = '/rest/pl/fe/matter/enroll/user/export';
             url += '?app=' + $scope.app.id;
-            url += '&rid=' + _oCriteria.rid;
+            url += '&rids=' + _oCriteria.rids;
             window.open(url);
         };
-        $scope.notify = function(isBatch) {
+        $scope.notify = function (isBatch) {
             srvEnrollRecord.notify(isBatch ? _oRows : null);
         };
-        $scope.filter = facListFilter.init(function() {
+        $scope.filter = facListFilter.init(function () {
             $scope.searchEnrollee(1);
         }, _oCriteria.filter);
-        $scope.advFilter = function() {
-            $uibModal.open({
-                templateUrl: '/views/default/pl/fe/matter/enroll/component/enrolleeFilter.html?_=2',
-                controller: ['$scope', '$uibModalInstance', function($scope2, $mi) {
-                    $scope2.app = $scope.app;
-                    $scope2.criteria = _oCriteria;
-                    $scope2.page = { size: 5 };
-                    $scope2.doSearchRound = function() {
-                        http2.get('/rest/pl/fe/matter/enroll/round/list?site=' + $scope.app.siteid + '&app=' + $scope.app.id, { page: $scope2.page }).then(function(rsp) {
-                            $scope2.rounds = rsp.data.rounds;
-                        });
-                    };
-                    $scope2.ok = function() {
-                        $scope2.rounds.forEach(function(round) {
-                            if ($scope2.criteria.rid == round.rid) {
-                                $scope2.criteria.turn_title = round.title;
-                            } else if ($scope2.criteria.rid == '') {
-                                $scope2.criteria.turn_title = '全部轮次';
-                            }
-                        });
-                        $mi.close($scope2.criteria);
-                    };
-                    $scope2.cancel = function() {
-                        $mi.dismiss();
-                    };
-                    $scope2.doSearchRound();
-                }],
-                windowClass: 'auto-height',
-                backdrop: 'static',
-            }).result.then(function(_oCriteria) {
-                $scope.searchEnrollee(1);
-                _fnAbsent();
+        $scope.advFilter = function () {
+            http2.post('/rest/script/time', {
+                html: {
+                    'enrollee': '/views/default/pl/fe/matter/enroll/component/enrolleePicker'
+                }
+            }).then(function (rsp) {
+                $uibModal.open({
+                    templateUrl: '/views/default/pl/fe/matter/enroll/component/enrolleeFilter.html?_=' + rsp.data.html.enrollee.time,
+                    controller: ['$scope', '$uibModalInstance', function ($scope2, $mi) {
+                        $scope2.app = $scope.app;
+                        $scope2.criteria = _oCriteria;
+                        $scope2.page = {
+                            size: 7
+                        };
+                        $scope2.doSearchRound = function () {
+                            http2.get('/rest/pl/fe/matter/enroll/round/list?site=' + $scope.app.siteid + '&app=' + $scope.app.id, {
+                                page: $scope2.page
+                            }).then(function (rsp) {
+                                $scope2.rounds = rsp.data.rounds;
+                            });
+                        };
+                        $scope2.ok = function () {
+                            $mi.close($scope2.criteria);
+                        };
+                        $scope2.cancel = function () {
+                            $mi.dismiss();
+                        };
+                        $scope2.doSearchRound();
+                    }],
+                    windowClass: 'auto-height',
+                    backdrop: 'static',
+                }).result.then(function (_oCriteria) {
+                    $scope.searchEnrollee(1);
+                    _fnAbsent();
+                });
             });
         };
-        $scope.searchEnrollee = function(pageAt) {
+        $scope.searchEnrollee = function (pageAt) {
             var url;
 
             _oRows.reset();
             pageAt && (_oPage.at = pageAt);
             url = '/rest/pl/fe/matter/enroll/user/enrollee?app=' + $scope.app.id;
-            http2.post(url, _oCriteria, { page: _oPage }).then(function(rsp) {
+            http2.post(url, _oCriteria, {
+                page: _oPage
+            }).then(function (rsp) {
                 srvEnrollRecord.init($scope.app, _oPage, _oCriteria, rsp.data.users);
                 $scope.enrollees = rsp.data.users;
             });
         };
-        $scope.repairEnrollee = function() {
+        $scope.repairEnrollee = function () {
             var url = '/rest/pl/fe/matter/enroll/user/repair?site=' + $scope.app.siteid;
             url += '&app=' + $scope.app.id;
-            http2.get(url).then(function(rsp) {
+            http2.get(url).then(function (rsp) {
                 $scope.searchEnrollee(1);
             });
         };
-        $scope.repairGroup = function() {
+        $scope.repairGroup = function () {
             var url = '/rest/pl/fe/matter/enroll/user/repairGroup?site=' + $scope.app.siteid;
             url += '&app=' + $scope.app.id;
-            http2.get(url).then(function(rsp) {
+            http2.get(url).then(function (rsp) {
                 $scope.searchEnrollee(1);
             });
         };
-        $scope.toggleAbsent = function() {
+        $scope.toggleAbsent = function () {
             $scope.category = $scope.category === 'absent' ? 'enrollee' : 'absent';
         };
-        $scope.$watch('app.entryRule', function(oRule) {
+        $scope.$watch('app.entryRule', function (oRule) {
             if (!oRule) return;
             $scope.rule = oRule;
             $scope.tmsTableWrapReady = 'Y';
--- views/default/pl/fe/matter/enroll/remark.js
+++ views/default/pl/fe/matter/enroll/remark.js
@@ -1,13 +1,15 @@
-define(['frame'], function(ngApp) {
+define(['frame'], function (ngApp) {
     'use strict';
-    ngApp.provider.controller('ctrlRemark', ['$scope', '$parse', '$filter', '$location', '$q', '$uibModal', 'http2', 'srvEnrollRecord', 'tmsSchema', 'tmsRowPicker', function($scope, $parse, $filter, $location, $q, $uibModal, http2, srvEnrollRecord, tmsSchema, tmsRowPicker) {
+    ngApp.provider.controller('ctrlRemark', ['$scope', '$parse', '$filter', '$location', '$q', '$uibModal', 'http2', 'srvEnrollRecord', 'tmsSchema', 'tmsRowPicker', function ($scope, $parse, $filter, $location, $q, $uibModal, http2, srvEnrollRecord, tmsSchema, tmsRowPicker) {
         function list(oPage) {
             var defer,
                 url;
 
             defer = $q.defer();
             url = '/rest/pl/fe/matter/enroll/remark/byApp?app=' + $location.search().id;
-            http2.post(url, oCriteria, { page: oPage }).then(function(rsp) {
+            http2.post(url, oCriteria, {
+                page: oPage
+            }).then(function (rsp) {
                 defer.resolve(rsp.data);
             });
             return defer.promise;
@@ -15,29 +17,35 @@ define(['frame'], function(ngApp) {
         }
 
         var oAgreedLabel, _oApp, oPage, oCriteria;
-        oAgreedLabel = { 'Y': '推荐', 'N': '屏蔽', 'A': '接受' };
-        $scope.page = oPage = { size: 30 };
+        oAgreedLabel = {
+            'Y': '推荐',
+            'N': '屏蔽',
+            'A': '接受'
+        };
+        $scope.page = oPage = {
+            size: 30
+        };
         $scope.criteria = oCriteria = {
             orderby: 'create_at',
             agreed: 'all'
         };
         // 选中的记录
         $scope.rows = new tmsRowPicker();
-        $scope.$watch('rows.allSelected', function(checked) {
+        $scope.$watch('rows.allSelected', function (checked) {
             $scope.rows.setAllSelected(checked, $scope.remarks);
         });
-        $scope.doSearch = function(pageAt) {
+        $scope.doSearch = function (pageAt) {
             if (pageAt) {
                 oPage.at = pageAt;
             }
-            list(oPage).then(function(oResult) {
+            list(oPage).then(function (oResult) {
                 var oAssocRecords, aRemarks;
                 oAssocRecords = oResult.records;
-                angular.forEach(oAssocRecords, function(oRecord) {
+                angular.forEach(oAssocRecords, function (oRecord) {
                     tmsSchema.forTable(oRecord, $scope.app._schemasById);
                 });
                 aRemarks = oResult.remarks;
-                aRemarks.forEach(function(oRemark) {
+                aRemarks.forEach(function (oRemark) {
                     var oRemarkTarget, oTargetSchema, targetContent, targetData;
                     oRemark._agreed = oAgreedLabel[oRemark.agreed] || '未表态';
                     if (oAssocRecords[oRemark.enroll_key]) {
@@ -46,12 +54,20 @@ define(['frame'], function(ngApp) {
                             if (_oApp._schemasById[oRemark.schema_id]) {
                                 oTargetSchema = _oApp._schemasById[oRemark.schema_id];
                                 oRemarkTarget.title = oTargetSchema.title;
-                                targetContent = tmsSchema.strRecData(oAssocRecords[oRemark.enroll_key]._data, [oTargetSchema], { fnDataFilter: function(dataId) { return dataId == oRemark.data_id; } });
+                                targetContent = tmsSchema.strRecData(oAssocRecords[oRemark.enroll_key]._data, [oTargetSchema], {
+                                    fnDataFilter: function (dataId) {
+                                        return dataId == oRemark.data_id;
+                                    }
+                                });
                                 oRemarkTarget.content = targetContent;
                             }
                         } else {
                             /* 对记录进行评论 */
-                            targetContent = tmsSchema.strRecData(oAssocRecords[oRemark.enroll_key]._data, _oApp.dynaDataSchemas, { fnSchemaFilter: function(oSchema) { return oSchema.shareable === 'Y'; } });
+                            targetContent = tmsSchema.strRecData(oAssocRecords[oRemark.enroll_key]._data, _oApp.dynaDataSchemas, {
+                                fnSchemaFilter: function (oSchema) {
+                                    return oSchema.shareable === 'Y';
+                                }
+                            });
                             oRemarkTarget.content = targetContent;
                         }
                         oRemarkTarget.nickname = oAssocRecords[oRemark.enroll_key].nickname;
@@ -60,42 +76,42 @@ define(['frame'], function(ngApp) {
                 $scope.remarks = aRemarks;
             });
         };
-        $scope.filter = function() {
+        $scope.filter = function () {
             $uibModal.open({
                 templateUrl: '/views/default/pl/fe/matter/enroll/component/remarkFilter.html?_=1',
-                controller: ['$scope', '$uibModalInstance', function($scope2, $mi) {
-                    http2.get('/rest/pl/fe/matter/enroll/user/enrollee?site=' + $location.search().site + '&app=' + $location.search().id).then(function(rsp) {
+                controller: ['$scope', '$uibModalInstance', function ($scope2, $mi) {
+                    http2.post('/rest/pl/fe/matter/enroll/user/enrollee?site=' + $location.search().site + '&app=' + $location.search().id, {}).then(function (rsp) {
                         $scope2.enrollees = rsp.data.users;
                     });
-                    http2.get('/rest/pl/fe/matter/enroll/user/remarker?site=' + $location.search().site + '&app=' + $location.search().id).then(function(rsp) {
+                    http2.get('/rest/pl/fe/matter/enroll/user/remarker?site=' + $location.search().site + '&app=' + $location.search().id).then(function (rsp) {
                         $scope2.remarkers = rsp.data.users;
                     });
                     $scope2.criteria = oCriteria;
-                    $scope2.ok = function() {
+                    $scope2.ok = function () {
                         $mi.close($scope2.criteria);
                     };
-                    $scope2.cancel = function() {
+                    $scope2.cancel = function () {
                         $mi.dismiss();
                     };
                 }],
                 windowClass: 'auto-height',
                 backdrop: 'static',
-            }).result.then(function(oCriteria) {
+            }).result.then(function (oCriteria) {
                 $scope.doSearch(1);
             });
         };
-        $scope.chooseOrderby = function(orderby) {
+        $scope.chooseOrderby = function (orderby) {
             oCriteria.orderby = orderby;
             $scope.doSearch(1);
         };
-        $scope.setAgreed = function(result) {
+        $scope.setAgreed = function (result) {
             var remarkIds = [];
             for (var i in $scope.rows.selected) {
                 if ($scope.rows.selected[i]) {
                     remarkIds.push($scope.remarks[i].id);
                 }
             }
-            srvEnrollRecord.agreeRemark(remarkIds, result).then(function(rsp) {
+            srvEnrollRecord.agreeRemark(remarkIds, result).then(function (rsp) {
                 for (var i in $scope.rows.selected) {
                     if ($scope.rows.selected[i]) {
                         $scope.remarks[i].agreed = result;
@@ -104,20 +120,20 @@ define(['frame'], function(ngApp) {
                 }
             });
         };
-        $scope.export = function() {
+        $scope.export = function () {
             var url;
             url = '/rest/pl/fe/matter/enroll/remark/export';
             url += '?app=' + $location.search().id;
             window.open(url);
         };
-        $scope.gotoRemark = function(oRemark) {
+        $scope.gotoRemark = function (oRemark) {
             var oSearch = $location.search();
             oSearch.ek = oRemark.enroll_key;
             oSearch.schema = oRemark.schema_id;
             oSearch.remark = oRemark.id;
             $location.path('/rest/pl/fe/matter/enroll/editor');
         };
-        $scope.$watch('app', function(oApp) {
+        $scope.$watch('app', function (oApp) {
             if (oApp) {
                 _oApp = oApp;
                 $scope.doSearch(1);

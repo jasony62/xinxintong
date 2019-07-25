d85c89ae3faa15b9ca54a79e9415eb78172bfaef
jasony62 <jasony62@qq.com>
2019-07-25 09:17:43 +0800

    update code

--- views/default/site/fe/user/member.js
+++ views/default/site/fe/user/member.js
@@ -4,19 +4,20 @@ require('./member.css');
 require('../../../../../asset/js/xxt.ui.notice.js');
 require('../../../../../asset/js/xxt.ui.http.js');
 require('../../../../../asset/js/xxt.ui.image.js');
+require('../../../../../asset/js/xxt.ui.schema.js');
 require('../matter/enroll/directive.js');
 
-var ngApp = angular.module('app', ['ui.bootstrap', 'notice.ui.xxt', 'http.ui.xxt', 'directive.enroll']);
-ngApp.config(['$locationProvider', function($locationProvider) {
+var ngApp = angular.module('app', ['ui.bootstrap', 'notice.ui.xxt', 'http.ui.xxt', 'directive.enroll', 'schema.ui.xxt']);
+ngApp.config(['$locationProvider', function ($locationProvider) {
     $locationProvider.html5Mode(true);
 }]);
-ngApp.directive('tmsImageInput', ['$compile', '$q', function($compile, $q) {
+ngApp.directive('tmsImageInput', ['$compile', '$q', function ($compile, $q) {
     var aModifiedImgFields;
     aModifiedImgFields = [];
     return {
         restrict: 'A',
-        controller: ['$scope', '$timeout', 'noticebox', function($scope, $timeout, noticebox) {
-            $scope.chooseImage = function(oSchema) {
+        controller: ['$scope', '$timeout', 'noticebox', function ($scope, $timeout, noticebox) {
+            $scope.chooseImage = function (oSchema) {
                 var oExtattr;
                 if ($scope.member.extattr) {
                     oExtattr = $scope.member.extattr;
@@ -31,17 +32,17 @@ ngApp.directive('tmsImageInput', ['$compile', '$q', function($compile, $q) {
                         return;
                     }
                 }
-                window.xxt.image.choose($q.defer()).then(function(imgs) {
+                window.xxt.image.choose($q.defer()).then(function (imgs) {
                     var phase;
                     phase = $scope.$root.$$phase;
                     if (phase === '$digest' || phase === '$apply') {
                         oExtattr[oSchema.id] = oExtattr[oSchema.id].concat(imgs);
                     } else {
-                        $scope.$apply(function() {
+                        $scope.$apply(function () {
                             oExtattr[oSchema.id] = oExtattr[oSchema.id].concat(imgs);
                         });
                     }
-                    $timeout(function() {
+                    $timeout(function () {
                         var i, j, img, eleImg;
                         for (i = 0, j = imgs.length; i < j; i++) {
                             img = imgs[i];
@@ -53,30 +54,32 @@ ngApp.directive('tmsImageInput', ['$compile', '$q', function($compile, $q) {
                     });
                 });
             };
-            $scope.removeImage = function(oSchema, index) {
+            $scope.removeImage = function (oSchema, index) {
                 $scope.member.extattr[oSchema.id].splice(index, 1);
             };
         }]
     }
 }]);
-ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation', 'http2', function($scope, $timeout, noticebox, LS, http2) {
-    var fnValidate = function(oSchema) {
-        var required = function(value, len, alerttext) {
+ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation', 'http2', 'tmsSchema', function ($scope, $timeout, noticebox, LS, http2, tmsSchema) {
+    function fnValidate(oSchema) {
+        function required(value, len, alerttext) {
             if (value == null || value == "" || value.length < len) {
                 noticebox.warn(alerttext);
                 return false;
             } else
                 return true;
-        };
-        var isMobile = function(value, alerttext) {
+        }
+
+        function isMobile(value, alerttext) {
             if (false === /^1[3|4|5|7|8|9][0-9]\d{4,8}$/.test(value)) {
                 noticebox.warn(alerttext);
                 return false;
             } else {
                 return true;
             }
-        };
-        var isEmail = function(value, alerttext) {
+        }
+
+        function isEmail(value, alerttext) {
             if (value === undefined) {
                 noticebox.warn(alerttext);
                 return false;
@@ -89,7 +92,8 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
             } else {
                 return true;
             }
-        };
+        }
+
         var member = $scope.member;
         if (member.name && false === required(member.name, 2, '请提供您的姓名！')) {
             return false;
@@ -100,22 +104,38 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
         if (member.email && false === isEmail(member.email, '请提供正确的邮箱！')) {
             return false;
         }
+        if (oSchema.extAttrs && oSchema.extAttrs.length) {
+            var oExtValue, oExtAttr, sCheckResult;
+            oExtValue = member.extattr || {};
+            for (var i = 0, ii = oSchema.extAttrs.length; i < ii; i++) {
+                oExtAttr = oSchema.extAttrs[i];
+                if (oExtAttr.required && (oExtAttr.required === 'Y')) {
+                    sCheckResult = tmsSchema.checkValue(oExtAttr, oExtValue[oExtAttr.id]);
+                    if (true !== sCheckResult) {
+                        noticebox.warn(sCheckResult);
+                        return false;
+                    }
+                }
+            }
+        }
         return true;
     };
 
     function sendRequest(url) {
         var oPosted;
         $scope.posting = true;
-        http2.post(url, $scope.member, { autoBreak: false }).then(function(rsp) {
+        http2.post(url, $scope.member, {
+            autoBreak: false
+        }).then(function (rsp) {
             $scope.posting = false;
-            http2.get(LS.j('passed', 'site', 'schema') + '&redirect=N').then(function(rsp) {
+            http2.get(LS.j('passed', 'site', 'schema') + '&redirect=N').then(function (rsp) {
                 if (window.parent && window.parent.onClosePlugin) {
                     window.parent.onClosePlugin(rsp.data);
                 } else {
                     location.href = rsp.data;
                 }
             });
-        }, function() {
+        }, function () {
             $scope.posting = false;
         });
     }
@@ -149,20 +169,23 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
     $scope.loginUser = {};
     $scope.subView = 'login';
     $scope.isRegister = false;
-    $scope.switchSubView = function(name) {
+    $scope.switchSubView = function (name) {
         $scope.subView = name;
     };
-    $scope.openThirdAppUrl = function(thirdApp) {
+    $scope.openThirdAppUrl = function (thirdApp) {
         location.href = '/rest/site/fe/user/login/byRegAndThird?thirdId=' + thirdApp.id;
     };
-    $scope.login = function() {
-        if($scope.loginUser.password) {
-            http2.post('/rest/site/fe/user/login/do?site=' + LS.s().site, $scope.loginUser).then(function(rsp) {
-                http2.post('/rest/site/fe/user/login/checkPwdStrength', {'account':$scope.loginUser.uname,'password':$scope.loginUser.password}).then(function(rsp) {
-                    if(!rsp.data.strength) {
+    $scope.login = function () {
+        if ($scope.loginUser.password) {
+            http2.post('/rest/site/fe/user/login/do?site=' + LS.s().site, $scope.loginUser).then(function (rsp) {
+                http2.post('/rest/site/fe/user/login/checkPwdStrength', {
+                    'account': $scope.loginUser.uname,
+                    'password': $scope.loginUser.password
+                }).then(function (rsp) {
+                    if (!rsp.data.strength) {
                         alert(rsp.data.msg);
                     }
-                    http2.get(LS.j('get', 'site', 'schema')).then(function(rsp) {
+                    http2.get(LS.j('get', 'site', 'schema')).then(function (rsp) {
                         var user = rsp.data;
                         $scope.user = user;
                         setMember(user);
@@ -171,37 +194,37 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
             });
         }
     };
-    $scope.loginByReg = function(oRegUser) {
-        http2.post('/rest/site/fe/user/login/byRegAndWxopenid?site=' + LS.s().site, oRegUser).then(function(rsp) {
+    $scope.loginByReg = function (oRegUser) {
+        http2.post('/rest/site/fe/user/login/byRegAndWxopenid?site=' + LS.s().site, oRegUser).then(function (rsp) {
             location.reload(true);
         });
     };
-    $scope.logout = function() {
-        http2.post('/rest/site/fe/user/logout/do?site=' + LS.s().site, $scope.loginUser).then(function(rsp) {
+    $scope.logout = function () {
+        http2.post('/rest/site/fe/user/logout/do?site=' + LS.s().site, $scope.loginUser).then(function (rsp) {
             location.reload(true);
         });
     };
-    $scope.register = function() {
+    $scope.register = function () {
         http2.post('/rest/site/fe/user/register/do?site=' + LS.s().site, {
             uname: $scope.loginUser.uname,
             nickname: $scope.loginUser.nickname,
             password: $scope.loginUser.password
-        }).then(function(rsp) {
+        }).then(function (rsp) {
             $scope.user = rsp.data;
             setMember($scope.user);
         });
     };
-    $scope.gotoHome = function() {
+    $scope.gotoHome = function () {
         location.href = '/rest/site/fe/user?site=' + LS.s().site;
     };
-    $scope.repeatPwd = (function() {
+    $scope.repeatPwd = (function () {
         return {
-            test: function(value) {
+            test: function (value) {
                 return value === $scope.password;
             }
         };
     })();
-    $scope.doAuth = function(ignoreCheck) {
+    $scope.doAuth = function (ignoreCheck) {
         if (!ignoreCheck) {
             if (!fnValidate($scope.schema)) {
                 return;
@@ -213,7 +236,7 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
         }
         sendRequest(LS.j('doAuth', 'site', 'schema'));
     };
-    $scope.doReauth = function() {
+    $scope.doReauth = function () {
         if (!fnValidate($scope.schema)) return;
         if (document.querySelectorAll('.ng-invalid-required').length) {
             noticebox.warn('请填写必填项');
@@ -221,7 +244,7 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
         }
         sendRequest(LS.j('doReauth', 'site', 'schema'));
     };
-    $scope.refreshPin = function(preEle) {
+    $scope.refreshPin = function (preEle) {
         var time, url, pinWidth;
         preEle ? preEle : preEle = document.getElementById('pinInput');
         if (preEle) {
@@ -232,18 +255,20 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
             $scope.pinImg = url + '&_=' + time;
         }
     };
-    $scope.shiftRegUser = function(oOtherRegUser) {
-        http2.post('/rest/site/fe/user/shiftRegUser?site=' + LS.s().site, { uname: oOtherRegUser.uname }).then(function(rsp) {
+    $scope.shiftRegUser = function (oOtherRegUser) {
+        http2.post('/rest/site/fe/user/shiftRegUser?site=' + LS.s().site, {
+            uname: oOtherRegUser.uname
+        }).then(function (rsp) {
             location.reload(true);
         });
     };
-    http2.get('/rest/site/fe/get?site=' + LS.s().site).then(function(rsp) {
+    http2.get('/rest/site/fe/get?site=' + LS.s().site).then(function (rsp) {
         $scope.site = rsp.data;
-        http2.get('/rest/site/fe/user/memberschema/get?site=' + LS.s().site + '&schema=' + LS.s().schema + '&matter=' + LS.s().matter).then(function(rsp) {
+        http2.get('/rest/site/fe/user/memberschema/get?site=' + LS.s().site + '&schema=' + LS.s().schema + '&matter=' + LS.s().matter).then(function (rsp) {
             var oMschema;
             $scope.schema = oMschema = rsp.data.schema;
             $scope.matter = rsp.data.matter;
-            http2.get(LS.j('get', 'site', 'schema')).then(function(rsp2) {
+            http2.get(LS.j('get', 'site', 'schema')).then(function (rsp2) {
                 $scope.user = rsp2.data;
                 /*内置用户认证信息*/
                 setMember($scope.user);
@@ -253,14 +278,14 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
                         $scope.loginUser.nickname = $scope.user.sns.wx.nickname;
                     }
                 }
-                $timeout(function() {
+                $timeout(function () {
                     var preEle = document.getElementById('pinInput');
                     if (preEle) {
                         $scope.refreshPin(preEle);
                     }
                 });
                 /* 解决多个注册账号的问题 */
-                http2.get('/rest/site/fe/user/get?site=' + LS.s().site).then(function(rsp) {
+                http2.get('/rest/site/fe/user/get?site=' + LS.s().site).then(function (rsp) {
                     if (rsp.data.siteRegistersByWx) {
                         $scope.user.siteRegistersByWx = rsp.data.siteRegistersByWx;
                     }
@@ -271,10 +296,10 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
             });
         });
     });
-    http2.get('/rest/site/fe/user/login/thirdList').then(function(rsp) {
+    http2.get('/rest/site/fe/user/login/thirdList').then(function (rsp) {
         $scope.thirdApps = rsp.data;
     });
-    http2.get('/rest/site/fe/user/getSafetyLevel').then(function(rsp) {
+    http2.get('/rest/site/fe/user/getSafetyLevel').then(function (rsp) {
         $scope.isRegister = rsp.data.register;
     });
     $scope.isSmallLayout = false;

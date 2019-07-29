2fd6df108637ee98f8f96c19a758f7ab8048a98e
jasony62 <jasony62@qq.com>
2019-07-29 11:14:40 +0800

    update code

--- models/sns/wx.php
+++ models/sns/wx.php
@@ -4,30 +4,30 @@ namespace sns;
  * 微信公众号
  */
 class wx_model extends \TMS_MODEL {
-	/**
-	 * 站点绑定的公众号
-	 */
-	public function &bySite($siteId, $options = []) {
-		$fields = isset($options['fields']) ? $options['fields'] : '*';
-		$q = array(
-			$fields,
-			'xxt_site_wx',
-			['siteid' => $siteId]
-		);
-		$wx = $this->query_obj_ss($q);
+    /**
+     * 站点绑定的公众号
+     */
+    public function &bySite($siteId, $options = []) {
+        $fields = isset($options['fields']) ? $options['fields'] : '*';
+        $q = array(
+            $fields,
+            'xxt_site_wx',
+            ['siteid' => $siteId],
+        );
+        $wx = $this->query_obj_ss($q);
 
-		return $wx;
-	}
-	/**
-	 * 创建绑定的公众号配置信息
-	 */
-	public function &create($siteId, $data = []) {
-		$data['siteid'] = $this->escape($siteId);
+        return $wx;
+    }
+    /**
+     * 创建绑定的公众号配置信息
+     */
+    public function &create($siteId, $data = []) {
+        $data['siteid'] = $this->escape($siteId);
 
-		$this->insert('xxt_site_wx', $data, false);
+        $this->insert('xxt_site_wx', $data, false);
 
-		$wx = $this->bySite($siteId);
+        $wx = $this->bySite($siteId);
 
-		return $wx;
-	}
+        return $wx;
+    }
 }

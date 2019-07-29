16ae43e7a191af1d16009809d2c7cadb3fc8eaa3
liao1a <153887111@qq.com>
2019-07-29 09:16:16 +0800

    文件限制参数返回给前端

--- tmsappconfig.php
+++ tmsappconfig.php
@@ -3,6 +3,8 @@ include_once dirname(__FILE__) . '/config.php';
 
 $config = new \stdClass;
 $config->noHookMaxTime = TMS_APP_NOHOOK_MAXTIME;
+$config->fileMaxSize = TMS_UPLOAD_FILE_MAXSIZE;
+$config->fileContentTypeWhite = TMS_UPLOAD_FILE_CONTENTTYPE_WHITE;
 
 $config = json_encode($config);
 

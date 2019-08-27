cfadc989539d0bf6c41f7ccb53094237359b47e8
jasony62 <jasony62@qq.com>
2019-08-27 14:08:20 +0800

    update code

--- views/default/pl/fe/matter/mission/plan.js
+++ views/default/pl/fe/matter/mission/plan.js
@@ -1,6 +1,6 @@
 'use strict';
 var ngApp = angular.module('app', ['ngRoute', 'ui.bootstrap', 'ui.tms', 'ui.xxt', 'http.ui.xxt', 'notice.ui.xxt', 'service.matter', 'protect.ui.xxt']);
-ngApp.config(['$locationProvider', 'srvSiteProvider', function($locationProvider, srvSiteProvider) {
+ngApp.config(['$locationProvider', 'srvSiteProvider', function ($locationProvider, srvSiteProvider) {
     $locationProvider.html5Mode(true);
     var ls, siteId;
     ls = location.search;
@@ -8,52 +8,68 @@ ngApp.config(['$locationProvider', 'srvSiteProvider', function($locationProvider
     //
     srvSiteProvider.config(siteId);
 }]);
-ngApp.controller('ctrlMain', ['$scope', '$location', 'srvSite', 'mediagallery', 'http2', 'tkEntryRule', function($scope, $location, srvSite, mediagallery, http2, tkEntryRule) {
+ngApp.controller('ctrlMain', ['$scope', '$location', 'srvSite', 'mediagallery', 'http2', 'tkEntryRule', function ($scope, $location, srvSite, mediagallery, http2, tkEntryRule) {
     var _oProto, _oBeforeProto;
     $scope.proto = _oProto = {
         id: '_pending',
         siteid: $location.search().site,
-        entryRule: { scope: {} },
-        app: { enroll: {}, signin: {}, group: { source: '' } },
+        entryRule: {
+            scope: {}
+        },
+        app: {
+            enroll: {},
+            signin: {},
+            group: {
+                source: ''
+            }
+        },
         userApp: ''
     };
     _oBeforeProto = angular.copy(_oProto);
     $scope.entryRule = _oProto.entryRule;
-    $scope.setPic = function() {
+    $scope.setPic = function () {
         var options = {
-            callback: function(url) {
+            callback: function (url) {
                 _oProto.pic = url + '?_=' + (new Date * 1);
             }
         };
         mediagallery.open($scope.site.id, options);
     };
-    $scope.removePic = function() {
+    $scope.removePic = function () {
         _oProto.pic = '';
     };
-    $scope.$on('xxt.tms-datepicker.change', function(event, data) {
+    $scope.$on('xxt.tms-datepicker.change', function (event, data) {
         var prop;
         if (data.state.indexOf('proto.') === 0) {
             prop = data.state.substr(8);
             _oProto[prop] = data.value;
         }
     });
-    $scope.doCreate = function() {
-        http2.post('/rest/pl/fe/matter/mission/create?site=' + $scope.site.id, _oProto).then(function(rsp) {
+    $scope.doCreate = function () {
+        http2.post('/rest/pl/fe/matter/mission/create?site=' + $scope.site.id, _oProto).then(function (rsp) {
             location.href = '/rest/pl/fe/matter/mission?site=' + $scope.site.id + '&id=' + rsp.data.id;
         });
     };
-    srvSite.get().then(function(oSite) {
+    srvSite.get().then(function (oSite) {
         $scope.site = oSite;
         _oProto.pic = oSite.heading_pic;
     });
-    srvSite.getLoginUser().then(function(oUser) {
+    srvSite.getLoginUser().then(function (oUser) {
         $scope.loginUser = oUser;
         _oProto.title = oUser.nickname + '的项目';
     });
-    http2.post('/rest/script/time', { html: { 'entryRule': '/views/default/pl/fe/_module/entryRule' } }).then(function(rsp) {
-        $scope.frameTemplates = { html: { 'entryRule': '/views/default/pl/fe/_module/entryRule.html?_=' + rsp.data.html.entryRule.time } };
+    http2.post('/rest/script/time', {
+        html: {
+            'entryRule': '/views/default/pl/fe/_module/entryRule'
+        }
+    }).then(function (rsp) {
+        $scope.frameTemplates = {
+            html: {
+                'entryRule': '/views/default/pl/fe/_module/entryRule.html?_=' + rsp.data.html.entryRule.time
+            }
+        };
     });
-    srvSite.snsList().then(function(oSns) {
+    srvSite.snsList().then(function (oSns) {
         $scope.tkEntryRule = new tkEntryRule(_oProto, oSns, true, ['group', 'enroll']);
     });
 }]);

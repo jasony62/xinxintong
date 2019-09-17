998fea5df2bbddab4c66cf23f6485f30b8966175
jasony62 <jasony62@qq.com>
2019-09-17 23:01:47 +0800

    清除代码

--- job/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-spl_autoload_register(function ($class) {
-	if (strpos($class, 'job') === 0) {
-		$class = str_replace('job\\', '', $class);
-		$classPath = str_replace('\\', '/', $class);
-		include __DIR__ . '/' . $classPath . '.php';
-	}
-});

a3802bc98f084324969c48bf487f000b08d84f58
jasony62 <jasony62@qq.com>
2019-09-18 17:48:10 +0800

    update code

--- models/matter/enroll/record.php
+++ models/matter/enroll/record.php
@@ -30,7 +30,6 @@ class record_model extends record_base {
 			'enroll_key' => $ek,
 			'userid' => empty($oUser->uid) ? '' : $oUser->uid,
 			'group_id' => empty($oUser->group_id) ? '' : $oUser->group_id,
-			'referrer' => $referrer,
 		];
 		/* 记录的状态 */
 		if (isset($aOptions['state']) && in_array($aOptions['state'], ['1', '99'])) {

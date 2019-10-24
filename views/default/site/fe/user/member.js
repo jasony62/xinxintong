5eac78bd22d4a47beae7259dc7e85bfbe15c3197
jasony62 <jasony62@qq.com>
2019-10-24 19:46:45 +0800

    update code

--- views/default/site/fe/user/member.js
+++ views/default/site/fe/user/member.js
@@ -122,17 +122,28 @@ ngApp.controller('ctrlMember', ['$scope', '$timeout', 'noticebox', 'tmsLocation'
     };
 
     function sendRequest(url) {
-        var oPosted;
         $scope.posting = true;
         http2.post(url, $scope.member, {
             autoBreak: false
         }).then(function (rsp) {
+            var actions = [{
+                label: '取消',
+                value: 'cancel'
+            }, {
+                label: '离开',
+                value: 'continue',
+                execWait: 5000
+            }];
             $scope.posting = false;
-            http2.get(LS.j('passed', 'site', 'schema') + '&redirect=N').then(function (rsp) {
-                if (window.parent && window.parent.onClosePlugin) {
-                    window.parent.onClosePlugin(rsp.data);
-                } else {
-                    location.href = rsp.data;
+            noticebox.confirm('已经提交成功，离开页面！', actions).then(function (action) {
+                if (action === 'continue') {
+                    http2.get(LS.j('passed', 'site', 'schema') + '&redirect=N').then(function (rsp) {
+                        if (window.parent && window.parent.onClosePlugin) {
+                            window.parent.onClosePlugin(rsp.data);
+                        } else {
+                            location.href = rsp.data;
+                        }
+                    });
                 }
             });
         }, function () {

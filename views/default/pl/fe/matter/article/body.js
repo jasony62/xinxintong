8a752e0791087274c56a0223cc74fb64759bd059
jasony62 <jasony62@qq.com>
2019-11-27 20:51:06 +0800

    update code

--- views/default/pl/fe/matter/article/body.js
+++ views/default/pl/fe/matter/article/body.js
@@ -1,10 +1,11 @@
-define(['frame'], function(ngApp) {
-    ngApp.provider.controller('ctrlBody', ['$scope', '$uibModal', 'http2', 'noticebox', 'srvSite', 'mediagallery', 'noticebox', 'srvApp', 'cstApp', '$timeout', 'tmsThumbnail', function($scope, $uibModal, http2, noticebox, srvSite, mediagallery, noticebox, srvApp, cstApp, $timeout, tmsThumbnail) {
-        var tinymceEditor, modifiedData = {};
+define(['frame'], function (ngApp) {
+    ngApp.provider.controller('ctrlBody', ['$scope', '$uibModal', 'http2', 'noticebox', 'srvSite', 'mediagallery', 'noticebox', 'srvApp', 'cstApp', '$timeout', 'tmsThumbnail', function ($scope, $uibModal, http2, noticebox, srvSite, mediagallery, noticebox, srvApp, cstApp, $timeout, tmsThumbnail) {
+        var tinymceEditor;
+        var modifiedData = {};
 
         $scope.modified = false;
         $scope.innerlinkTypes = cstApp.innerlink;
-        window.onbeforeunload = function(e) {
+        window.onbeforeunload = function (e) {
             var message;
             if ($scope.modified) {
                 message = '修改还没有保存，是否要离开当前页面？',
@@ -20,16 +21,16 @@ define(['frame'], function(ngApp) {
                 }
             }
         };
-        $scope.submit = function() {
-            http2.post('/rest/pl/fe/matter/article/update?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id, modifiedData).then(function() {
+        $scope.submit = function () {
+            http2.post('/rest/pl/fe/matter/article/update?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id, modifiedData).then(function () {
                 modifiedData = {};
                 $scope.modified = false;
                 noticebox.success('完成保存');
             });
         };
-        $scope.remove = function() {
+        $scope.remove = function () {
             if (window.confirm('确定删除？')) {
-                http2.get('/rest/pl/fe/matter/article/remove?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id).then(function(rsp) {
+                http2.get('/rest/pl/fe/matter/article/remove?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id).then(function (rsp) {
                     if ($scope.editing.mission) {
                         location = "/rest/pl/fe/matter/mission?site=" + $scope.editing.siteid + "&id=" + $scope.editing.mission.id;
                     } else {
@@ -38,7 +39,7 @@ define(['frame'], function(ngApp) {
                 });
             }
         };
-        $scope.$on('tinymce.multipleimage.open', function(event, callback) {
+        $scope.$on('tinymce.multipleimage.open', function (event, callback) {
             var options = {
                 callback: callback,
                 multiple: true,
@@ -46,7 +47,7 @@ define(['frame'], function(ngApp) {
             };
             mediagallery.open($scope.editing.siteid, options);
         });
-        $scope.embedMatter = function() {
+        $scope.embedMatter = function () {
             var options = {
                 matterTypes: $scope.innerlinkTypes,
                 singleMatter: true
@@ -54,7 +55,7 @@ define(['frame'], function(ngApp) {
             if ($scope.editing.mission) {
                 options.mission = $scope.editing.mission;
             }
-            srvSite.openGallery(options).then(function(result) {
+            srvSite.openGallery(options).then(function (result) {
                 var editor = tinymce.get('body1'),
                     dom = editor.dom,
                     selection = editor.selection,
@@ -68,7 +69,7 @@ define(['frame'], function(ngApp) {
                         while (sibling.parentNode !== editor.getBody()) {
                             sibling = sibling.parentNode;
                         }
-                        angular.forEach(result.matters, function(matter) {
+                        angular.forEach(result.matters, function (matter) {
                             fn = "openMatter($event,'" + matter.id + "','" + result.type + "')";
                             domMatter = dom.create('p', {
                                 'wrap': 'matter'
@@ -81,7 +82,7 @@ define(['frame'], function(ngApp) {
                         });
                     } else {
                         /*没有选中页面上的元素*/
-                        angular.forEach(result.matters, function(matter) {
+                        angular.forEach(result.matters, function (matter) {
                             fn = "openMatter($event,'" + matter.id + "','" + result.type + "')";
                             domMatter = dom.add(editor.getBody(), 'p', {
                                 'wrap': 'matter'
@@ -96,7 +97,7 @@ define(['frame'], function(ngApp) {
                 }
             });
         };
-        var insertLink = function(data) {
+        var insertLink = function (data) {
             var editor, dom, html;
             if (data.url.length > 0) {
                 editor = tinymce.get('body1');
@@ -109,28 +110,28 @@ define(['frame'], function(ngApp) {
                 editor.insertContent('<p>&nbsp;</p>' + html + '<p>&nbsp;</p>');
             }
         }
-        $scope.embedLink = function() {
+        $scope.embedLink = function () {
             $uibModal.open({
                 templateUrl: 'insertMedia.html',
-                controller: ['$uibModalInstance', '$scope', function($mi, $scope) {
+                controller: ['$uibModalInstance', '$scope', function ($mi, $scope) {
                     $scope.embedType = 'link';
                     $scope.data = {
                         url: '',
                         text: ''
                     };
-                    $scope.cancel = function() {
+                    $scope.cancel = function () {
                         $mi.dismiss()
                     };
-                    $scope.ok = function() {
+                    $scope.ok = function () {
                         $mi.close($scope.data)
                     };
                 }],
                 backdrop: 'static',
-            }).result.then(function(data) {
+            }).result.then(function (data) {
                 insertLink(data);
             });
         }
-        var insertVideo = function(url) {
+        var insertVideo = function (url) {
             var editor, dom, html;
             if (url.length > 0) {
                 editor = tinymce.get('body1');
@@ -152,27 +153,27 @@ define(['frame'], function(ngApp) {
                 editor.insertContent('<p>&nbsp;</p>' + html + '<p>&nbsp;</p>');
             }
         };
-        $scope.embedVideo = function() {
+        $scope.embedVideo = function () {
             $uibModal.open({
                 templateUrl: 'insertMedia.html',
-                controller: ['$uibModalInstance', '$scope', function($mi, $scope) {
+                controller: ['$uibModalInstance', '$scope', function ($mi, $scope) {
                     $scope.embedType = 'video';
                     $scope.data = {
                         url: ''
                     };
-                    $scope.cancel = function() {
+                    $scope.cancel = function () {
                         $mi.dismiss()
                     };
-                    $scope.ok = function() {
+                    $scope.ok = function () {
                         $mi.close($scope.data)
                     };
                 }],
                 backdrop: 'static',
-            }).result.then(function(data) {
+            }).result.then(function (data) {
                 insertVideo(data.url);
             });
         };
-        var insertAudio = function(url) {
+        var insertAudio = function (url) {
             var editor, dom, html;
             if (url.length > 0) {
                 editor = tinymce.get('body1');
@@ -184,40 +185,41 @@ define(['frame'], function(ngApp) {
                 editor.insertContent('<p>&nbsp;</p>' + html + '<p>&nbsp;</p>');
             }
         };
-        $scope.embedAudio = function() {
+        $scope.embedAudio = function () {
             $uibModal.open({
                 templateUrl: 'insertMedia.html',
-                controller: ['$uibModalInstance', '$scope', function($mi, $scope) {
+                controller: ['$uibModalInstance', '$scope', function ($mi, $scope) {
                     $scope.embedType = 'audio';
                     $scope.data = {
                         url: ''
                     };
-                    $scope.cancel = function() {
+                    $scope.cancel = function () {
                         $mi.dismiss()
                     };
-                    $scope.ok = function() {
+                    $scope.ok = function () {
                         $mi.close($scope.data)
                     };
                 }],
                 backdrop: 'static',
-            }).result.then(function(data) {
+            }).result.then(function (data) {
                 insertAudio(data.url);
             });
         };
-        $scope.delAttachment = function(index, att) {
-            http2.get('/rest/pl/fe/matter/article/attachment/del?site=' + $scope.editing.siteid + '&id=' + att.id).then(function(rsp) {
+        $scope.delAttachment = function (index, att) {
+            http2.get('/rest/pl/fe/matter/article/attachment/del?site=' + $scope.editing.siteid + '&id=' + att.id).then(function (rsp) {
                 $scope.editing.attachments.splice(index, 1);
             });
         };
-        $scope.downloadUrl = function(att) {
+        $scope.downloadUrl = function (att) {
             return '/rest/site/fe/matter/article/attachmentGet?site=' + $scope.editing.siteid + '&articleid=' + $scope.editing.id + '&attachmentid=' + att.id;
         };
 
-        $scope.$watch('editing.body_md', function(content) {
+        $scope.$watch('editing.body_md', function (content) {
             modifiedData.body_md = content;
             $scope.modified = true;
         });
-        $scope.$watch('editing', function(editing) {
+        $scope.$watch('editing', function (editing) {
+            var filemaxsize = 0
             if (!editing) return;
             if (editing.is_markdown !== 'Y')
                 if (tinymceEditor)
@@ -236,7 +238,7 @@ define(['frame'], function(ngApp) {
                 testChunks: false,
             });
             r.assignBrowse(document.getElementById('addAttachment'));
-            r.on('fileAdded', function(oFile, event) {
+            r.on('fileAdded', function (oFile, event) {
                 var f = oFile.file;
                 if (filetypes) {
                     if (!(f.name.lastIndexOf("."))) {
@@ -250,17 +252,17 @@ define(['frame'], function(ngApp) {
                         return false;
                     }
                 }
-                if (filemaxsize!==0 && filemaxsize * 1024 * 1024 <= f.size) {
-                    noticebox.error("文件上传失败，大小不能超过" + filemaxsize+ "M");
+                if (filemaxsize !== 0 && filemaxsize * 1024 * 1024 <= f.size) {
+                    noticebox.error("文件上传失败，大小不能超过" + filemaxsize + "M");
                     return false;
                 }
-                $scope.$apply(function() {
+                $scope.$apply(function () {
                     noticebox.progress('开始上传文件');
                 });
                 r.upload();
             });
-            r.on('progress', function(file, event) {
-                $scope.$apply(function() {
+            r.on('progress', function (file, event) {
+                $scope.$apply(function () {
                     noticebox.progress('正在上传文件：' + Math.floor(r.progress() * 100) + '%');
                     if (Math.floor(r.progress() * 100) === 100) {
                         noticebox.close();
@@ -271,7 +273,7 @@ define(['frame'], function(ngApp) {
                 noticebox.error('上传发生错误，请稍后再试!');
                
             });*/
-            r.on('complete', function() {
+            r.on('complete', function () {
                 var f, lastModified, posted;
                 f = r.files.pop().file;
                 lastModified = f.lastModified ? f.lastModified : (f.lastModifiedDate ? f.lastModifiedDate.getTime() : 0);
@@ -283,12 +285,12 @@ define(['frame'], function(ngApp) {
                     uniqueIdentifier: f.uniqueIdentifier,
                 };
 
-                http2.post('/rest/pl/fe/matter/article/attachment/add?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id, posted).then(function(rsp) {
+                http2.post('/rest/pl/fe/matter/article/attachment/add?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id, posted).then(function (rsp) {
                     $scope.editing.attachments.push(rsp.data);
                 });
             });
         });
-        $scope.$on('tinymce.instance.init', function(event, editor) {
+        $scope.$on('tinymce.instance.init', function (event, editor) {
             tinymceEditor = editor;
             if ($scope.editing) {
                 if ($scope.editing.is_markdown !== 'Y') {
@@ -296,7 +298,7 @@ define(['frame'], function(ngApp) {
                 }
             }
         });
-        $scope.$on('tinymce.content.change', function(event, changed) {
+        $scope.$on('tinymce.content.change', function (event, changed) {
             var content;
             var oArticle = $scope.editing;
             if (oArticle.is_markdown !== 'Y') {

1f07e1c9d039a9b44e7bd9d9732b634cef2ae1d5
jasony62 <jasony62@qq.com>
2020-02-27 14:05:09 +0800

    update code

--- config.php
+++ config.php
@@ -43,7 +43,7 @@ file_exists(dirname(__FILE__) . '/cus/config.php') && include_once dirname(__FIL
  */
 !defined('APP_LOGO') && define('APP_LOGO', '/static/img/logo.png');
 /**
- * 定义应用登录注册也的bannner图
+ * 定义应用登录注册页的bannner图
  */
 !defined('APP_ACCESS_BANNER') && define('APP_ACCESS_BANNER', '/static/img/access.png');
 /**

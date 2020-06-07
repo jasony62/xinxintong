14c227a7b8cc5ab18172c7fbf8fe27e4ee431699
jasony62 <jasony62@qq.com>
2020-06-07 17:12:07 +0800

    解决记录活动上传文件的bug

--- models/matter/tmplmsg/batch.php
+++ models/matter/tmplmsg/batch.php
@@ -26,18 +26,20 @@ class batch_model extends \TMS_MODEL
    * @param string $siteId
    * @param int $tmplmsgId 应用内模板消息id
    * @param object $oCreator
-   * @param array $userids
+   * @param array $receivers
+   * @param object $params 
    *
    */
   public function send($siteId, $tmplmsgId, $oCreator, $receivers, $params, $aOptions = [])
   {
     if (count($receivers) === 0) {
-      return true;
+      return [true, 'ok'];
     }
 
-    /*接收消息的用户*/
+    /* 接收消息的用户 */
     $modelAcnt = $this->model('site\user\account');
-    $mapOfUsers = [];
+    $mapOfUsers = []; // 接收消息的用户，进行了去重
+    $wxOpenids = []; // 具备微信openid的用户列表，准备发送微信模板消息
     foreach ($receivers as $oReceiver) {
       if (isset($oReceiver->unionid)) {
         $oUser = $modelAcnt->byPrimaryUnionid($siteId, $oReceiver->unionid);
@@ -47,6 +49,7 @@ class batch_model extends \TMS_MODEL
       if ($oUser) {
         isset($oReceiver->assoc_with) && $oUser->assoc_with = $oReceiver->assoc_with;
         $mapOfUsers[$oUser->uid] = $oUser;
+        if (!empty($oUser->wx_openid)) $wxOpenids[] = $oUser->wx_openid;
       }
     }
 
@@ -60,15 +63,34 @@ class batch_model extends \TMS_MODEL
       'template_id' => $tmpl->templateid,
       'url' => $url,
     ];
-    // 文本格式模板消息
-    $txtTmplMsg = [];
+    $coverData = []; // 消息推送服务封面消息参数
+    $txtTmplMsg = []; // 文本格式模板消息
     $txtTmplMsg[] = $tmpl->title;
-    // 组装模板内容
+    /* 组装模板内容 */
     if ($tmpl->params) {
       foreach ($tmpl->params as $tp) {
         $value = isset($params->{$tp->pname}) ? $params->{$tp->pname} : (isset($params->{$tp->id}) ? $params->{$tp->id} : '');
         $wxTmplMsg['data'][$tp->pname] = ['value' => $value, 'color' => '#173177'];
         $txtTmplMsg[] = $tp->plabel . '：' . $value;
+        $coverData[$tp->pname] = ['value' => $value, 'color' => empty($tp->color) ? '#173177' : $tp->color];
+      }
+    }
+
+    /* 指定了消息推送服务 */
+    if (defined('TMS_MESSENGER_BACK_ADDRESS')) {
+      /* 通过消息服务发送微信模板消息 */
+      if (!empty($tmpl->templateid) && count($wxOpenids)) {
+        /* 在消息服务中创建发送消息任务 */
+        list($success, $oMsgTask) = $this->_createMsgTask($siteId, $tmpl, $coverData, $url);
+        if ($success !== true) return [false, $oMsgTask];
+
+        /* 在消息服务中创建发送消息 */
+        list($success, $result) = $this->_createMsg($siteId, $oMsgTask->code, $wxOpenids);
+        if ($success !== true) return [false, $result];
+
+        /* 进行消息推送 */
+        list($success, $result) = $this->_pushMsgTask($siteId, $oMsgTask->code);
+        if ($success !== true) return [false, $result];
       }
     }
 
@@ -98,15 +120,18 @@ class batch_model extends \TMS_MODEL
           $wxTmplMsg['touser'] = $oUser->wx_openid;
           $log['openid'] = $oUser->wx_openid;
           $log['data'] = $modelTmpl->escape($modelTmpl->toJson($wxTmplMsg));
-          if (!isset($snsConfig)) {
-            $snsConfig = $this->model('sns\wx')->bySite($tmpl->siteid);
-            $wxProxy = $this->model('sns\wx\proxy', $snsConfig);
-          }
-          $rst = $wxProxy->messageTemplateSend($wxTmplMsg);
-          if ($rst[0] === false) {
-            $log['status'] = 'failed:' . $rst[1];
-          } else {
-            $log['msgid'] = $rst[1]->msgid;
+          /* 没有指定消息推送服务，使用本地推送 */
+          if (!defined('TMS_MESSENGER_BACK_ADDRESS')) {
+            if (!isset($snsConfig)) {
+              $snsConfig = $this->model('sns\wx')->bySite($tmpl->siteid);
+              $wxProxy = $this->model('sns\wx\proxy', $snsConfig);
+            }
+            $rst = $wxProxy->messageTemplateSend($wxTmplMsg);
+            if ($rst[0] === false) {
+              $log['status'] = 'failed:' . $rst[1];
+            } else {
+              $log['msgid'] = $rst[1]->msgid;
+            }
           }
           $modelTmpl->insert('xxt_log_tmplmsg_detail', $log, false);
         } else {
@@ -117,7 +142,7 @@ class batch_model extends \TMS_MODEL
           }
           $log['data'] = $modelTmpl->escape($modelTmpl->toJson($wxTxtTmplMsg));
 
-          $rst = $this->_sendTxtByOpenid($siteId, $oUser->wx_openid, 'wx', $wxTxtTmplMsg, $log);
+          $this->_sendTxtByOpenid($siteId, $oUser->wx_openid, 'wx', $wxTxtTmplMsg, $log);
         }
       }
       /* 易信用户，将模板消息转换文本消息 */
@@ -129,9 +154,11 @@ class batch_model extends \TMS_MODEL
         }
         $log['data'] = $modelTmpl->escape($modelTmpl->toJson($qyTxtTmplMsg));
 
-        $rst = $this->_sendTxtByOpenid($siteId, $oUser->qy_openid, 'qy', $qyTxtTmplMsg, $log);
+        $this->_sendTxtByOpenid($siteId, $oUser->qy_openid, 'qy', $qyTxtTmplMsg, $log);
       }
     }
+
+    return [true, 'ok'];
   }
   /**
    * 创建批次
@@ -154,6 +181,84 @@ class batch_model extends \TMS_MODEL
     $oBatch->id = $this->insert('xxt_log_tmplmsg_batch', $oBatch, true);
 
     return $oBatch;
+  }
+  /**
+   * 通过消息服务创建发送任务
+   * 
+   * @param object $tmpl 微信消息模板定义，包含模板参数，来源于表xxt_tmplmsg
+   * @param array $coverData 消息封面参数默认值
+   * @param string $bodyUrl 消息内容url
+   */
+  private function _createMsgTask($siteId, $tmpl, $coverData, $bodyUrl)
+  {
+    if (!defined('TMS_MESSENGER_BACK_ADDRESS')) {
+      return [false, '没有配置[TMS_MESSENGER_BACK_ADDRESS]参数'];
+    }
+    if (empty($tmpl->tms_msg_wx_template_code)) {
+      return [false, '指定的消息模板没有对应的消息服务微信模版编码'];
+    }
+
+    $cover = ["templateCode" => $tmpl->tms_msg_wx_template_code];
+    if (!empty($coverData)) $cover['data'] = $coverData;
+
+    $posted = ['title' => 'test', 'remark' => 'test', 'cover' => $cover];
+
+    if (!empty($bodyUrl)) $posted['body'] = ['url' => $bodyUrl];
+
+    $url = TMS_MESSENGER_BACK_ADDRESS . "/send/task/add";
+    $url .= "?bucket={$siteId}";
+
+    list($success, $rsp) = tmsHttpPost($url, $posted);
+    if ($success !== true) return [false, $rsp];
+    if ($rsp->code !== 0) return [false, $rsp->msg];
+
+    return [true, $rsp->result];
+  }
+  /**
+   * 创建消息任务 
+   */
+  private function _createMsg($siteId, $taskCode, $receivers)
+  {
+    if (!defined('TMS_MESSENGER_BACK_ADDRESS')) {
+      return [false, '没有配置[TMS_MESSENGER_BACK_ADDRESS]参数'];
+    }
+    if (empty($taskCode)) {
+      return [false, '指定的消息任务编码为空'];
+    }
+    if (empty($receivers)) {
+      return [false, '没有指定接收消息的用户'];
+    }
+    $url = TMS_MESSENGER_BACK_ADDRESS . "/send/message/add";
+    $url .= "?bucket={$siteId}&task={$taskCode}";
+
+    $posted = ["receivers" => $receivers];
+
+    list($success, $rsp) = tmsHttpPost($url, $posted);
+    if ($success !== true) return [false, $rsp];
+    if ($rsp->code !== 0) return [false, $rsp->msg];
+
+    return [true, $rsp->result];
+  }
+  /**
+   * 通过消息服务推送消息
+   */
+  private function _pushMsgTask($siteId, $taskCode)
+  {
+    if (!defined('TMS_MESSENGER_BACK_ADDRESS')) {
+      return [false, '没有配置[TMS_MESSENGER_BACK_ADDRESS]参数'];
+    }
+    if (empty($taskCode)) {
+      return [false, '指定的消息任务编码为空'];
+    }
+
+    $url = TMS_MESSENGER_BACK_ADDRESS . "/send/task/push";
+    $url .= "?bucket={$siteId}&code={$taskCode}";
+
+    list($success, $rsp) = tmsHttpGet($url);
+    if ($success !== true) return [false, $rsp];
+    if ($rsp->code !== 0) return [false, $rsp->msg];
+
+    return [true, $rsp->result];
   }
   /**
    *

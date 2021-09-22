d05db7ed3cc394b611239612a3eb50577ab0e6ff
jasony62 <jasony62@qq.com>
2021-09-22 10:00:02 +0800

    优化代码

--- views/default/pl/fe/matter/article/user.js
+++ views/default/pl/fe/matter/article/user.js
@@ -5,32 +5,35 @@ define(['frame'], function (ngApp) {
     'srvUser',
     function ($scope, srvUser) {
       $scope.userapp = {}
+      /*获取用户行为数据*/
+      $scope.list = function () {
+        srvUser.watch($scope.editing).then(({ userapp, readers }) => {
+          /* 如果用户来源于分组活动，补充用户分组信息 */
+          if (userapp && userapp.type === 'group') {
+            if (Array.isArray(userapp.teams)) {
+              let teamsById = userapp.teams.reduce((prev, curr) => {
+                prev[curr.team_id] = curr
+                return prev
+              }, {})
+              readers.forEach((reader) => {
+                if (teamsById[reader.team_id])
+                  reader.team = teamsById[reader.team_id]
+              })
+            }
+          }
+          $scope.readers = readers
+        })
+      }
+      /*获得单图文数据*/
       $scope.$watch('editing', function (nv) {
         if (!nv) return
-        let entryRule = nv.entryRule
-        let { scope, group, member } = entryRule
+        let { scope, group, member } = nv.entryRule
         if ((scope && scope.group === 'Y') || scope.member === 'Y') {
           /*用户来源活动类型*/
           if (scope.group === 'Y' && group) $scope.userapp.type = 'group'
           else if (scope.member === 'Y' && member)
             $scope.userapp.type = 'mschema'
-          /*获取用户行为数据*/
-          srvUser.watch(nv).then(({ userapp, readers }) => {
-            /* 如果用户来源于分组活动，补充用户分组信息 */
-            if (userapp && userapp.type === 'group') {
-              if (Array.isArray(userapp.teams)) {
-                let teamsById = userapp.teams.reduce((prev, curr) => {
-                  prev[curr.team_id] = curr
-                  return prev
-                }, {})
-                readers.forEach((reader) => {
-                  if (teamsById[reader.team_id])
-                    reader.team = teamsById[reader.team_id]
-                })
-              }
-            }
-            $scope.readers = readers
-          })
+          $scope.list()
         }
       })
     },

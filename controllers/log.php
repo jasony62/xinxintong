c0ea5664662f795971ea0d6f4362afee5e084389
jasony62 <jasony62@qq.com>
2023-10-20 21:28:39 +0800

    优化代码

--- controllers/log.php
+++ controllers/log.php
@@ -1,40 +1,44 @@
 <?php
+
 /**
  * 记录日志
  */
-class log extends TMS_CONTROLLER {
-	/**
-	 *
-	 */
-	public function get_access_rule() {
-		$rule_action['rule_type'] = 'white';
-		$rule_action['actions'][] = 'index';
+class log extends TMS_CONTROLLER
+{
+  /**
+   *
+   */
+  public function get_access_rule()
+  {
+    $rule_action['rule_type'] = 'white';
+    $rule_action['actions'][] = 'index';
 
-		return $rule_action;
-	}
-	/**
-	 * 通过日志记录
-	 */
-	public function add_action() {
-		$referer = !empty(tms_get_server('HTTP_REFERER')) ? tms_get_server('HTTP_REFERER') : '';
-		if (preg_match('/[?&]site=([^&]*)/', $referer, $matches)) {
-			$siteid = $matches[1];
-		} else {
-			$siteid = 'platform';
-		}
+    return $rule_action;
+  }
+  /**
+   * 通过日志记录
+   */
+  public function add_action()
+  {
+    $referer = !empty(tms_get_server('HTTP_REFERER')) ? tms_get_server('HTTP_REFERER') : '';
+    if (preg_match('/[?&]site=([^&]*)/', $referer, $matches)) {
+      $siteid = $matches[1];
+    } else {
+      $siteid = 'platform';
+    }
 
-		$modelWay = $this->model('site\fe\way');
-		$this->who = $modelWay->who($siteid);
+    $modelWay = $this->model('site\fe\way');
+    $this->who = $modelWay->who($siteid);
 
-		$data = $this->getPostJson();
+    $data = $this->getPostJson();
 
-		$agent = tms_get_server('HTTP_USER_AGENT');
+    $agent = tms_get_server('HTTP_USER_AGENT');
 
-		$src = isset($data->src) ? $data->src : '';
-		$msg = isset($data->msg) ? $data->msg : '';
+    $src = isset($data->src) ? $data->src : '';
+    $msg = isset($data->msg) ? $data->msg : '';
 
-		$this->model('log')->log($this->who->uid, $src, $msg, $agent, $referer);
+    $this->model('log')->log($this->who->uid, $src, $msg, $agent, $referer);
 
-		return new \ResponseData('ok');
-	}
+    return new \ResponseData('ok');
+  }
 }
